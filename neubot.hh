12d5ce993541e36d5d6d12e2e50e9e5774793b14
Simone Basso <bassosimone@gmail.com>
2014-03-31 13:01:37 +0200

    neubot.hh: nonworking cast operators: quick fix

--- neubot.hh
+++ neubot.hh
@@ -14,111 +14,6 @@ namespace Neubot {
     class Pollable;
     class Poller;
 
-    class EchoServer {
-        struct NeubotEchoServer *_context;
-
-      public:
-
-// Swig doesn't understand the cast operator
-#ifndef SWIG
-        operator struct NeubotEchoServer *(void) {
-            return (this->_context);
-        }
-#endif
-
-        EchoServer(Poller *poller, int use_ipv6, const char *address,
-          const char *port) {
-            this->_context = NeubotEchoServer_construct(
-              (struct NeubotPoller *) poller, use_ipv6, address, port);
-            if (this->_context == NULL)
-                abort();
-        };
-
-    };
-
-    class Pollable {
-        struct NeubotPollable *_context;
-
-        static void handle_read__(void *opaque) {
-            Pollable *self = (Pollable *) opaque;
-            self->handle_read();
-        };
-
-        static void handle_write__(void *opaque) {
-            Pollable *self = (Pollable *) opaque;
-            self->handle_write();
-        };
-
-        static void handle_close__(void *opaque) {
-            Pollable *self = (Pollable *) opaque;
-            self->handle_close();
-        };
-
-      public:
-
-// Swig doesn't understand the cast operator
-#ifndef SWIG
-        operator struct NeubotPollable *(void) {
-            return (this->_context);
-        }
-#endif
-
-        virtual void handle_read(void) = 0;
-
-        virtual void handle_write(void) = 0;
-
-        virtual void handle_close(void) = 0;
-
-        Pollable(Poller *poller) {
-            this->_context = NeubotPollable_construct(
-              (struct NeubotPoller *) poller, this->handle_read__,
-              this->handle_write__, this->handle_close__, this);
-            if (this->_context == NULL)
-                abort();
-        };
-
-        int attach(long long filenum) {
-            return (NeubotPollable_attach(this->_context, filenum));
-        };
-
-        void detach(void) {
-            NeubotPollable_detach(this->_context);
-        };
-
-        long long fileno(void) {
-            return (NeubotPollable_fileno(this->_context));
-        };
-
-        int set_readable(void) {
-            return (NeubotPollable_set_readable(this->_context));
-        };
-
-        int unset_readable(void) {
-            return (NeubotPollable_unset_readable(this->_context));
-        };
-
-        int set_writable(void) {
-            return (NeubotPollable_set_writable(this->_context));
-        };
-
-        int unset_writable(void) {
-            return (NeubotPollable_unset_writable(this->_context));
-        };
-
-        void set_timeout(double delta) {
-            NeubotPollable_set_timeout(this->_context, delta);
-        };
-
-        void clear_timeout(void) {
-            NeubotPollable_clear_timeout(this->_context);
-        };
-
-        virtual ~Pollable(void) {
-            NeubotPollable_close(this->_context);
-        };
-
-    };
-
     class Poller {
         struct NeubotPoller *_context;
 
@@ -130,6 +25,10 @@ namespace Neubot {
             return (this->_context);
         }
 #endif
+
+        NeubotPoller *get_struct(void) {
+            return (this->_context);
+        };
 
         Poller(void) {
             this->_context = NeubotPoller_construct();
@@ -181,6 +80,112 @@ namespace Neubot {
 
         void break_loop(void) {
             NeubotPoller_break_loop(this->_context);
+        };
+
+    };
+
+    class EchoServer {
+        struct NeubotEchoServer *_context;
+
+      public:
+
+// Swig doesn't understand the cast operator
+#ifndef SWIG
+        operator struct NeubotEchoServer *(void) {
+            return (this->_context);
+        }
+#endif
+
+        EchoServer(Poller *poller, int use_ipv6, const char *address,
+          const char *port) {
+            this->_context = NeubotEchoServer_construct(
+              poller->get_struct(), use_ipv6, address,
+              port);
+            if (this->_context == NULL)
+                abort();
+        };
+
+    };
+
+    class Pollable {
+        struct NeubotPollable *_context;
+
+        static void handle_read__(void *opaque) {
+            Pollable *self = (Pollable *) opaque;
+            self->handle_read();
+        };
+
+        static void handle_write__(void *opaque) {
+            Pollable *self = (Pollable *) opaque;
+            self->handle_write();
+        };
+
+        static void handle_close__(void *opaque) {
+            Pollable *self = (Pollable *) opaque;
+            self->handle_close();
+        };
+
+      public:
+
+// Swig doesn't understand the cast operator
+#ifndef SWIG
+        operator struct NeubotPollable *(void) {
+            return (this->_context);
+        }
+#endif
+
+        virtual void handle_read(void) = 0;
+
+        virtual void handle_write(void) = 0;
+
+        virtual void handle_close(void) = 0;
+
+        Pollable(Poller *poller) {
+            this->_context = NeubotPollable_construct(
+              poller->get_struct(), this->handle_read__,
+              this->handle_write__, this->handle_close__, this);
+            if (this->_context == NULL)
+                abort();
+        };
+
+        int attach(long long filenum) {
+            return (NeubotPollable_attach(this->_context, filenum));
+        };
+
+        void detach(void) {
+            NeubotPollable_detach(this->_context);
+        };
+
+        long long fileno(void) {
+            return (NeubotPollable_fileno(this->_context));
+        };
+
+        int set_readable(void) {
+            return (NeubotPollable_set_readable(this->_context));
+        };
+
+        int unset_readable(void) {
+            return (NeubotPollable_unset_readable(this->_context));
+        };
+
+        int set_writable(void) {
+            return (NeubotPollable_set_writable(this->_context));
+        };
+
+        int unset_writable(void) {
+            return (NeubotPollable_unset_writable(this->_context));
+        };
+
+        void set_timeout(double delta) {
+            NeubotPollable_set_timeout(this->_context, delta);
+        };
+
+        void clear_timeout(void) {
+            NeubotPollable_clear_timeout(this->_context);
+        };
+
+        virtual ~Pollable(void) {
+            NeubotPollable_close(this->_context);
         };
 
     };
