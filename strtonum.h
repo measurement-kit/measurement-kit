13389ba2eef1a7bd6092c86bf103f2f3de210e6b
Simone Basso <bassosimone@gmail.com>
2014-04-23 17:24:30 +0200

    Correctly export neubot_strtonum() to C++ code

--- strtonum.h
+++ strtonum.h
@@ -2,5 +2,13 @@
  * Public domain, 2013 Simone Basso.
  */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 long long neubot_strtonum(const char *, long long, long long,
                           const char **);
+
+#ifdef __cplusplus
+}
+#endif
