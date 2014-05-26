0e91c23b06c072169e1ce5178d5494d7675ee1eb
alessandro40 <alessandro.40@alice.it>
2014-05-26 18:04:43 +0200

    Rewrite connection.py test using libneubot.py API

--- test/connection.py
+++ test/connection.py
@@ -7,91 +7,71 @@ import sys
 
 sys.path.insert(0, "/usr/local/share/libneubot")
 
-from libneubot import LIBNEUBOT
-from libneubot import NEUBOT_SLOT_VO
+from libneubot import Connection
+from libneubot import Poller
+from libneubot import Protocol
 
 # See https://mail.python.org/pipermail/python-list/2010-January/563868.html
 BUFF_LENGTH = 65535
 BUFF_TYPE = ctypes.c_char * BUFF_LENGTH
 
-def _terminate(ccontext):
-    connection = ccontext[0]
-    proto = LIBNEUBOT.NeubotConnection_get_protocol(connection)
-    if not proto:
-        sys.exit("connection - cannot get protocol")
-    poller = LIBNEUBOT.NeubotProtocol_get_poller(proto)
-    if not poller:
-        sys.exit("connection - cannot get poller")
-    LIBNEUBOT.NeubotPoller_break_loop(poller)
+class MyProtocol(Protocol):
 
-def connect_callback(ccontext):
-    sys.exit("connection - unexpected connect event")
+    def slot_connect(self):
+        sys.exit("connection - unexpected connect event")
 
-def data_callback(ccontext):
-    connection = ccontext[0]
-    buff = bytearray(BUFF_LENGTH)
-    llbuff = BUFF_TYPE.from_buffer(buff)
-    while True:
-        result = LIBNEUBOT.NeubotConnection_read(connection,
-          llbuff, BUFF_LENGTH)
-        if result < 0:
-            sys.exit("connection - read failed")
-        if result == 0:
-            sys.stderr.write("connection - exhausted input buffer\n")
-            break
-        result = LIBNEUBOT.NeubotConnection_write(connection,
-          llbuff, result)
-        if result != 0:
-            sys.exit("connection - write failed")
+    def slot_data(self):
+        buff = bytearray(BUFF_LENGTH)
+        llbuff = BUFF_TYPE.from_buffer(buff)
+        while True:
+            result = self.connection.read(llbuff, BUFF_LENGTH)
+            if result < 0:
+                sys.exit("connection - read failed")
+            if result == 0:
+                sys.stderr.write("connection - exhausted input buffer\n")
+                break
+            result = self.connection.write(llbuff, result)
+            if result != 0:
+                sys.exit("connection - write failed")
 
-def ssl_callback(ccontext):
-    sys.stderr.write("connection - SSL\n")
+    def slot_ssl(self):
+        sys.stderr.write("connection - SSL\n")
 
-def flush_callback(ccontext):
-    sys.stderr.write("connection - flushed\n")
+    def slot_flush(self):
+        sys.stderr.write("connection - flushed\n")
 
-def eof_callback(ccontext):
-    sys.stderr.write("connection - eof\n")
-    _terminate(ccontext)
+    def slot_eof(self):
+        sys.stderr.write("connection - eof\n")
+        _terminate(self)
 
-def error_callback(ccontext):
-    sys.stderr.write("connection - error\n")
-    _terminate(ccontext)
+    def slot_error(self):
+        sys.stderr.write("connection - error\n")
+        _terminate(self)
 
-CONNECT_CALLBACK = NEUBOT_SLOT_VO(connect_callback)
-SSL_CALLBACK = NEUBOT_SLOT_VO(ssl_callback)
-DATA_CALLBACK = NEUBOT_SLOT_VO(data_callback)
-FLUSH_CALLBACK = NEUBOT_SLOT_VO(flush_callback)
-EOF_CALLBACK = NEUBOT_SLOT_VO(eof_callback)
-ERROR_CALLBACK = NEUBOT_SLOT_VO(error_callback)
 
 def main():
     """ Main function """
 
-    poller = LIBNEUBOT.NeubotPoller_construct()
+    poller = Poller() 
     if not poller:
         sys.exit("connection - cannot construct the poller")
 
-    context = []
-    ccontext = ctypes.py_object(context)
-
-    proto = LIBNEUBOT.NeubotProtocol_construct(poller, CONNECT_CALLBACK,
-      SSL_CALLBACK, DATA_CALLBACK, FLUSH_CALLBACK, EOF_CALLBACK,
-      ERROR_CALLBACK, ccontext)
+    proto = MyProtocol(poller)
     if not proto:
         sys.exit("connection - cannot construct the protocol")
 
-    connection = LIBNEUBOT.NeubotConnection_attach(proto, 0)
-    if not connection:
+    proto.connection = Connection.attach(proto, 0)
+    if not proto.connection:
         sys.exit("connection - cannot attach the connection")
 
-    result = LIBNEUBOT.NeubotConnection_set_timeout(connection, 7.0)
+    result = proto.connection.set_timeout(7.0)
     if result != 0:
         sys.exit("connection - cannot set the timeout")
 
-    context.append(connection)
-
-    LIBNEUBOT.NeubotPoller_loop(poller)
+    if proto.connection.enable_read() != 0:
+        sys.exit("connection - cannot enable read")
+    
+    poller.loop() 
 
 if __name__ == "__main__":
     main()
