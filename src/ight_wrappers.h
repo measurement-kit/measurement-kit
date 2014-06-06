98def29b2520c09b5789ec64bd7dd90a7e4bf889
Simone Basso <bassosimone@gmail.com>
2014-06-06 19:58:44 +0200

    Remove wrappers from C++ to C and Python
    The wrappers from C++ to C and Python were added to LibNeubot following
    this rationale:
    
    1) LibNeubot was to be written in C++ for SWIG to generate more
       easily usable NDK/JNI code;
    
    2) The C interface was helpful to cherry-pick functions from Python
       using ctypes and to create a nice Python interface around the rought
       edges of the ctypes interface. In turn, this was because LibNeubot
       was to be used both for mobile and Neubot for PC.
    
    After the discussion with @hellais that led to Libight, the scope was
    narrowed to include only 1), because Libight is meant to support Neubot
    and OONI on mobile platforms only.
    
    Therefore (and, also, because the wrappers are on my way now that I am
    working on a fully C++ poller that uses std::function and other C++11
    goodies), I suggest that we remove the `optional complexity` represented
    by 2) to develop more quickly.
    
    In fact, if 2) is not a Libight objective, my time is better spent working
    on the C++ core rather than working to keep 2) up to date.
    
    I tested this change by rebuilding everything as in `make distclean;
    autoreconf -i; ./configure; make V=0 check', without breakages or new
    warnings.
    
    I am aware that this diff removes some Python based tests, but C++ tests
    are to be written in any case (I'd rather do testing of the C++ core than
    do testing of the Python wrapper of the C wrapper of the core itself).

--- src/ight_wrappers.h
+++ src/ight_wrappers.h
@@ -21,12 +21,9 @@ struct evbuffer;
 
 /* Classes: */
 
-struct IghtConnection;
 struct IghtEchoServer;
 struct IghtPollable;
 struct IghtPoller;
-struct IghtProtocol;
-struct IghtStringVector;
 
 /* IghtPoller API: */
 
@@ -47,81 +44,10 @@ void IghtPoller_loop(struct IghtPoller *);
 
 void IghtPoller_break_loop(struct IghtPoller *);
 
-/* IghtStringVector API: */
-
-struct IghtStringVector *IghtStringVector_construct(struct IghtPoller *,
-    size_t);
-
-int IghtStringVector_append(struct IghtStringVector *, const char *);
-
-struct IghtPoller *IghtStringVector_get_poller(struct IghtStringVector *);
-
-const char *IghtStringVector_get_next(struct IghtStringVector *);
-
-void IghtStringVector_destruct(struct IghtStringVector *);
-
 /* IghtEchoServer API: */
 
 struct IghtEchoServer *IghtEchoServer_construct(struct IghtPoller *, int,
     const char *, const char *);
-
-/* IghtProtocol API: */
-
-struct IghtProtocol *IghtProtocol_construct(struct IghtPoller *, ight_slot_vo,
-    ight_slot_vo, ight_slot_vo, ight_slot_vo, ight_slot_vo, ight_slot_vo,
-    void *);
-
-struct IghtPoller *IghtProtocol_get_poller(struct IghtProtocol *);
-
-void IghtProtocol_destruct(struct IghtProtocol *);
-
-/* IghtConnection API: */
-
-struct IghtConnection *IghtConnection_attach(struct IghtProtocol *, long long);
-
-struct IghtConnection *IghtConnection_connect(struct IghtProtocol *,
-    const char *, const char *, const char *);
-
-struct IghtConnection *IghtConnection_connect_hostname(struct IghtProtocol *,
-    const char *, const char *, const char *);
-
-struct IghtProtocol *IghtConnection_get_protocol(struct IghtConnection *);
-
-int IghtConnection_set_timeout(struct IghtConnection *, double);
-
-int IghtConnection_clear_timeout(struct IghtConnection *);
-
-int IghtConnection_start_tls(struct IghtConnection *, unsigned);
-
-int IghtConnection_read(struct IghtConnection *, char *, size_t);
-
-int IghtConnection_readline(struct IghtConnection *, char *, size_t);
-
-int IghtConnection_readn(struct IghtConnection *, char *, size_t);
-
-int IghtConnection_discardn(struct IghtConnection *, size_t);
-
-int IghtConnection_write(struct IghtConnection *, const char *, size_t);
-
-int IghtConnection_puts(struct IghtConnection *, const char *);
-
-int IghtConnection_write_rand(struct IghtConnection *, size_t);
-
-int IghtConnection_write_readbuf(struct IghtConnection *, const char *, size_t);
-
-int IghtConnection_puts_readbuf(struct IghtConnection *, const char *);
-
-int IghtConnection_write_rand_readbuf(struct IghtConnection *, size_t);
-
-int IghtConnection_read_into_(struct IghtConnection *, struct evbuffer *);
-
-int IghtConnection_write_from_(struct IghtConnection *, struct evbuffer *);
-
-int IghtConnection_enable_read(struct IghtConnection *);
-
-int IghtConnection_disable_read(struct IghtConnection *);
-
-void IghtConnection_close(struct IghtConnection *);
 
 /* IghtPollable API: */
 
