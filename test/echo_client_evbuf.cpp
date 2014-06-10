05be648429306ca1299e961e99d6d40c6af5b4f4
Simone Basso <bassosimone@gmail.com>
2014-06-10 09:32:02 +0200

    Switch to the BSD license
    This commit switches to the BSD license all the files that
    are not touched by other pending pull requests (particularly
    by #6 that, if accepted, removes a number of files).
    
    Depending on #6 destiny, another commit to switch the remainder
    of the tree will or will not follow.

--- test/echo_client_evbuf.cpp
+++ test/echo_client_evbuf.cpp
@@ -1,24 +1,8 @@
-/* libneubot/test/echo_client_evbuf.cpp */
-
 /*-
- * Copyright (c) 2013-2014
- *     Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
- *     and Simone Basso <bassosimone@gmail.com>.
+ * This file is part of Libight <https://libight.github.io/>.
  *
- * This file is part of Neubot <http://www.neubot.org/>.
- *
- * Neubot is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Neubot is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+ * Libight is free software. See AUTHORS and LICENSE for more
+ * information on the copying conditions.
  */
 
 /*
