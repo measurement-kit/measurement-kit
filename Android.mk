e5f0d65e1f5bcce392475136e34b7c6a022a7f68
alessandro40 <alessandro.40@alice.it>
2014-06-26 10:11:24 +0200

    Adapt Android.mk to Libight file system
    Libight has a different file system than LibNeubot, so I had to change the path
    of the source files.
    
    Moreover, this Android.mk includes the Android.mk of LibEvent, in order to build
    it as well, during the compilation of Libight.

--- Android.mk
+++ Android.mk
@@ -1,21 +1,31 @@
-LOCAL_PATH := $(call my-dir)
+TOP_LOCAL_PATH := $(call my-dir)
+
+include $(TOP_LOCAL_PATH)/src/ext/libevent/Android.mk
+
+LOCAL_PATH := $(TOP_LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_LIBRARIES :=event2
+LOCAL_STATIC_LIBRARIES := event2
 
-LOCAL_MODULE:= neubot
-LOCAL_MODULE_TAGS:= optional
+LOCAL_C_INCLUDES := \
+    src \
+    src/ext/libevent/include \
+    src/ext/libevent/android
+
+LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%)
+
+LOCAL_MODULE:= ight
 
 LOCAL_CFLAGS := -DIGHT_ANDROID
 
 LOCAL_SRC_FILES := \
-    echo.c \
-    libneubot.cpp \
-    log.c \
-    pollable.cpp \
-    poller.c \
-    strtonum.c \
-    utils.c
+    src/common/log.c \
+    src/common/poller.cpp \
+    src/common/stringvector.cpp \
+    src/common/utils.c \
+    src/ext/strtonum.c \
+    src/net/connection.cpp \
+    src/net/protocol.cpp
 
 include $(BUILD_STATIC_LIBRARY)
