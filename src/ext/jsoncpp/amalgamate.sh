67036531933643954b8df3e6f6d00a9967d67297
Simone Basso <bassosimone@gmail.com>
2014-09-22 19:39:42 +0200

    Add amalgamated JsonCpp sources
    Also add the very simple script used to generate the
    amalgamated sources, amalgamate.sh.
    
    I opted for adding the amalgamated source file and
    header in the repository, as opposed to automatically
    generating it at compile time, for now.
    
    If we see that autogenerating the file is better, we
    can switch to that model later.

---
+++ src/ext/jsoncpp/amalgamate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#
+# This file is part of Libight <https://libight.github.io/>.
+#
+# Libight is free software. See AUTHORS and LICENSE for more
+# information on the copying conditions.
+#
+
+python jsoncpp/amalgamate.py -t jsoncpp -s jsoncpp.cpp -i json.h
