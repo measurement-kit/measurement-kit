bd4d15f6181a31ed0371bc2a4f3d952a5318702b
Arturo Filastò <art@fuffa.org>
2014-10-21 12:17:25 +0200

    Add copy contructor also to FileReporter

--- src/report/file.hpp
+++ src/report/file.hpp
@@ -14,6 +14,11 @@ public:
       const time_t& start_time, const std::string& probe_ip,
       const std::map<std::string, std::string>& options,
       const std::string& filename);
+
+  FileReporter(const FileReporter& that) : test_name(that.test_name),
+    test_version(that.test_version), probe_ip(that.probe_ip),
+    start_time(that.start_time), options(that.options) {};
+
   void open();
   void writeEntry(ReportEntry& entry);
   void close();
