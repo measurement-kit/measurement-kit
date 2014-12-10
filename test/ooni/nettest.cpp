23e6822369f8b0ee794ebf54d597d3e9145c6f09
Simone Basso <bassosimone@gmail.com>
2014-12-10 18:17:55 +0100

    Review the code and understand the cause of segfault
    The problem is related to the end callback. Now, I am going
    to investigate what the problem is.

--- test/ooni/nettest.cpp
+++ test/ooni/nettest.cpp
@@ -11,9 +11,13 @@ using namespace ight::ooni::nettest;
 TEST_CASE("The NetTest should callback when it has finished running") {
   NetTest nettest("");
   nettest.begin([&](){
+    ight_break_loop();
+
+#if 0
     nettest.end([&](){
       ight_break_loop();
     });
+#endif
   });
   ight_loop();
 }
