21b34107b453a48c9a68844ed0133e1ce93a7308
Simone Basso <bassosimone@gmail.com>
2015-01-01 13:17:04 +0100

    Add regress tests for pointer.hpp
    The regress test include a test showing that it is safe to assign the
    return value of std::make_shared<T> to SharedPointer<T>.
    
    This is safe because SharedPointer<T> have exactly the same fields
    as std::shared_pointer<T> and because it inherits the copy and move
    constructors from std::shared_pointer<T>.

---
+++ test/common/pointer.cpp
@@ -0,0 +1,43 @@
+/*-
+ * This file is part of Libight <https://libight.github.io/>.
+ *
+ * Libight is free software. See AUTHORS and LICENSE for more
+ * information on the copying conditions.
+ */
+
+//
+// Tests for src/common/pointer.hpp
+//
+
+#define CATCH_CONFIG_MAIN
+#include "src/ext/Catch/single_include/catch.hpp"
+
+#include "src/common/pointer.hpp"
+
+using namespace ight::common::pointer;
+
+struct Foo {
+    double elem = 3.14;
+    Foo() {}
+    Foo(double x) : elem(x) {}
+};
+
+TEST_CASE("SharedPointer raises an exception when the pointee is nullptr") {
+    SharedPointer<Foo> foo;
+    REQUIRE_THROWS(auto k = foo->elem);
+    REQUIRE_THROWS(*foo);
+}
+
+TEST_CASE("We can safely assign to SharedPointer an empty shared_ptr") {
+    std::shared_ptr<Foo> antani;
+    SharedPointer<Foo> necchi = antani;
+    REQUIRE_THROWS(auto k = necchi->elem);
+    REQUIRE_THROWS(*necchi);
+}
+
+TEST_CASE("We can assign to SharedPointer the result of make_shared") {
+    SharedPointer<Foo> necchi = std::make_shared<Foo>(6.28);
+    REQUIRE(necchi->elem == 6.28);
+    auto foo = *necchi;
+    REQUIRE(foo.elem == 6.28);
+}
