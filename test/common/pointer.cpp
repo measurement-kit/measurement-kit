c73f858c0bfbf7a0a59baf7390a2cc2f0578853d
alessandro40 <alessandro.40@alice.it>
2015-02-17 12:55:47 +0100

    Change include path of "common/pointer.hpp"

--- test/common/pointer.cpp
+++ test/common/pointer.cpp
@@ -12,7 +12,7 @@
 #define CATCH_CONFIG_MAIN
 #include "src/ext/Catch/single_include/catch.hpp"
 
-#include <ight/src/common/pointer.hpp>
+#include <include/ight/common/pointer.hpp>
 
 using namespace ight::common::pointer;
 
