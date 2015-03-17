7c11ed92abe139af2a1af34668dcbfe231b684c0
Simone Basso <bassosimone@gmail.com>
2015-03-17 10:21:23 +0100

    Reindent files that were using tabs
    Basically, I run clang-format -i on those files and then I
    used the following
    
        sed -i ':a;N;$!ba;s/}\n}\n}\n/\n}}}\n/g' $files
    
    to restore }}} to close namespaces at end of headers.

--- include/ight/net/connection.hpp
+++ include/ight/net/connection.hpp
@@ -5,7 +5,7 @@
  * information on the copying conditions.
  */
 #ifndef IGHT_NET_CONNECTION_HPP
-# define IGHT_NET_CONNECTION_HPP
+#define IGHT_NET_CONNECTION_HPP
 
 #include <ight/common/constraints.hpp>
 #include <ight/common/error.hpp>
@@ -43,301 +43,267 @@ using namespace ight::protocols;
 
 class ConnectionState {
 
-	BuffereventSocket bev;
-	dns::Request dns_request;
-	unsigned int connecting = 0;
-	char *address = NULL;
-	char *port = NULL;
-	StringVector *addrlist = NULL;
-	char *family = NULL;
-	StringVector *pflist = NULL;
-	unsigned int must_resolve_ipv4 = 0;
-	unsigned int must_resolve_ipv6 = 0;
-	SharedPointer<DelayedCall> start_connect;
+    BuffereventSocket bev;
+    dns::Request dns_request;
+    unsigned int connecting = 0;
+    char *address = NULL;
+    char *port = NULL;
+    StringVector *addrlist = NULL;
+    char *family = NULL;
+    StringVector *pflist = NULL;
+    unsigned int must_resolve_ipv4 = 0;
+    unsigned int must_resolve_ipv6 = 0;
+    SharedPointer<DelayedCall> start_connect;
 
-	// Libevent callbacks
-	static void handle_read(bufferevent *, void *);
-	static void handle_write(bufferevent *, void *);
-	static void handle_event(bufferevent *, short, void *);
+    // Libevent callbacks
+    static void handle_read(bufferevent *, void *);
+    static void handle_write(bufferevent *, void *);
+    static void handle_event(bufferevent *, short, void *);
 
-	// Functions used when connecting
-	void connect_next(void);
-	void handle_resolve(int, char, std::vector<std::string>);
-	void resolve();
-	bool resolve_internal(char);
+    // Functions used when connecting
+    void connect_next(void);
+    void handle_resolve(int, char, std::vector<std::string>);
+    void resolve();
+    bool resolve_internal(char);
 
-	std::function<void(void)> on_connect_fn = [](void) {
-		/* nothing */
-	};
+    std::function<void(void)> on_connect_fn = [](void) {
+        /* nothing */
+    };
 
-	std::function<void(void)> on_ssl_fn = [](void) {
-		/* nothing */
-	};
+    std::function<void(void)> on_ssl_fn = [](void) {
+        /* nothing */
+    };
 
-	std::function<void(SharedPointer<Buffer>)> on_data_fn = [](
-			SharedPointer<Buffer>) {
-		/* nothing */
-	};
+    std::function<void(SharedPointer<Buffer>)> on_data_fn =
+        [](SharedPointer<Buffer>) {
+        /* nothing */
+    };
 
-	std::function<void(void)> on_flush_fn = [](void) {
-		/* nothing */
-	};
+    std::function<void(void)> on_flush_fn = [](void) {
+        /* nothing */
+    };
 
-	std::function<void(Error)> on_error_fn = [](Error) {
-		/* nothing */
-	};
+    std::function<void(Error)> on_error_fn = [](Error) {
+        /* nothing */
+    };
 
-    public:
-	ConnectionState(const char *, const char *, const char *,
-	    evutil_socket_t = IGHT_SOCKET_INVALID);
+  public:
+    ConnectionState(const char *, const char *, const char *,
+                    evutil_socket_t = IGHT_SOCKET_INVALID);
 
-	ConnectionState(ConnectionState&) = delete;
-	ConnectionState& operator=(ConnectionState&) = delete;
-	ConnectionState(ConnectionState&&) = delete;
-	ConnectionState& operator=(ConnectionState&&) = delete;
+    ConnectionState(ConnectionState &) = delete;
+    ConnectionState &operator=(ConnectionState &) = delete;
+    ConnectionState(ConnectionState &&) = delete;
+    ConnectionState &operator=(ConnectionState &&) = delete;
 
-	~ConnectionState(void);
+    ~ConnectionState(void);
 
-	void on_connect(std::function<void(void)> fn) {
-		on_connect_fn = fn;
-	};
+    void on_connect(std::function<void(void)> fn) { on_connect_fn = fn; };
 
-	void on_ssl(std::function<void(void)> fn) {
-		on_ssl_fn = fn;
-	};
+    void on_ssl(std::function<void(void)> fn) { on_ssl_fn = fn; };
 
-	void on_data(std::function<void(SharedPointer<Buffer>)> fn) {
-		on_data_fn = fn;
-		enable_read();
-	};
+    void on_data(std::function<void(SharedPointer<Buffer>)> fn) {
+        on_data_fn = fn;
+        enable_read();
+    };
 
-	void on_flush(std::function<void(void)> fn) {
-		on_flush_fn = fn;
-	};
+    void on_flush(std::function<void(void)> fn) { on_flush_fn = fn; };
 
-	void on_error(std::function<void(Error)> fn) {
-		on_error_fn = fn;
-	};
+    void on_error(std::function<void(Error)> fn) { on_error_fn = fn; };
 
-	evutil_socket_t get_fileno(void) {
-		return (bufferevent_getfd(this->bev));
-	}
+    evutil_socket_t get_fileno(void) { return (bufferevent_getfd(this->bev)); }
 
-	void set_timeout(double timeout) {
-		struct timeval tv, *tvp;
-		tvp = ight_timeval_init(&tv, timeout);
-		if (bufferevent_set_timeouts(this->bev, tvp, tvp) != 0) {
-			throw std::runtime_error("cannot set timeout");
-		}
-	}
+    void set_timeout(double timeout) {
+        struct timeval tv, *tvp;
+        tvp = ight_timeval_init(&tv, timeout);
+        if (bufferevent_set_timeouts(this->bev, tvp, tvp) != 0) {
+            throw std::runtime_error("cannot set timeout");
+        }
+    }
 
-	void clear_timeout(void) {
-		this->set_timeout(-1);
-	}
+    void clear_timeout(void) { this->set_timeout(-1); }
 
-	void start_tls(unsigned int) {
-		throw std::runtime_error("not implemented");
-	}
+    void start_tls(unsigned int) {
+        throw std::runtime_error("not implemented");
+    }
 
-	void send(const void *base, size_t count) {
-		if (base == NULL || count == 0) {
-			throw std::runtime_error("invalid argument");
-		}
-		if (bufferevent_write(bev, base, count) != 0) {
-			throw std::runtime_error("cannot write");
-		}
-	}
+    void send(const void *base, size_t count) {
+        if (base == NULL || count == 0) {
+            throw std::runtime_error("invalid argument");
+        }
+        if (bufferevent_write(bev, base, count) != 0) {
+            throw std::runtime_error("cannot write");
+        }
+    }
 
-	void send(std::string data) {
-		send(data.c_str(), data.length());
-	}
+    void send(std::string data) { send(data.c_str(), data.length()); }
 
-	void send(SharedPointer<Buffer> data) {
-		send(*data);
-	}
+    void send(SharedPointer<Buffer> data) { send(*data); }
 
-	void send(Buffer& data) {
-		data >> bufferevent_get_output(bev);
-	}
+    void send(Buffer &data) { data >> bufferevent_get_output(bev); }
 
-	void enable_read(void) {
-		if (bufferevent_enable(this->bev, EV_READ) != 0) {
-			throw std::runtime_error("cannot enable read");
-		}
-	}
+    void enable_read(void) {
+        if (bufferevent_enable(this->bev, EV_READ) != 0) {
+            throw std::runtime_error("cannot enable read");
+        }
+    }
 
-	void disable_read(void) {
-		if (bufferevent_disable(this->bev, EV_READ) != 0) {
-			throw std::runtime_error("cannot disable read");
-		}
-	}
+    void disable_read(void) {
+        if (bufferevent_disable(this->bev, EV_READ) != 0) {
+            throw std::runtime_error("cannot disable read");
+        }
+    }
 
-	void close(void);
+    void close(void);
 
-	void emit_connect() {
-		on_connect_fn();
-	}
+    void emit_connect() { on_connect_fn(); }
 
-	void emit_data(SharedPointer<Buffer> data) {
-		on_data_fn(data);
-	}
+    void emit_data(SharedPointer<Buffer> data) { on_data_fn(data); }
 
-	void emit_flush() {
-		on_flush_fn();
-	}
+    void emit_flush() { on_flush_fn(); }
 
-	void emit_error(Error err) {
-		on_error_fn(err);
-	}
+    void emit_error(Error err) { on_error_fn(err); }
 };
 
 class Connection : public Transport {
 
-	ConnectionState *state = NULL;
+    ConnectionState *state = NULL;
 
-    public:
+  public:
+    virtual void emit_connect() override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->emit_connect();
+    }
 
-	virtual void emit_connect() override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->emit_connect();
-	}
+    virtual void emit_data(SharedPointer<Buffer> data) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->emit_data(data);
+    }
 
-	virtual void emit_data(SharedPointer<Buffer> data) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->emit_data(data);
-	}
+    virtual void emit_flush() override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->emit_flush();
+    }
 
-	virtual void emit_flush() override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->emit_flush();
-	}
+    virtual void emit_error(Error err) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->emit_error(err);
+    }
 
-	virtual void emit_error(Error err) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->emit_error(err);
-	}
+    Connection(void) { /* nothing to do */ }
+    Connection(evutil_socket_t fd) {
+        state = new ConnectionState("PF_UNSPEC", "0.0.0.0", "0", fd);
+    }
+    Connection(const char *af, const char *a, const char *p) {
+        state = new ConnectionState(af, a, p);
+    }
 
-	Connection(void) {
-		/* nothing to do */
-	}
-	Connection(evutil_socket_t fd) {
-		state = new ConnectionState("PF_UNSPEC", "0.0.0.0",
-		    "0", fd);
-	}
-	Connection(const char *af, const char *a, const char *p) {
-		state = new ConnectionState(af, a, p);
-	}
+    virtual void close(void) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->close();
+    }
 
-	virtual void close(void) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->close();
-	}
+    virtual ~Connection();
 
-	virtual ~Connection();
+    virtual void on_connect(std::function<void(void)> fn) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->on_connect(fn);
+    };
 
-	virtual void on_connect(std::function<void(void)> fn) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->on_connect(fn);
-	};
+    virtual void on_ssl(std::function<void(void)> fn) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->on_ssl(fn);
+    };
 
-	virtual void on_ssl(std::function<void(void)> fn) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->on_ssl(fn);
-	};
+    virtual void
+    on_data(std::function<void(SharedPointer<Buffer>)> fn) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->on_data(fn);
+    };
 
-	virtual void on_data(std::function<void(
-			SharedPointer<Buffer>)> fn) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->on_data(fn);
-	};
+    virtual void on_flush(std::function<void(void)> fn) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->on_flush(fn);
+    };
 
-	virtual void on_flush(std::function<void(void)> fn) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->on_flush(fn);
-	};
+    virtual void on_error(std::function<void(Error)> fn) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->on_error(fn);
+    };
 
-	virtual void on_error(std::function<void(Error)> fn) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->on_error(fn);
-	};
+    evutil_socket_t get_fileno(void) {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        return (state->get_fileno());
+    }
 
-	evutil_socket_t get_fileno(void) {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		return (state->get_fileno());
-	}
+    virtual void set_timeout(double timeout) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->set_timeout(timeout);
+    }
 
-	virtual void set_timeout(double timeout) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->set_timeout(timeout);
-	}
+    virtual void clear_timeout(void) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->clear_timeout();
+    }
 
-	virtual void clear_timeout(void) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->clear_timeout();
-	}
+    void start_tls(unsigned int d) {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->start_tls(d);
+    }
 
-	void start_tls(unsigned int d) {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->start_tls(d);
-	}
+    virtual void send(const void *base, size_t count) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->send(base, count);
+    }
 
-	virtual void send(const void *base, size_t count) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->send(base, count);
-	}
+    virtual void send(std::string str) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->send(str);
+    }
 
-	virtual void send(std::string str) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->send(str);
-	}
+    virtual void send(SharedPointer<Buffer> sourcebuf) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->send(sourcebuf);
+    }
 
-	virtual void send(SharedPointer<Buffer> sourcebuf) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->send(sourcebuf);
-	}
+    virtual void send(Buffer &sourcebuf) override {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->send(sourcebuf);
+    }
 
-	virtual void send(Buffer& sourcebuf) override {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->send(sourcebuf);
-	}
+    void enable_read(void) {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->enable_read();
+    }
 
-	void enable_read(void) {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->enable_read();
-	}
+    void disable_read(void) {
+        if (state == NULL)
+            throw std::runtime_error("Invalid state");
+        state->disable_read();
+    }
 
-	void disable_read(void) {
-		if (state == NULL)
-			throw std::runtime_error("Invalid state");
-		state->disable_read();
-	}
+    virtual std::string socks5_address() override { return ""; /* no proxy */ }
 
-	virtual std::string socks5_address() override {
-		return "";  /* no proxy */
-	}
-
-	virtual std::string socks5_port() override {
-		return "";  /* no proxy */
-	}
+    virtual std::string socks5_port() override { return ""; /* no proxy */ }
 };
 
 }}}
--- src/common/string_vector.cpp
+++ src/common/string_vector.cpp
@@ -11,65 +11,55 @@
 
 #include <ight/common/string_vector.hpp>
 
-#define IGHT_STRINGVECTOR_MAX 512  // Large enough
+#define IGHT_STRINGVECTOR_MAX 512 // Large enough
 
 using namespace ight::common::string_vector;
 using namespace ight::common::poller;
 
-StringVector::StringVector(Poller *p, size_t cnt)
-{
-	if (p == NULL || cnt == 0 || cnt > IGHT_STRINGVECTOR_MAX)
-		throw new (std::bad_alloc);
+StringVector::StringVector(Poller *p, size_t cnt) {
+    if (p == NULL || cnt == 0 || cnt > IGHT_STRINGVECTOR_MAX)
+        throw new (std::bad_alloc);
 
-	this->base = (char **) calloc(cnt, sizeof (char *));
-	if (this->base == NULL)
-		throw new (std::bad_alloc);
+    this->base = (char **)calloc(cnt, sizeof(char *));
+    if (this->base == NULL)
+        throw new (std::bad_alloc);
 
-	this->count = cnt;
-	this->iter = 0;
-	this->pos = 0;
-	this->poller = p;
+    this->count = cnt;
+    this->iter = 0;
+    this->pos = 0;
+    this->poller = p;
 }
 
-int
-StringVector::append(const char *str)
-{
-	if (this->pos > this->count)
-		abort();
-	if (str == NULL || this->pos == this->count)
-		return (-1);
-	this->base[this->pos] = strdup(str);
-	if (this->base[this->pos] == NULL)
-		return (-1);
-	++this->pos;
-	return (0);
+int StringVector::append(const char *str) {
+    if (this->pos > this->count)
+        abort();
+    if (str == NULL || this->pos == this->count)
+        return (-1);
+    this->base[this->pos] = strdup(str);
+    if (this->base[this->pos] == NULL)
+        return (-1);
+    ++this->pos;
+    return (0);
 }
 
-Poller *
-StringVector::get_poller(void)
-{
-	return (this->poller);
+Poller *StringVector::get_poller(void) { return (this->poller); }
+
+const char *StringVector::get_next(void) {
+    if (this->iter > this->pos)
+        abort();
+    if (this->iter == this->pos)
+        return (NULL);
+    return (this->base[this->iter++]);
 }
 
-const char *
-StringVector::get_next(void)
-{
-	if (this->iter > this->pos)
-		abort();
-	if (this->iter == this->pos)
-		return (NULL);
-	return (this->base[this->iter++]);
-}
+StringVector::~StringVector(void) {
+    size_t i;
 
-StringVector::~StringVector(void)
-{
-	size_t i;
-
-	if (this->base == NULL)
-		return;
-	if (this->pos > this->count)
-		abort();
-	for (i = 0; i < this->pos; ++i)
-		free(this->base[i]);
-	free(this->base);
+    if (this->base == NULL)
+        return;
+    if (this->pos > this->count)
+        abort();
+    for (i = 0; i < this->pos; ++i)
+        free(this->base[i]);
+    free(this->base);
 }
