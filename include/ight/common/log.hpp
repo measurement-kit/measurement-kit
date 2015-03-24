b543501dd1b71db21c72477d842647f3cea9adcd
Simone Basso <bassosimone@gmail.com>
2015-03-24 16:09:27 +0100

    Implement custom logger that one can personalize

--- include/ight/common/log.hpp
+++ include/ight/common/log.hpp
@@ -7,19 +7,120 @@
 #ifndef IGHT_COMMON_LOG_HPP
 # define IGHT_COMMON_LOG_HPP
 
+#include <ight/common/constraints.hpp>
+#include <ight/common/pointer.hpp>
+
 #include <functional>
+#include <stdarg.h>
 
-void ight_warn(const char *, ...)
-  __attribute__((format(printf, 1, 2)));
+namespace ight {
+namespace common {
+namespace log {
 
-void ight_info(const char *, ...)
-  __attribute__((format(printf, 1, 2)));
+using namespace ight::common::constraints;
+using namespace ight::common::pointer;
 
-void ight_debug(const char *, ...)
-  __attribute__((format(printf, 1, 2)));
+class Logger : public NonCopyable, public NonMovable {
 
-void ight_set_verbose(int);
+  private:
+    std::function<void(const char *)> consumer;
+    int verbose = 0;
+    char buffer[32768];
 
-void ight_set_logger(std::function<void(const char *)>);
+  public:
+    Logger();
+
+    void logv(const char *fmt, va_list ap);
+
+    void warn(const char *fmt, ...) __attribute__((format(printf, 2, 3))) {
+        va_list ap;
+        va_start(ap, fmt);
+        logv(fmt, ap);
+        va_end(ap);
+    }
+
+    void info(const char *fmt, ...) __attribute__((format(printf, 2, 3))) {
+        if (verbose) {
+            va_list ap;
+            va_start(ap, fmt);
+            logv(fmt, ap);
+            va_end(ap);
+        }
+    }
+
+    void debug(const char *fmt, ...) __attribute__((format(printf, 2, 3))) {
+        if (verbose) {
+            va_list ap;
+            va_start(ap, fmt);
+            logv(fmt, ap);
+            va_end(ap);
+        }
+    }
+
+    void set_verbose(int v) {
+        verbose = v;
+    }
+
+    int is_verbose() {
+        return verbose;
+    }
+
+    void set_logger(std::function<void(const char *)> fn) {
+        consumer = fn;
+    }
+};
+
+inline SharedPointer<Logger> make_logger() {
+    return SharedPointer<Logger>{new Logger};
+}
+
+struct DefaultLogger {
+    static SharedPointer<Logger> get() {
+        static SharedPointer<Logger> singleton = make_logger();
+        return singleton;
+    }
+};
+
+}}}
+
+inline void ight_warn(const char *, ...) __attribute__((format(printf, 1, 2)));
+inline void ight_debug(const char *, ...) __attribute__((format(printf, 1, 2)));
+inline void ight_info(const char *, ...) __attribute__((format(printf, 1, 2)));
+
+inline void ight_warn(const char *fmt, ...) {
+    auto logger = ight::common::log::DefaultLogger::get();
+    va_list ap;
+    va_start(ap, fmt);
+    logger->logv(fmt, ap);
+    va_end(ap);
+}
+
+inline void ight_info(const char *fmt, ...) {
+    auto logger = ight::common::log::DefaultLogger::get();
+    if (logger->is_verbose()) {
+        va_list ap;
+        va_start(ap, fmt);
+        logger->logv(fmt, ap);
+        va_end(ap);
+    }
+}
+
+inline void ight_debug(const char *fmt, ...) {
+    auto logger = ight::common::log::DefaultLogger::get();
+    if (logger->is_verbose()) {
+        va_list ap;
+        va_start(ap, fmt);
+        logger->logv(fmt, ap);
+        va_end(ap);
+    }
+}
+
+inline void ight_set_verbose(int v) {
+    ight::common::log::DefaultLogger::get()->set_verbose(v);
+}
+
+inline void ight_set_logger(std::function<void(const char *)> fn) {
+    ight::common::log::DefaultLogger::get()->set_logger(fn);
+}
 
 #endif
--- src/common/log.cpp
+++ src/common/log.cpp
@@ -5,86 +5,27 @@
  * information on the copying conditions.
  */
 
-#include <stdarg.h>
-#include <stdio.h>
-
 #include <ight/common/log.hpp>
 
-class Logger {
+#include <stdio.h>
 
-    char buffer[32768];
-    int verbose_ = 0;
-    std::function<void(const char *)> consumer = [](const char *s) {
+using namespace ight::common::log;
+
+Logger::Logger() {
+    consumer = [](const char *s) {
         fprintf(stderr, "%s\n", s);
     };
-
-    Logger() {}  /* We use singleton pattern */
-
-public:
-    static Logger& get() {
-        static Logger singleton;
-        return singleton;
-    }
-
-    void set_verbose(int v) {
-        verbose_ = v;
-    }
-
-    int verbose() {
-        return verbose_;
-    }
-
-    void set_logger(std::function<void(const char *)> fn) {
-        consumer = fn;
-    }
-
-    void log(const char *fmt, va_list ap) {
-        int res = vsnprintf(buffer, sizeof (buffer), fmt, ap);
-        /*
-         * Once we know that res is non-negative we make it unsigned,
-         * which allows the compiler to promote the smaller of res and
-         * sizeof (buffer) to the correct size if needed.
-         */
-        if (res < 0 || (unsigned int) res >= sizeof (buffer)) {
-            return;
-        }
-        consumer(buffer);
-    }
-};
-
-void ight_warn(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    Logger::get().log(fmt, ap);
-    va_end(ap);
 }
 
-void ight_info(const char *fmt, ...) {
-    auto logger = Logger::get();
-    if (!logger.verbose()) {
+void Logger::logv(const char *fmt, va_list ap) {
+    int res = vsnprintf(buffer, sizeof (buffer), fmt, ap);
+    /*
+     * Once we know that res is non-negative we make it unsigned,
+     * which allows the compiler to promote the smaller of res and
+     * sizeof (buffer) to the correct size if needed.
+     */
+    if (res < 0 || (unsigned int) res >= sizeof (buffer)) {
         return;
     }
-    va_list ap;
-    va_start(ap, fmt);
-    logger.log(fmt, ap);
-    va_end(ap);
-}
-
-void ight_debug(const char *fmt, ...) {
-    auto logger = Logger::get();
-    if (!logger.verbose()) {
-        return;
-    }
-    va_list ap;
-    va_start(ap, fmt);
-    logger.log(fmt, ap);
-    va_end(ap);
-}
-
-void ight_set_verbose(int v) {
-    Logger::get().set_verbose(v);
-}
-
-void ight_set_logger(std::function<void(const char *)> fn) {
-    Logger::get().set_logger(fn);
+    consumer(buffer);
 }
