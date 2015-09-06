c43e8dfb9877e313f5768bbf3f088a42642ec17c
Simone Basso <bassosimone@gmail.com>
2015-09-06 11:05:33 +0200

    Split http test code

---
+++ test/http/request.cpp
@@ -0,0 +1,258 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+//
+// Regression tests for `protocols/http.hpp` and `protocols/http.cpp`.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "src/ext/Catch/single_include/catch.hpp"
+
+#include <measurement_kit/common.hpp>
+#include <measurement_kit/http.hpp>
+
+using namespace measurement_kit::common;
+using namespace measurement_kit::net;
+using namespace measurement_kit::http;
+
+TEST_CASE("HTTP Request works as expected") {
+    //measurement_kit::set_verbose(1);
+    Request r({
+        {"url", "http://www.google.com/robots.txt"},
+        {"method", "GET"},
+        {"http_version", "HTTP/1.1"},
+    }, {
+        {"Accept", "*/*"},
+    }, "", [&](Error error, Response&& response) {
+        if (error != 0) {
+            std::cout << "Error: " << (int) error << "\r\n";
+            measurement_kit::break_loop();
+            return;
+        }
+        std::cout << "HTTP/" << response.http_major << "."
+                << response.http_minor << " " << response.status_code
+                << " " << response.reason << "\r\n";
+        for (auto& kv : response.headers) {
+            std::cout << kv.first << ": " << kv.second << "\r\n";
+        }
+        std::cout << "\r\n";
+        std::cout << response.body.read(128) << "\r\n";
+        std::cout << "[snip]\r\n";
+        measurement_kit::break_loop();
+    });
+    measurement_kit::loop();
+}
+
+TEST_CASE("HTTP request behaves correctly when EOF indicates body END") {
+    //measurement_kit::set_verbose(1);
+
+    auto called = 0;
+
+    //
+    // TODO: find a way to prevent a connection to nexa.polito.it when
+    // this test run, possibly creating a stub for connect() just as
+    // we created stubs for many libevent APIs.
+    //
+
+    Request r({
+        {"url", "http://nexa.polito.it/"},
+        {"method", "GET"},
+        {"http_version", "HTTP/1.1"},
+    }, {
+        {"Accept", "*/*"},
+    }, "", [&called](Error, Response&&) {
+        ++called;
+    });
+
+    auto stream = r.get_stream();
+    auto transport = stream->get_transport();
+
+    transport.emit_connect();
+
+    Buffer data;
+    data << "HTTP/1.1 200 Ok\r\n";
+    data << "Content-Type: text/plain\r\n";
+    data << "Connection: close\r\n";
+    data << "Server: Antani/1.0.0.0\r\n";
+    data << "\r\n";
+    data << "1234567";
+    transport.emit_data(data);
+    transport.emit_error(0);
+
+    REQUIRE(called == 1);
+}
+
+TEST_CASE("HTTP Request correctly receives errors") {
+    measurement_kit::set_verbose(1);
+    Request r({
+        {"url", "http://nexa.polito.it:81/robots.txt"},
+        {"method", "GET"},
+        {"http_version", "HTTP/1.1"},
+        {"timeout", "3.0"},
+    }, {
+        {"Accept", "*/*"},
+    }, "", [&](Error error, Response&& response) {
+        if (error != 0) {
+            std::cout << "Error: " << (int) error << "\r\n";
+            measurement_kit::break_loop();
+            return;
+        }
+        std::cout << "HTTP/" << response.http_major << "."
+                << response.http_minor << " " << response.status_code
+                << " " << response.reason << "\r\n";
+        for (auto& kv : response.headers) {
+            std::cout << kv.first << ": " << kv.second << "\r\n";
+        }
+        std::cout << "\r\n";
+        std::cout << response.body.read(128) << "\r\n";
+        std::cout << "[snip]\r\n";
+        measurement_kit::break_loop();
+    });
+    measurement_kit::loop();
+}
+
+TEST_CASE("HTTP Request works as expected over Tor") {
+    measurement_kit::set_verbose(1);
+    Request r({
+        {"url", "http://www.google.com/robots.txt"},
+        {"method", "GET"},
+        {"http_version", "HTTP/1.1"},
+        {"socks5_proxy", "127.0.0.1:9050"},
+    }, {
+        {"Accept", "*/*"},
+    }, "", [&](Error error, Response&& response) {
+        if (error != 0) {
+            std::cout << "Error: " << (int) error << "\r\n";
+            measurement_kit::break_loop();
+            return;
+        }
+        std::cout << "HTTP/" << response.http_major << "."
+                << response.http_minor << " " << response.status_code
+                << " " << response.reason << "\r\n";
+        for (auto& kv : response.headers) {
+            std::cout << kv.first << ": " << kv.second << "\r\n";
+        }
+        std::cout << "\r\n";
+        std::cout << response.body.read(128) << "\r\n";
+        std::cout << "[snip]\r\n";
+        measurement_kit::break_loop();
+    });
+    measurement_kit::loop();
+}
+
+TEST_CASE("Behavior is correct when only tor_socks_port is specified") {
+    //measurement_kit::set_verbose(1);
+
+    Settings settings{
+        {"method", "POST"},
+        {"http_version", "HTTP/1.1"},
+        {"tor_socks_port", "9055"},
+    };
+
+    settings["url"] = "httpo://nkvphnp3p6agi5qq.onion/bouncer";
+    Request r1{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    settings["url"] = "http://ooni.torproject.org/";
+    Request r2{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    REQUIRE(r1.socks5_address() == "127.0.0.1");
+    REQUIRE(r1.socks5_port() == "9055");
+    REQUIRE(r2.socks5_address() == "");
+    REQUIRE(r2.socks5_port() == "");
+}
+
+TEST_CASE("Behavior is correct with both tor_socks_port and socks5_proxy") {
+    //measurement_kit::set_verbose(1);
+
+    Settings settings{
+        {"method", "POST"},
+        {"http_version", "HTTP/1.1"},
+        {"tor_socks_port", "9999"},
+        {"socks5_proxy", "127.0.0.1:9055"},
+    };
+
+    settings["url"] = "httpo://nkvphnp3p6agi5qq.onion/bouncer";
+    Request r1{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    settings["url"] = "http://ooni.torproject.org/";
+    Request r2{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    REQUIRE(r1.socks5_address() == "127.0.0.1");
+    REQUIRE(r1.socks5_port() == "9999");
+    REQUIRE(r2.socks5_address() == "127.0.0.1");
+    REQUIRE(r2.socks5_port() == "9055");
+}
+
+TEST_CASE("Behavior is corrent when only socks5_proxy is specified") {
+    //measurement_kit::set_verbose(1);
+
+    Settings settings{
+        {"method", "POST"},
+        {"http_version", "HTTP/1.1"},
+        {"socks5_proxy", "127.0.0.1:9055"},
+    };
+
+    settings["url"] = "httpo://nkvphnp3p6agi5qq.onion/bouncer";
+    Request r1{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    settings["url"] = "http://ooni.torproject.org/";
+    Request r2{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    REQUIRE(r1.socks5_address() == "127.0.0.1");
+    REQUIRE(r1.socks5_port() == "9055");
+    REQUIRE(r2.socks5_address() == "127.0.0.1");
+    REQUIRE(r2.socks5_port() == "9055");
+}
+
+TEST_CASE("Behavior is OK w/o tor_socks_port and socks5_proxy") {
+    //measurement_kit::set_verbose(1);
+
+    Settings settings{
+        {"method", "POST"},
+        {"http_version", "HTTP/1.1"},
+    };
+
+    settings["url"] = "httpo://nkvphnp3p6agi5qq.onion/bouncer";
+    Request r1{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    settings["url"] = "http://ooni.torproject.org/";
+    Request r2{settings, {
+        {"Accept", "*/*"},
+    }, "{\"test-helpers\": [\"dns\"]}", [](Error, Response&&) {
+        /* nothing */
+    }};
+
+    REQUIRE(r1.socks5_address() == "127.0.0.1");
+    REQUIRE(r1.socks5_port() == "9050");
+    REQUIRE(r2.socks5_address() == "");
+    REQUIRE(r2.socks5_port() == "");
+}
---
+++ test/http/response_parser.cpp
@@ -0,0 +1,185 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#define CATCH_CONFIG_MAIN
+#include "src/ext/Catch/single_include/catch.hpp"
+
+#include <measurement_kit/common.hpp>
+#include <measurement_kit/http.hpp>
+
+using namespace measurement_kit::common;
+using namespace measurement_kit::net;
+using namespace measurement_kit::http;
+
+TEST_CASE("We don't leak when we receive an invalid message") {
+
+    //measurement_kit::set_verbose(1);
+
+    ResponseParser parser;
+    std::string data;
+
+    //
+    // We pass the parser an invalid message that should trigger an
+    // exception. After the exception, the internal parser should be
+    // in a state by which, when the external parser is deleted,
+    // the internal parser is also deleted.
+    //
+
+    data = "";
+    data += "XXX XXX XXX XXX\r\n";
+    data += "Content-Type: text/plain\r\n";
+    data += "Connection: close\r\n";
+    data += "Server: Antani/1.0.0.0\r\n";
+    data += "\r\n";
+    data += "1234567";
+
+    REQUIRE_THROWS(parser.feed(data));
+}
+
+TEST_CASE("We don't leak when we receive a UPGRADE") {
+
+    //measurement_kit::set_verbose(1);
+
+    ResponseParser parser;
+    std::string data;
+
+    //
+    // We pass the parser an invalid message that should trigger an
+    // exception. After the exception, the internal parser should be
+    // in a state by which, when the external parser is deleted,
+    // the internal parser is also deleted.
+    //
+
+    data = "";
+    data += "HTTP/1.1 200 Ok\r\n";
+    data += "Content-Type: text/plain\r\n";
+    data += "Connection: upgrade\r\n";
+    data += "Upgrade: websockets\r\n";
+    data += "Server: Antani/1.0.0.0\r\n";
+    data += "\r\n";
+
+    REQUIRE_THROWS(parser.feed(data));
+}
+
+TEST_CASE("The HTTP response parser works as expected") {
+    auto data = std::string();
+    auto parser = ResponseParser();
+    auto body = std::string();
+
+    /*measurement_kit::set_verbose(1);*/
+
+    //
+    // Request #1
+    //
+
+    data = "";
+    data += "HTTP/1.1 200 Ok\r\n";
+    data += "Content-Type: text/plain\r\n";
+    data += "Content-Length: 7\r\n";
+    data += "Server: Antani/1.0.0.0\r\n";
+    data += "\r\n";
+    data += "1234567";
+
+    parser.on_headers_complete([](unsigned short major, unsigned short minor,
+            unsigned int code, std::string&& reason, std::map<std::string,
+            std::string>&& headers) {
+        REQUIRE(major == 1);
+        REQUIRE(minor == 1);
+        REQUIRE(code == 200);
+        REQUIRE(reason == "Ok");
+        REQUIRE(headers.size() == 3);
+        REQUIRE(headers.at("Content-Type") == "text/plain");
+        REQUIRE(headers.at("Content-Length") == "7");
+        REQUIRE(headers.at("Server") == "Antani/1.0.0.0");
+    });
+
+    body = "";
+    parser.on_body([&](std::string s) {
+        body += s;
+    });
+
+    parser.on_end([&](void) {
+        REQUIRE(body == "1234567");
+    });
+
+    for (auto c : data) {
+        measurement_kit::debug("%c\n", c);
+        parser.feed(c);
+    }
+
+    //
+    // Request #2
+    //
+
+    data = "";
+    data += "HTTP/1.1 202 Accepted\r\n";
+    data += "Content-Type: text/html\r\n";
+    data += "Transfer-Encoding: chunked\r\n";
+    data += "Server: Antani/2.0.0.0\r\n";
+    data += "\r\n";
+    data += "3\r\nabc\r\n";
+    data += "3\r\nabc\r\n";
+    data += "3\r\nabc\r\n";
+    data += "0\r\nX-Trailer: trailer\r\n\r\n";
+
+    parser.on_headers_complete([](unsigned short major, unsigned short minor,
+            unsigned int code, std::string&& reason, std::map<std::string,
+            std::string>&& headers) {
+        REQUIRE(major == 1);
+        REQUIRE(minor == 1);
+        REQUIRE(code == 202);
+        REQUIRE(reason == "Accepted");
+        REQUIRE(headers.size() == 3);
+        REQUIRE(headers.at("Content-Type") == "text/html");
+        REQUIRE(headers.at("Transfer-Encoding") == "chunked");
+        REQUIRE(headers.at("Server") == "Antani/2.0.0.0");
+    });
+
+    body = "";
+    parser.on_body([&](std::string s) {
+        body += s;
+    });
+
+    parser.on_end([&](void) {
+        REQUIRE(body == "abcabcabc");
+    });
+
+    for (auto c : data) {
+        measurement_kit::debug("%c\n", c);
+        parser.feed(c);
+    }
+}
+
+TEST_CASE("Response parser eof() does not trigger immediate distruction") {
+
+    //
+    // Provide input data by which the end of body is signalled by
+    // the connection being closed, such that invoking the parser's
+    // eof() method is goind to trigger the on_end() callback.
+    //
+    // Then, in the callback delete the parser object, which should
+    // not trigger a crash because the real parser should understand
+    // that it is parsing and should delay its destruction.
+    //
+
+    //measurement_kit::set_verbose(1);
+
+    auto parser = new ResponseParser();
+    std::string data;
+
+    data = "";
+    data += "HTTP/1.1 200 Ok\r\n";
+    data += "Content-Type: text/plain\r\n";
+    data += "Connection: close\r\n";
+    data += "Server: Antani/1.0.0.0\r\n";
+    data += "\r\n";
+    data += "1234567";
+
+    parser->feed(data);
+
+    parser->on_end([parser]() {
+        delete parser;
+    });
+    parser->eof();
+}
