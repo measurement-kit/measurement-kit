7656a395578d80c8f629275db0aadffe3c664097
Simone Basso <bassosimone@gmail.com>
2015-10-01 15:23:05 +0200

    Add basic Tor controller

---
+++ include/measurement_kit/tor.hpp
@@ -0,0 +1,10 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#ifndef MEASUREMENT_KIT_TOR_HPP
+#define MEASUREMENT_KIT_TOR_HPP
+
+#include <measurement_kit/tor/control.hpp>
+
+#endif
