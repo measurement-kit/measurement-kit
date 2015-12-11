ee3365802b6793b971a8c6e088fe1d4938574eea
Simone Basso <bassosimone@gmail.com>
2015-12-11 15:21:02 +0100

    poller: allow to modify default evdns-base state
    I need this in the mobile applications. For example, with Android
    there is no /etc/resolv.conf, and I need a mechanism to specify the
    name servers to be used by the default evdns-base instance.
    
    Note that in MeasurementKit there is also a custom Resolver object
    constructed with default parameters, but this is not the same as the
    evdns-base instance used by the Poller. The latter is, in fact, the
    instance actually used when running, e.g., OONI tests.
    
    More can be done to improve MeasurementKit with respect to the
    poller and resolver facilities used when running tests, but this
    is not the right moment to do so. We are close to a stable release
    and hence we should make as minimal changes as possible.

--- include/measurement_kit/common/poller.hpp
+++ include/measurement_kit/common/poller.hpp
@@ -8,6 +8,8 @@
 #include <measurement_kit/common/constraints.hpp>
 #include <measurement_kit/common/libs.hpp>
 
+#include <string>
+
 struct evdns_base;
 struct event_base;
 
@@ -27,6 +29,13 @@ class Poller : public NonCopyable, public NonMovable {
     void loop_once();
 
     void break_loop();
+
+    /// Get number of configured nameservers in default resolver
+    int count_nameservers();
+
+    /// Add nameserver to the list of name-servers of the default resolver
+    /// \param address Address and optionally port (e.g. "8.8.8.8:53")
+    void add_nameserver(std::string address);
 
     static Poller *global() {
         static Poller singleton;
@@ -60,6 +69,12 @@ inline void loop(void) { Poller::global()->loop(); }
 inline void loop_once(void) { Poller::global()->loop_once(); }
 
 inline void break_loop(void) { Poller::global()->break_loop(); }
+
+inline int count_nameservers() { return Poller::global()->count_nameservers(); }
+
+inline void add_nameserver(std::string address) {
+    Poller::global()->add_nameserver(address);
+}
 
 } // namespace mk
 #endif
