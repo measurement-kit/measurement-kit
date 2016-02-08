a6166a60436b70855f8f71b078987649db24318d
Simone Basso <bassosimone@gmail.com>
2016-02-08 21:13:57 +0100

    Make sure we use correct repository
    While there also remove entries from `scripts/update-deps.sh`.

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -11,7 +11,6 @@ get() {
 }
 
 rm -rf tmp/*
-get akheron/jansson v2.7-52-ge44b223 jansson
 get boostorg/assert boost-1.59.0 boost/assert
 get boostorg/config boost-1.59.0 boost/config
 get boostorg/core boost-1.59.0-8-g3add966 boost/core
@@ -28,7 +27,5 @@ get boostorg/typeof boost-1.59.0 boost/typeof
 get boostorg/utility boost-1.59.0 boost/utility
 get jbeder/yaml-cpp release-0.5.2-16-g97d56c3 yaml-cpp
 get joyent/http-parser v2.6.0 http-parser
-get measurement-kit/libevent patches-2.0 libevent patches-2.0
-get measurement-kit/libmaxminddb master libmaxminddb
 get philsquared/Catch v1.2.1 Catch
 rm -rf tmp
