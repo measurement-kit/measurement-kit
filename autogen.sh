0de2b18b8cb2ade43e590cf84c590581371ffc88
Simone Basso <bassosimone@gmail.com>
2016-03-18 01:58:36 +0100

    autogen.sh: automatically update .gitignore

--- autogen.sh
+++ autogen.sh
@@ -49,6 +49,7 @@ gen_executables() {
                 echo "if $3"
                 echo "    $1 += $2/$bin_name"
                 echo "endif"
+                echo "$2/$bin_name" >> .gitignore
                 echo "$(slug $2/$bin_name)_SOURCES = $2/$name"
                 echo "$(slug $2/$bin_name)_LDADD = libmeasurement_kit.la"
             fi
@@ -80,6 +81,10 @@ echo ""                                                >> include.am
 gen_headers include/measurement_kit                    >> include.am
 gen_executables noinst_PROGRAMS example BUILD_EXAMPLES >> include.am
 gen_executables ALL_TESTS test BUILD_TESTS             >> include.am
+
+echo "* Updating .gitignore"
+sort -u .gitignore > .gitignore.new
+mv .gitignore.new .gitignore
 
 echo "* Fetching dependencies"
 get akheron/jansson v2.7-52-ge44b223 jansson
