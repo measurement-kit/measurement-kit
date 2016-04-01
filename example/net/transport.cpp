f2f349f451889d633ff3c0cccfb7f11ba9e40209
Simone Basso <bassosimone@gmail.com>
2016-04-01 02:04:37 +0200

    example: add net/transport+socks5 example

---
+++ example/net/transport.cpp
@@ -0,0 +1,108 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#include <functional>
+#include <iostream>
+#include <measurement_kit/common.hpp>
+#include <measurement_kit/http.hpp>
+#include <measurement_kit/net.hpp>
+#include <regex>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+
+using namespace mk;
+using namespace mk::net;
+
+static const char *kv_usage =
+        "usage: ./example/net/transport [-v] [-P address:port] url\n";
+
+static void print_line(std::string line) {
+    std::string s;
+    for (auto chr : line) {
+        if (chr == '\r' || chr == '\n') {
+            break;
+        }
+        s += chr;
+    }
+    debug("< %s", s.c_str());
+}
+
+int main(int argc, char **argv) {
+
+    Settings settings;
+    char ch;
+    while ((ch = getopt(argc, argv, "P:v")) != -1) {
+        switch (ch) {
+        case 'P':
+            settings["socks5_proxy"] = optarg;
+            break;
+        case 'v':
+            set_verbose(1);
+            break;
+        default:
+            std::cout << kv_usage;
+            exit(1);
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (argc != 1) {
+        std::cout << kv_usage;
+        exit(1);
+    }
+    http::Url url = http::parse_url(argv[0]);
+
+    loop_with_initial_event([&]() {
+        connect(url.address, url.port, [&](Error error, Var<Transport> tx) {
+            if (error) {
+                debug("* error: %d", (int)error);
+                break_loop();
+                return;
+            }
+            Var<Buffer> incoming(new Buffer);
+            Var<bool> reading_meta(new bool(true));
+            tx->set_timeout(10);
+            tx->send("GET " + url.pathquery + " HTTP/1.0\r\n");
+            debug("> GET %s HTTP/1.0", url.pathquery.c_str());
+            tx->send("Accept: */*\r\n");
+            debug("> Accept: */*");
+            tx->send("Connection: close\r\n");
+            debug("> Connection: close");
+            tx->send("Host: " + url.address + "\r\n");
+            debug("> Host: %s", url.address.c_str());
+            tx->send("\r\n");
+            debug(">");
+            tx->on_error([=](Error error) {
+                if (error != EOFError()) {
+                    debug("* error: %d", (int)error);
+                } else {
+                    debug("* EOF");
+                }
+                tx->close();
+                break_loop();
+            });
+            tx->on_data([=](Buffer data) {
+                *incoming << data;
+                while (*reading_meta) {
+                    ErrorOr<std::string> line = incoming->readline(1024);
+                    if (!line) {
+                        tx->emit_error(line.as_error());
+                        return;
+                    }
+                    if (*line == "") {
+                        return;
+                    }
+                    print_line(*line);
+                    if (*line == "\r\n" || *line == "\n") {
+                        *reading_meta = false;
+                        break;
+                    }
+                }
+                debug("<+%llu-bytes", (unsigned long long)incoming->length());
+            });
+        }, settings);
+    });
+}
