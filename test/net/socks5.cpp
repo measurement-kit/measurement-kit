d937a725fdd96d46bb2f835b776f04780d1a52b2
Simone Basso <bassosimone@gmail.com>
2016-04-02 15:09:45 +0200

    socks5: attach to already established connection
    This diff could introduce memory leaks when/if Var<Transport> is
    captured inside of Socks5 code. We should check.
    
    With this diff in tree, we can concentrate on refactoring HTTP code
    using a connect() function. Once that piece of work is done, it's then
    possible to remove the old connect code from Connection.
    
    This diff also adds an example program that could be used to test
    transport in general and socks5 support in particular.
    
    This diff also does s/socks5::/socks_/g because creating a socks5
    subnamespace for a bunch of functions was over engineering.

--- test/net/socks5.cpp
+++ test/net/socks5.cpp
@@ -12,7 +12,7 @@ using namespace mk;
 using namespace mk::net;
 
 TEST_CASE("format_auth_request() works as expected") {
-    Buffer buffer = socks5::format_auth_request();
+    Buffer buffer = socks5_format_auth_request();
     REQUIRE(buffer.length() == 3);
     std::string message = buffer.read();
     REQUIRE(message[0] == '\5');
@@ -24,7 +24,7 @@ TEST_CASE("parse_auth_response() works as expected") {
 
     SECTION("When there is no input at all") {
         Buffer input;
-        ErrorOr<bool> rc = socks5::parse_auth_response(input);
+        ErrorOr<bool> rc = socks5_parse_auth_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == false);
     }
@@ -32,7 +32,7 @@ TEST_CASE("parse_auth_response() works as expected") {
     SECTION("When there is just one byte of data") {
         Buffer input;
         input.write_uint8(5);
-        ErrorOr<bool> rc = socks5::parse_auth_response(input);
+        ErrorOr<bool> rc = socks5_parse_auth_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == false);
     }
@@ -41,7 +41,7 @@ TEST_CASE("parse_auth_response() works as expected") {
         Buffer input;
         input.write_uint8(4);
         input.write_uint8(0);
-        ErrorOr<bool> rc = socks5::parse_auth_response(input);
+        ErrorOr<bool> rc = socks5_parse_auth_response(input);
         REQUIRE(rc.as_error() == BadSocksVersionError());
         REQUIRE_THROWS_AS(rc.as_value(), Error);
     }
@@ -50,7 +50,7 @@ TEST_CASE("parse_auth_response() works as expected") {
         Buffer input;
         input.write_uint8(5);
         input.write_uint8(16);
-        ErrorOr<bool> rc = socks5::parse_auth_response(input);
+        ErrorOr<bool> rc = socks5_parse_auth_response(input);
         REQUIRE(rc.as_error() == NoAvailableSocksAuthenticationError());
         REQUIRE_THROWS_AS(rc.as_value(), Error);
     }
@@ -59,7 +59,7 @@ TEST_CASE("parse_auth_response() works as expected") {
         Buffer input;
         input.write_uint8(5);
         input.write_uint8(0);
-        ErrorOr<bool> rc = socks5::parse_auth_response(input);
+        ErrorOr<bool> rc = socks5_parse_auth_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == true);
     }
@@ -67,7 +67,7 @@ TEST_CASE("parse_auth_response() works as expected") {
 
 TEST_CASE("format_connect_request() works as expected") {
     SECTION("When the address is too long") {
-        ErrorOr<Buffer> rc = socks5::format_connect_request({
+        ErrorOr<Buffer> rc = socks5_format_connect_request({
             {"address", std::string(1024, 'A')},
         });
         REQUIRE(rc.as_error() == SocksAddressTooLongError());
@@ -75,7 +75,7 @@ TEST_CASE("format_connect_request() works as expected") {
     }
 
     SECTION("When the port number is negative") {
-        ErrorOr<Buffer> rc = socks5::format_connect_request({
+        ErrorOr<Buffer> rc = socks5_format_connect_request({
             {"address", "130.192.91.211"},
             {"port", -1},
         });
@@ -84,7 +84,7 @@ TEST_CASE("format_connect_request() works as expected") {
     }
 
     SECTION("When the port number is too large") {
-        ErrorOr<Buffer> rc = socks5::format_connect_request({
+        ErrorOr<Buffer> rc = socks5_format_connect_request({
             {"address", "130.192.91.211"},
             {"port", 65536},
         });
@@ -95,7 +95,7 @@ TEST_CASE("format_connect_request() works as expected") {
     SECTION("When input is OK") {
         std::string address = "130.192.91.211";
         uint16_t orig_port = 8080;
-        ErrorOr<Buffer> rc = socks5::format_connect_request({
+        ErrorOr<Buffer> rc = socks5_format_connect_request({
             {"address", address},
             {"port", orig_port},
         });
@@ -117,7 +117,7 @@ TEST_CASE("parse_connect_response() works as expected") {
 
     SECTION("When there are less than five bytes of input") {
         Buffer input("ABCD");
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == false);
     }
@@ -129,7 +129,7 @@ TEST_CASE("parse_connect_response() works as expected") {
         input.write_uint8(0);
         input.write_uint8(3);
         input.write_uint8(0);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == BadSocksVersionError());
         REQUIRE_THROWS_AS(rc.as_value(), Error);
     }
@@ -141,7 +141,7 @@ TEST_CASE("parse_connect_response() works as expected") {
         input.write_uint8(0);
         input.write_uint8(3);
         input.write_uint8(0);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == SocksError());
         REQUIRE_THROWS_AS(rc.as_value(), Error);
     }
@@ -153,7 +153,7 @@ TEST_CASE("parse_connect_response() works as expected") {
         input.write_uint8(1);
         input.write_uint8(3);
         input.write_uint8(0);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == BadSocksReservedFieldError());
         REQUIRE_THROWS_AS(rc.as_value(), Error);
     }
@@ -165,7 +165,7 @@ TEST_CASE("parse_connect_response() works as expected") {
         input.write_uint8(0);
         input.write_uint8(44);
         input.write_uint8(0);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == BadSocksAtypeValueError());
         REQUIRE_THROWS_AS(rc.as_value(), Error);
     }
@@ -177,7 +177,7 @@ TEST_CASE("parse_connect_response() works as expected") {
         input.write_uint8(0);
         input.write_uint8(3);
         input.write_uint8(6);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == false);
     }
@@ -195,7 +195,7 @@ TEST_CASE("parse_connect_response() works as expected") {
         input.write_uint8(0);
         // </IPv4>
         input.write_uint16(8080);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == true);
         REQUIRE(input.length() == 0);
@@ -216,7 +216,7 @@ TEST_CASE("parse_connect_response() works as expected") {
         input.write_uint8('g');
         // </len+string>
         input.write_uint16(8080);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == true);
         REQUIRE(input.length() == 0);
@@ -239,7 +239,7 @@ TEST_CASE("parse_connect_response() works as expected") {
                 input.write_uint8(0);
         // </IPv6>
         input.write_uint16(8080);
-        ErrorOr<bool> rc = socks5::parse_connect_response(input);
+        ErrorOr<bool> rc = socks5_parse_connect_response(input);
         REQUIRE(rc.as_error() == NoError());
         REQUIRE(rc.as_value() == true);
         REQUIRE(input.length() == 0);
