f2be2a2fa7ace390d71f0713b70ed1ed922613e7
Simone Basso <bassosimone@gmail.com>
2016-04-14 17:37:03 +0200

    Implement connect_many() required by NDT
    There is a case where the NDT client could open more than one
    connection to the remote server. Add code in `net` to allow to
    implement that use case. Also add regress tests.

---
+++ src/net/connect-many.cpp
@@ -0,0 +1,18 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#include "src/net/connect-many.hpp"
+
+namespace mk {
+namespace net {
+
+void connect_many(std::string address, int port, int num,
+        ConnectManyCb callback, Settings settings, Logger *logger,
+        Poller *poller) {
+    connect_many_<net::connect>(connect_many_make(address, port, num,
+            callback, settings, logger, poller));
+}
+
+} // namespace net
+} // namespace mk
