61697ebf10400f59cd03196e214bdfe89d1f3845
AntonioLangiu <antonio.langiu@studenti.polito.it>
2016-04-20 14:01:04 +0200

    apply clang-format

--- src/ooni/utils.hpp
+++ src/ooni/utils.hpp
@@ -5,16 +5,14 @@
 #ifndef SRC_OONI_UTILS_HPP
 #define SRC_OONI_UTILS_HPP
 
+#include "src/ext/json/src/json.hpp"
+#include <GeoIP.h>
 #include <measurement_kit/common.hpp>
 #include <measurement_kit/http.hpp>
 #include <regex>
 #include <string>
-#include "src/ext/json/src/json.hpp"
-#include <GeoIP.h>
-
 
 using Json = nlohmann::json;
-
 
 namespace mk {
 namespace ooni {
@@ -22,32 +20,33 @@ namespace ooni {
 template <decltype(mk::http::get) httpget = mk::http::get>
 void ip_lookup(Callback<std::string> callback) {
     httpget("http://geoip.ubuntu.com/lookup",
-              [=](Error err, http::Response response) {
-                  if (err) {
-                      callback(err, "");
-                      return;
-                  }
-                  if (response.status_code != 200) {
-                      callback(GenericError(), "");
-                      return;
-                  }
-                  std::smatch m;
-                  std::regex regex("<Ip>(.*)</Ip>");
-                  if (std::regex_search(response.body, m, regex) == false) {
-                      callback(GenericError(), "");
-                      return;
-                  }
-                  callback(NoError(), m[1]);
-              }, {}, "", {}, Logger::global(), Poller::global());
+            [=](Error err, http::Response response) {
+                if (err) {
+                    callback(err, "");
+                    return;
+                }
+                if (response.status_code != 200) {
+                    callback(GenericError(), "");
+                    return;
+                }
+                std::smatch m;
+                std::regex regex("<Ip>(.*)</Ip>");
+                if (std::regex_search(response.body, m, regex) == false) {
+                    callback(GenericError(), "");
+                    return;
+                }
+                callback(NoError(), m[1]);
+            },
+            {}, "", {}, Logger::global(), Poller::global());
 }
 
 // this is needed to return a const char instead of a char for asn resolver
-inline GEOIP_API const char * MK_GeoIP_name_by_name_gl(GeoIP * gi, const char *host,
-                                              GeoIPLookup * gl) {
-    return (const char *) GeoIP_name_by_name_gl(gi,host,gl);
+inline GEOIP_API const char *
+MK_GeoIP_name_by_name_gl(GeoIP *gi, const char *host, GeoIPLookup *gl) {
+    return (const char *)GeoIP_name_by_name_gl(gi, host, gl);
 }
 
-template<decltype(GeoIP_country_code3_by_name_gl) resolver>
+template <decltype(GeoIP_country_code3_by_name_gl) resolver>
 std::string geoip_query(std::string ip, std::string path) {
     GeoIP *gi;
     GeoIPLookup gl;
@@ -64,7 +63,8 @@ std::string geoip_query(std::string ip, std::string path) {
     return std::string(result);
 }
 
-inline ErrorOr<Json> geoip(std::string ip, std::string path_country, std::string path_asn) {
+inline ErrorOr<Json> geoip(std::string ip, std::string path_country,
+                           std::string path_asn) {
     Json json;
     std::string result;
 
@@ -72,18 +72,18 @@ inline ErrorOr<Json> geoip(std::string ip, std::string path_country, std::string
     if (result == "") {
         return GenericError();
     }
-    json["country_code"]  = result;
+    json["country_code"] = result;
     result = geoip_query<GeoIP_country_name_by_name_gl>(ip, path_country);
     if (result == "") {
         return GenericError();
     }
-    json["country_name"]  = result;
-    
+    json["country_name"] = result;
+
     result = geoip_query<MK_GeoIP_name_by_name_gl>(ip, path_asn);
     if (result == "") {
         return GenericError();
     }
-    json["country_asn"]  = result;
+    json["asn"] = result;
     return ErrorOr<Json>(json);
 }
 
--- test/ooni/utils.cpp
+++ test/ooni/utils.cpp
@@ -5,10 +5,10 @@
 #define CATCH_CONFIG_MAIN
 #include "src/ext/Catch/single_include/catch.hpp"
 
+#include "src/common/check_connectivity.hpp"
 #include "src/ooni/utils.hpp"
 #include <measurement_kit/common.hpp>
 #include <measurement_kit/http.hpp>
-#include "src/common/check_connectivity.hpp"
 
 TEST_CASE("ip lookup works") {
     if (mk::CheckConnectivity::is_down()) {
@@ -23,8 +23,11 @@ TEST_CASE("ip lookup works") {
 }
 
 TEST_CASE("geoip works") {
-    std::string resolved = "{\"country_asn\":\"AS15169 Google Inc.\",\"country_code\":\"USA\",\"country_name\":\"United States\"}";
-    mk::ErrorOr<Json> json = mk::ooni::geoip("8.8.8.8", "test/fixtures/GeoIP.dat", "test/fixtures/GeoIPASNum.dat");
-    REQUIRE (json);
-    REQUIRE (resolved == json->dump());
+    std::string resolved = "{\"country_asn\":\"AS15169 Google "
+                           "Inc.\",\"country_code\":\"USA\",\"country_name\":"
+                           "\"United States\"}";
+    mk::ErrorOr<Json> json = mk::ooni::geoip(
+        "8.8.8.8", "test/fixtures/GeoIP.dat", "test/fixtures/GeoIPASNum.dat");
+    REQUIRE(json);
+    REQUIRE(resolved == json->dump());
 }
