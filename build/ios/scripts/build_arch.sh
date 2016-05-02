5790bc69176f9408d912b4a595442dc8c18de613
AntonioLangiu <antonio.langiu@studenti.polito.it>
2016-05-02 13:44:41 +0200

    Use -miphoneos-version-min for ios and -mios-simulator-version-min for the simulator

--- build/ios/scripts/build_arch.sh
+++ build/ios/scripts/build_arch.sh
@@ -10,12 +10,16 @@ fi
 PLATFORM=$1
 ARCH=$2
 
+MINIOSVERSION="7.1"
 if [ "$PLATFORM" = "iphoneos" ]; then
     EXTRA_CONFIG="--host=arm-apple-darwin14 --target=arm-apple-darwin14"
+    MINVERSION="-miphoneos-version-min=$MINIOSVERSION"
 elif [ "$ARCH" = "i386" ]; then
     EXTRA_CONFIG="--host=i386-apple-darwin14 --target=i386-apple-darwin14"
+    MINVERSION="-mios-simulator-version-min=$MINIOSVERSION"
 elif [ "$ARCH" = "x86_64" ]; then
     EXTRA_CONFIG="--host=x86_64-apple-darwin14 --target=x86_64-apple-darwin14"
+    MINVERSION="-mios-simulator-version-min=$MINIOSVERSION"
 else
     echo "$0: unsupported configuration" 1>&2
     exit 1
@@ -32,12 +36,11 @@ SDKVERSION=`echo "$FIRST_SDK" | cut -d\  -f2`
 echo "Using this SDK version: $SDKVERSION"
 
 DEVELOPER=$(xcode-select -print-path)
-MINIOSVERSION="7.1"
 
 export PATH="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/:${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/usr/bin/:${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin:${DEVELOPER}/usr/bin:${PATH}"
 
-export CC="$(xcrun -find -sdk ${PLATFORM} cc) -arch ${ARCH} -miphoneos-version-min=${MINIOSVERSION}"
-export CXX="$(xcrun -find -sdk ${PLATFORM} g++) -arch ${ARCH} -miphoneos-version-min=${MINIOSVERSION}"
+export CC="$(xcrun -find -sdk ${PLATFORM} cc) -arch ${ARCH} $MINVERSION"
+export CXX="$(xcrun -find -sdk ${PLATFORM} g++) -arch ${ARCH} $MINVERSION"
 export CPPFLAGS="-isysroot $(xcrun -sdk ${PLATFORM} --show-sdk-path)"
 export CFLAGS="-isysroot $(xcrun -sdk ${PLATFORM} --show-sdk-path)"
 export CXXFLAGS="-isysroot $(xcrun -sdk ${PLATFORM} --show-sdk-path)"
