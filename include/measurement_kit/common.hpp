dc90da339516b6fb8735f3b3a5ef329f3fdc3eef
Simone Basso <bassosimone@gmail.com>
2016-05-10 11:53:22 +0200

    Add continuation template (#553)
    * Add continuation template
    
    Perhaps not super intuitive, the continuation is anyway useful
    for implementing simple coroutines. Required in NDT code.
    
    * explain
    
    * Fix build after 9d6682b9

--- include/measurement_kit/common.hpp
+++ include/measurement_kit/common.hpp
@@ -3,6 +3,7 @@
 #define MEASUREMENT_KIT_COMMON_HPP
 #include <measurement_kit/common/async.hpp>
 #include <measurement_kit/common/callback.hpp>
+#include <measurement_kit/common/continuation.hpp>
 #include <measurement_kit/common/delegate.hpp>
 #include <measurement_kit/common/error.hpp>
 #include <measurement_kit/common/error_or.hpp>
@@ -13,8 +14,8 @@
 #include <measurement_kit/common/non_copyable.hpp>
 #include <measurement_kit/common/non_movable.hpp>
 #include <measurement_kit/common/reactor.hpp>
-#include <measurement_kit/common/settings_entry.hpp>
 #include <measurement_kit/common/settings.hpp>
+#include <measurement_kit/common/settings_entry.hpp>
 #include <measurement_kit/common/var.hpp>
 #include <measurement_kit/common/version.hpp>
 #endif
