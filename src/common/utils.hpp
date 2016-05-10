e156dc6a9a3d830282b16ee839f96d16ec8fa0aa
Simone Basso <bassosimone@gmail.com>
2016-05-10 21:43:15 +0200

    s/c/utils.hpp: group similar functions

--- src/common/utils.hpp
+++ src/common/utils.hpp
@@ -20,39 +20,27 @@ struct timeval;
 namespace mk {
 
 void timeval_now(timeval *);
-
 double time_now();
-
 void utc_time_now(struct tm *);
-
 std::string timestamp(const struct tm *);
-
-evutil_socket_t listen(int, const char *, const char *);
+timeval *timeval_init(timeval *, double);
 
 void xfree(void *);
 
-timeval *timeval_init(timeval *, double);
-
+evutil_socket_t listen(int, const char *, const char *);
 int storage_init(sockaddr_storage *, socklen_t *, const char *, const char *,
                  const char *);
-
 int storage_init(sockaddr_storage *, socklen_t *, int, const char *,
                  const char *);
-
 int storage_init(sockaddr_storage *, socklen_t *, int, const char *, int);
-
 evutil_socket_t socket_create(int, int, int);
-
 int socket_connect(evutil_socket_t, sockaddr_storage *, socklen_t);
-
 int socket_listen(evutil_socket_t, sockaddr_storage *, socklen_t);
 
 std::string random_str(size_t length);
-
 std::string random_str_uppercase(size_t length);
 
 std::string unreverse_ipv6(std::string s);
-
 std::string unreverse_ipv4(std::string s);
 
 } // namespace mk
