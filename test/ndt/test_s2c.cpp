b59a09e8faed77fb7834c7c9fdb2d61f7992943c
Simone Basso <bassosimone@gmail.com>
2016-05-14 20:20:09 +0200

    test_s2c: hopefully reached 100% of coverage

--- test/ndt/test_s2c.cpp
+++ test/ndt/test_s2c.cpp
@@ -32,9 +32,8 @@ static void failure(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
 
 TEST_CASE("finalizing_test() deals with read_ndt() error") {
     Var<Context> ctx(new Context);
-    test_s2c::finalizing_test_impl<failure>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::finalizing_test_impl<failure>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void invalid(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
@@ -43,9 +42,8 @@ static void invalid(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
 
 TEST_CASE("finalizing_test() deals with receiving invalid message") {
     Var<Context> ctx(new Context);
-    test_s2c::finalizing_test_impl<invalid>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::finalizing_test_impl<invalid>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static int empty_counter = 0;
@@ -59,23 +57,20 @@ static void empty(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
 
 TEST_CASE("finalizing_test() deals with receiving invalid json") {
     Var<Context> ctx(new Context);
-    test_s2c::finalizing_test_impl<empty>(ctx, [](Error err) {
-        REQUIRE(err == NoError());
-    });
+    test_s2c::finalizing_test_impl<empty>(
+        ctx, [](Error err) { REQUIRE(err == NoError()); });
 }
 
 TEST_CASE("run() deals with messages::read() failure") {
     Var<Context> ctx(new Context);
-    test_s2c::run_impl<failure>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::run_impl<failure>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 TEST_CASE("run() deals with receiving message different from PREPARE") {
     Var<Context> ctx(new Context);
-    test_s2c::run_impl<invalid>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::run_impl<invalid>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void invalid_port(Var<Context>,
@@ -85,9 +80,8 @@ static void invalid_port(Var<Context>,
 
 TEST_CASE("run() deals with receiving invalid port") {
     Var<Context> ctx(new Context);
-    test_s2c::run_impl<invalid_port>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::run_impl<invalid_port>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void too_large(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
@@ -96,9 +90,8 @@ static void too_large(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
 
 TEST_CASE("run() deals with receiving too large port") {
     Var<Context> ctx(new Context);
-    test_s2c::run_impl<too_large>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::run_impl<too_large>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void too_small(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
@@ -107,9 +100,8 @@ static void too_small(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
 
 TEST_CASE("run() deals with receiving too small port") {
     Var<Context> ctx(new Context);
-    test_s2c::run_impl<too_small>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::run_impl<too_small>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void success(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
@@ -117,8 +109,8 @@ static void success(Var<Context>, Callback<Error, uint8_t, std::string> cb) {
 }
 
 static void failure(std::string, int,
-                    Callback<Error, Continuation<Error, double>> cb,
-                    double, Settings, Var<Logger>, Var<Reactor>) {
+                    Callback<Error, Continuation<Error, double>> cb, double,
+                    Settings, Var<Logger>, Var<Reactor>) {
     cb(GenericError(), [](Callback<Error, double>) {
         REQUIRE(false); // should not happen
     });
@@ -126,12 +118,9 @@ static void failure(std::string, int,
 
 TEST_CASE("run() deals with coroutine failure") {
     Var<Context> ctx(new Context);
-    test_s2c::run_impl<success, messages::format_test_msg,
-                       messages::read_json,
-                       messages::write,
-                       failure>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::run_impl<success, messages::format_test_msg, messages::read_json,
+                       messages::write, failure>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static int failure_in_test_start_cnt = 0;
@@ -144,22 +133,19 @@ static void failure_in_test_start(Var<Context>,
     }
 }
 
-static void connect_but_fail_later(std::string, int,
-                    Callback<Error, Continuation<Error, double>> cb,
-                    double, Settings, Var<Logger>, Var<Reactor>) {
-    cb(NoError(), [](Callback<Error, double> cb) {
-        cb(GenericError(), 0.0);
-    });
+static void
+connect_but_fail_later(std::string, int,
+                       Callback<Error, Continuation<Error, double>> cb, double,
+                       Settings, Var<Logger>, Var<Reactor>) {
+    cb(NoError(), [](Callback<Error, double> cb) { cb(GenericError(), 0.0); });
 }
 
 TEST_CASE("run() deals with error when reading TEST_START") {
     Var<Context> ctx(new Context);
     test_s2c::run_impl<failure_in_test_start, messages::format_test_msg,
-                       messages::read_json,
-                       messages::write,
-                       connect_but_fail_later>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+                       messages::read_json, messages::write,
+                       connect_but_fail_later>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static int not_test_start_cnt = 0;
@@ -175,16 +161,14 @@ static void not_test_start(Var<Context>,
 TEST_CASE("run() deals with unexpected message instead of TEST_START") {
     Var<Context> ctx(new Context);
     test_s2c::run_impl<not_test_start, messages::format_test_msg,
-                       messages::read_json,
-                       messages::write,
-                       connect_but_fail_later>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+                       messages::read_json, messages::write,
+                       connect_but_fail_later>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static int test_prepare_and_start_cnt = 0;
 static void test_prepare_and_start(Var<Context>,
-                           Callback<Error, uint8_t, std::string> cb) {
+                                   Callback<Error, uint8_t, std::string> cb) {
     if (test_prepare_and_start_cnt++ == 0) {
         cb(NoError(), TEST_PREPARE, "3010");
     } else {
@@ -196,11 +180,15 @@ static void test_prepare_and_start(Var<Context>,
 TEST_CASE("run() deals with coroutine terminating with error") {
     Var<Context> ctx(new Context);
     test_s2c::run_impl<test_prepare_and_start, messages::format_test_msg,
-                       messages::read_json,
-                       messages::write,
-                       connect_but_fail_later>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+                       messages::read_json, messages::write,
+                       connect_but_fail_later>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
+}
+
+static void success(std::string, int,
+                    Callback<Error, Continuation<Error, double>> cb, double,
+                    Settings, Var<Logger>, Var<Reactor>) {
+    cb(NoError(), [](Callback<Error, double> cb) { cb(NoError(), 0.0); });
 }
 
 static void failure(Var<Context>, Callback<Error, uint8_t, json> cb) {
@@ -210,11 +198,8 @@ static void failure(Var<Context>, Callback<Error, uint8_t, json> cb) {
 TEST_CASE("run() deals with error when reading TEST_MSG") {
     Var<Context> ctx(new Context);
     test_s2c::run_impl<test_prepare_and_start, messages::format_test_msg,
-                       failure,
-                       messages::write,
-                       connect_but_fail_later>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+                       failure, messages::write, success>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void invalid_msg(Var<Context>, Callback<Error, uint8_t, json> cb) {
@@ -224,29 +209,21 @@ static void invalid_msg(Var<Context>, Callback<Error, uint8_t, json> cb) {
 TEST_CASE("run() deals with unexpected message instead of TEST_MSG") {
     Var<Context> ctx(new Context);
     test_s2c::run_impl<test_prepare_and_start, messages::format_test_msg,
-                       invalid_msg,
-                       messages::write,
-                       connect_but_fail_later>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+                       invalid_msg, messages::write, success>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void msg_test_ok(Var<Context>, Callback<Error, uint8_t, json> cb) {
     cb(NoError(), TEST_MSG, {});
 }
 
-static ErrorOr<Buffer> failure(std::string) {
-    return GenericError();
-}
+static ErrorOr<Buffer> failure(std::string) { return GenericError(); }
 
 TEST_CASE("run() deals with format_test_msg() failure") {
     Var<Context> ctx(new Context);
-    test_s2c::run_impl<test_prepare_and_start, failure,
-                       msg_test_ok,
-                       messages::write,
-                       connect_but_fail_later>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+    test_s2c::run_impl<test_prepare_and_start, failure, msg_test_ok,
+                       messages::write, success>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
 
 static void failure(Var<Context>, Buffer, Callback<Error> cb) {
@@ -256,9 +233,6 @@ static void failure(Var<Context>, Buffer, Callback<Error> cb) {
 TEST_CASE("run() deals with write() failure") {
     Var<Context> ctx(new Context);
     test_s2c::run_impl<test_prepare_and_start, messages::format_test_msg,
-                       msg_test_ok,
-                       failure,
-                       connect_but_fail_later>(ctx, [](Error err) {
-        REQUIRE(err == GenericError());
-    });
+                       msg_test_ok, failure, success>(
+        ctx, [](Error err) { REQUIRE(err == GenericError()); });
 }
