69533a9b3d94bfc6667d6750dd297ed321293c78
Simone Basso <bassosimone@gmail.com>
2016-05-14 17:22:40 +0200

    one more test

---
+++ test/ndt/ndt_test.cpp
@@ -0,0 +1,16 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#define CATCH_CONFIG_MAIN
+
+#include "src/ext/Catch/single_include/catch.hpp"
+#include <measurement_kit/ndt.hpp>
+
+using namespace mk::ndt;
+
+TEST_CASE("The NDT test DSL works") {
+    NdtTest()
+        .set_verbosity(MK_LOG_INFO)
+        .run();
+}
