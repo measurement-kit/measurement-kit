75d5dd42a63580cc728c1f96f44c2e49dd8ac826
AntonioLangiu <antonio.langiu@studenti.polito.it>
2016-05-16 21:13:23 +0200

    use promise and future to sync the two threads

--- src/common/runner.cpp
+++ src/common/runner.cpp
@@ -3,6 +3,9 @@
 // information on the copying conditions.
 
 #include <measurement_kit/common.hpp>
+#include <mutex>
+#include <thread>
+#include <future>
 
 namespace mk {
 
@@ -10,10 +13,19 @@ Runner::Runner() {}
 
 void Runner::run_test(Var<NetTest> test, std::function<void(Var<NetTest>)> fn) {
     if (!running) {
+        std::promise<bool> promise;
+        std::future<bool> future = promise.get_future();
         // WARNING: below we're passing `this` to the thread, which means that
         // the destructor MUST wait the thread. Otherwise, when the thread dies
         // many strange things could happen (I have seen SIGABRT).
-        thread = std::thread([this]() { reactor->loop(); });
+        debug("runner: starting reactor in background...");
+        thread = std::thread([&promise, this]() {
+            reactor->loop_with_initial_event([&promise]() {
+                debug("runner: starting reactor in background... ok");
+                promise.set_value(true);
+            });
+        });
+        future.wait();
         running = true;
     }
     active += 1;
