717c4c6052ae76a86e6683aa656d4d8f1587f192
Simone Basso <bassosimone@gmail.com>
2016-06-07 22:32:23 +0200

    Update ref to precompiled dependencies

--- build/android/scripts/build.sh
+++ build/android/scripts/build.sh
@@ -42,7 +42,7 @@ echo "Downloading and verifying precompiled dependencies from github"
     # Note: the precompiled dependencies MAY be downloadable from a previous
     # release, what matters to decide if they're outdated is the `spec` dir that
     # you can find inside each tarball and contains version number info
-    DEPS_URL=https://github.com/measurement-kit/measurement-kit/releases/download/v0.2.0-alpha/android-dependencies-20160606T122542Z.tgz
+    DEPS_URL=https://github.com/measurement-kit/measurement-kit/releases/download/v0.2.0-alpha/android-dependencies-20160607T203701Z.tgz
     DEPS_FILE=$(basename $DEPS_URL)
     curl --progress-bar -LO $DEPS_URL
     curl --progress-bar -LO $DEPS_URL.asc
--- build/ios/scripts/build.sh
+++ build/ios/scripts/build.sh
@@ -15,7 +15,7 @@ echo "Downloading and verifying precompiled dependencies from github"
     # Note: the precompiled dependencies MAY be downloadable from a previous
     # release, what matters to decide if they're outdated is the `spec` dir that
     # you can find inside each tarball and contains version number info
-    DEPS_URL=https://github.com/measurement-kit/measurement-kit/releases/download/v0.2.0-alpha/ios-dependencies-20160605T234602Z.tgz
+    DEPS_URL=https://github.com/measurement-kit/measurement-kit/releases/download/v0.2.0-alpha/ios-dependencies-20160607T165015Z.tgz
     DEPS_FILE=$(basename $DEPS_URL)
     curl --progress-bar -LO $DEPS_URL
     curl --progress-bar -LO $DEPS_URL.asc
