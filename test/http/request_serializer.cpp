f856637a22920e935b71a8b21cd08ce2a6c968e8
Carmine D'Amico <Carmine920@gmail.com>
2016-06-09 22:14:16 +0200

    Fixed request serializer (#670)
    * Fixed path in request
    
    * bug fix
    
    * Revert "Fixed path in request"
    
    This reverts commit b44d31ccbf6acc69db934eb06d28248d1d4acf54.
    
    * bug fix
    
    * bug fix
    
    * bug fix

--- test/http/request_serializer.cpp
+++ test/http/request_serializer.cpp
@@ -41,3 +41,30 @@ TEST_CASE("HTTP Request serializer works as expected") {
     expect += "0123456789";
     REQUIRE(serialized == expect);
 }
+
+TEST_CASE("HTTP Request serializer works as expected with explicit path") {
+    auto serializer = RequestSerializer(
+        {
+            {"http/follow_redirects", "yes"},
+            {"http/url",
+             "http://www.example.com/antani?clacsonato=yes#melandri"},
+            {"http/path", "/antani?amicimiei"},
+            {"http/ignore_body", "yes"},
+            {"http/method", "GET"},
+            {"http/http_version", "HTTP/1.0"},
+        },
+        {
+            {"User-Agent", "Antani/1.0.0.0"},
+        },
+        "0123456789");
+    Buffer buffer;
+    serializer.serialize(buffer);
+    auto serialized = buffer.read();
+    std::string expect = "GET /antani?amicimiei HTTP/1.0\r\n";
+    expect += "User-Agent: Antani/1.0.0.0\r\n";
+    expect += "Host: www.example.com\r\n";
+    expect += "Content-Length: 10\r\n";
+    expect += "\r\n";
+    expect += "0123456789";
+    REQUIRE(serialized == expect);
+}
