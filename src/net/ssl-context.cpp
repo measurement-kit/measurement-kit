242acdf7f0ac2f0ab258fbe1029171fe5dfd3d3a
Simone Basso <bassosimone@gmail.com>
2016-06-11 00:00:53 +0200

    Disable dubious EVP_cleanup() call

--- src/net/ssl-context.cpp
+++ src/net/ssl-context.cpp
@@ -80,7 +80,7 @@ SslContext::SslContext(std::string ca_bundle_path) {
 }
 
 SslContext::~SslContext() {
-    EVP_cleanup();
+    //EVP_cleanup();  // XXX Should this be called on every destruction?
     SSL_CTX_free(ctx);
 }
 
