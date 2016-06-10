a0f1d14eb6e842829be2fc3df65824d2ea0f9f94
Simone Basso <bassosimone@gmail.com>
2016-06-11 00:30:43 +0200

    name changes for symmetry with other modules

--- src/dns/query.cpp
+++ src/dns/query.cpp
@@ -2,7 +2,7 @@
 // Measurement-kit is free software. See AUTHORS and LICENSE for more
 // information on the copying conditions.
 
-#include "src/dns/query.hpp"
+#include "src/dns/query_impl.hpp"
 struct evdns_base;
 
 namespace mk {
@@ -19,7 +19,7 @@ extern "C" {
 void query (QueryClass dns_class, QueryType dns_type,
         std::string name, Callback<Error, Message> cb,
         Settings settings, Var<Reactor> reactor) {
-    query_debug (dns_class, dns_type, name, cb, settings, reactor);
+    query_impl (dns_class, dns_type, name, cb, settings, reactor);
 }
 
 } // namespace dns
