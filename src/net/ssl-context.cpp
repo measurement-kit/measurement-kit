08e8275de93c327e2df5a301b102c68f49642490
Simone Basso <bassosimone@gmail.com>
2016-06-10 23:58:27 +0200

    More clear exceptions

--- src/net/ssl-context.cpp
+++ src/net/ssl-context.cpp
@@ -6,7 +6,7 @@
 #include <openssl/evp.h>
 #include <openssl/ossl_typ.h>
 #include <openssl/ssl.h>
-#include <exception>
+#include <stdexcept>
 #include <iostream>
 #include <measurement_kit/net.hpp>
 #include "src/net/ssl-context.hpp"
@@ -42,12 +42,12 @@ void SslContext::init(std::string ca_bundle_path) {
     ctx = SSL_CTX_new(TLSv1_client_method());
     if (ctx == nullptr) {
         debug("ssl: failed to create SSL_CTX");
-        throw std::exception();
+        throw std::runtime_error("Failed to create SSL_CTX");
     }
 
     if (SSL_CTX_load_verify_locations(ctx, ca_bundle_path.c_str(), NULL) != 1) {
         debug("ssl: failed to load verify location");
-        throw std::exception();
+        throw std::runtime_error("Failed to load verify location");
     };
 
     SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
