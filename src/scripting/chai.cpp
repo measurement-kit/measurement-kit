cdca2da39a9fda4e4e8dc6bbdfe8f59a4fc1497e
Simone Basso <bassosimone@gmail.com>
2016-07-02 18:42:01 +0200

    Some more improvements

--- src/scripting/chai.cpp
+++ src/scripting/chai.cpp
@@ -17,7 +17,7 @@ using chai_generic_t = chaiscript::Boxed_Value;
 using chai_map_t = std::map<std::string, chai_generic_t>;
 using chai_vector_t = std::vector<chai_generic_t>;
 
-struct Resume {
+struct YieldFrom {
     chai_t &chai;
     std::atomic<size_t> complete{0};
     std::vector<Callback<>> finalizers;
@@ -25,91 +25,94 @@ struct Resume {
     chai_vector_t retval;
     std::vector<Callback<>> runners;
 
-    Resume(chai_t &owner) : chai(owner) {}
+    YieldFrom(chai_t &owner) : chai(owner) {}
 };
 
 template <typename T>
-void maybe_copy_from_chai(Var<Resume> R, Settings &s, const chai_map_t &m,
+void maybe_copy_from_chai(Var<YieldFrom> YF, Settings &s, const chai_map_t &m,
                           std::string k) {
     if (m.find(k) != m.end()) {
-        s[k] = R->chai.boxed_cast<T>(m.at(k));
+        s[k] = YF->chai.boxed_cast<T>(m.at(k));
     }
 }
 
-static void maybe_stop_reactor(Var<Resume> R) {
-    if (++R->complete >= R->retval.size()) {
-        R->reactor->break_loop();
+static void maybe_stop_reactor(Var<YieldFrom> YF) {
+    if (++YF->complete >= YF->retval.size()) {
+        YF->reactor->break_loop();
     }
 }
 
-static Callback<> eval_dns_query(Var<Resume> R, size_t index,
+static Callback<> eval_dns_query(Var<YieldFrom> YF, size_t index,
                                  chai_vector_t expression) {
-    auto qclass = R->chai.boxed_cast<std::string>(expression.at(1));
-    auto qtype = R->chai.boxed_cast<std::string>(expression.at(2));
-    auto qquery = R->chai.boxed_cast<std::string>(expression.at(3));
-    auto qmap = R->chai.boxed_cast<chai_map_t>(expression.at(4));
+    auto qclass = YF->chai.boxed_cast<std::string>(expression.at(1));
+    auto qtype = YF->chai.boxed_cast<std::string>(expression.at(2));
+    auto qquery = YF->chai.boxed_cast<std::string>(expression.at(3));
+    auto qmap = YF->chai.boxed_cast<chai_map_t>(expression.at(4));
     Settings settings;
-    maybe_copy_from_chai<std::string>(R, settings, qmap, "dns/nameserver");
-    maybe_copy_from_chai<int>(R, settings, qmap, "dns/attempts");
-    maybe_copy_from_chai<double>(R, settings, qmap, "dns/timeout");
+    maybe_copy_from_chai<std::string>(YF, settings, qmap, "dns/nameserver");
+    maybe_copy_from_chai<int>(YF, settings, qmap, "dns/attempts");
+    maybe_copy_from_chai<double>(YF, settings, qmap, "dns/timeout");
     return [=]() {
         dns::query(qclass.c_str(), qtype.c_str(), qquery,
                    [=](Error err, dns::Message message) {
-                       maybe_stop_reactor(R);
-                       R->finalizers.at(index) = [=]() {
+                       maybe_stop_reactor(YF);
+                       YF->finalizers.at(index) = [=]() {
                            chai_vector_t tuple;
                            tuple.push_back(chai_generic_t{err});
                            tuple.push_back(chai_generic_t{message});
-                           R->retval.at(index) = chai_generic_t{tuple};
+                           YF->retval.at(index) = chai_generic_t{tuple};
                        };
                    },
-                   settings, R->reactor);
+                   settings, YF->reactor);
     };
 }
 
-static Callback<> do_eval(Var<Resume> R, size_t index, std::string form,
+static Callback<> do_eval(Var<YieldFrom> YF, size_t index, std::string form,
                           chai_vector_t expression) {
     if (form == "dns_query") {
-        return eval_dns_query(R, index, expression);
+        return eval_dns_query(YF, index, expression);
     }
     // TODO: expose more MK APIs
     throw std::runtime_error("asked to evaluate an unknown form");
 }
 
 static chai_vector_t do_yield_from(chai_t &chai, chai_vector_t args) {
-    Var<Resume> R{new Resume{chai}};
-    R->retval.resize(args.size()); /* R->retval.size() used to stop reactor */
-    R->finalizers.resize(args.size());
-    R->runners.resize(args.size());
+    Var<YieldFrom> YF{new YieldFrom{chai}};
+    YF->retval.resize(args.size()); /* YF->retval.size() used to stop reactor */
+    YF->finalizers.resize(args.size());
+    YF->runners.resize(args.size());
     size_t index = 0;
     for (auto arg : args) {
-        auto expression = R->chai.boxed_cast<chai_vector_t>(arg);
-        auto form = R->chai.boxed_cast<std::string>(expression.at(0));
-        R->runners.at(index) = do_eval(R, index, form, expression);
+        auto expression = YF->chai.boxed_cast<chai_vector_t>(arg);
+        auto form = YF->chai.boxed_cast<std::string>(expression.at(0));
+        YF->runners.at(index) = do_eval(YF, index, form, expression);
         index += 1;
     }
-    R->reactor->loop_with_initial_event([=]() {
-        for (auto f : R->runners) {
+    YF->reactor->loop_with_initial_event([=]() {
+        for (auto f : YF->runners) {
             f();
         }
     });
-    for (auto f : R->finalizers) {
+    for (auto f : YF->finalizers) {
         f();
     }
-    return R->retval;
+    return YF->retval;
 }
 
 Error chai(std::string filepath) {
     chai_t chai(chaiscript::Std_Lib::library());
-    chai.add(chaiscript::fun(&do_yield_from), "yield_from");
-    chai.add(chaiscript::fun(
-                 [&](chai_vector_t args) { return do_yield_from(chai, args); }),
-             "yield_from");
-    chai.add(chaiscript::fun([]() { return Error(); }), "xo");
+
+    /*
+     * expose common
+     */
 
     chai.add(chaiscript::user_type<Error>(), "Error");
     chai.add(chaiscript::fun(&Error::code), "code");
     chai.add(chaiscript::fun(&Error::reason), "reason");
+
+    /*
+     * expose dns
+     */
 
     chai.add(chaiscript::user_type<dns::Message>(), "DnsMessage");
     chai.add(chaiscript::fun(&dns::Message::rtt), "rtt");
@@ -142,6 +145,14 @@ Error chai(std::string filepath) {
 
     chai.add(chaiscript::user_type<dns::QueryType>(), "DnsQueryType");
     chai.add(chaiscript::fun(&dns::QueryType::str), "str");
+
+    /*
+     * declare our yield-like operator and run
+     */
+
+    chai.add(chaiscript::fun(
+                 [&](chai_vector_t args) { return do_yield_from(chai, args); }),
+             "yield_from");
 
     chai.eval_file(filepath);
     return NoError();
