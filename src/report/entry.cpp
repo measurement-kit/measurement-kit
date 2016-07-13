f178c2fcd416ea9dbcddf17f8935c353c976616a
Simone Basso <bassosimone@gmail.com>
2016-07-13 09:08:10 +0200

    Remove merge leftover to repair build

--- src/report/entry.cpp
+++ src/report/entry.cpp
@@ -25,21 +25,6 @@ namespace report {
     return entry;
 }
 
-Entry &Entry::operator=(Entry value) {
-    nlohmann::json::operator=(value);
-    return *this;
-}
-
-Entry &Entry::operator[](std::string key) {
-    // Note: out_of_range could not happen because operator[] returns
-    // a null json object if the accessed key is missing
-    try {
-        return static_cast<Entry &>(nlohmann::json::operator[](key));
-    } catch (std::domain_error &) {
-        throw JsonDomainError();
-    }
-}
-
 void Entry::push_back(Entry value) {
     try {
         nlohmann::json::push_back(value);
