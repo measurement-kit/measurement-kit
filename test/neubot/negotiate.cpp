444ee12e3db34e8e65b40be1994336210492317a
diyderot <carmine920@gmail.com>
2016-07-14 13:12:04 +0200

    bug fix

--- test/neubot/negotiate.cpp
+++ test/neubot/negotiate.cpp
@@ -29,7 +29,7 @@ static void receive_no_authentication_key(Var<net::Transport>, Settings, Headers
                     "\"authorization\": \"antani\", "
                     "\"queue_pos\": \"1\", "
                     "\"real_address\": \"0.0.0.0\"}";
-    cb(MockedError(), response);
+    cb(NoError(), response);
 }
 
 TEST_CASE("Server doesn't allow authentication") {
@@ -40,6 +40,23 @@ TEST_CASE("Server doesn't allow authentication") {
     );
 }
 
+static void receive_invalid_status_code(Var<net::Transport>, Settings, Headers,
+                                            std::string,
+                                            Callback<Error, Var<http::Response>> cb,
+                                            Var<Reactor> = Reactor::global(),
+                                            Var<Logger> = Logger::global()) {
+    Var<http::Response> response(new http::Response);
+    response->status_code = 500;
+    cb(NoError(), response);
+}
+
+TEST_CASE("Server doesn't allow authentication") {
+
+    loop_negotiate<receive_invalid_status_code>( nullptr,
+        [](Error error) { REQUIRE(error); }, {},
+        Reactor::global(), Logger::global()
+    );
+}
 
 
 TEST_CASE("Test works as expected") {
