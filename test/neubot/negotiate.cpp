077e571a10fa33e697a5c3f59e2299b96fafd232
diyderot <carmine920@gmail.com>
2016-07-22 12:15:21 +0200

    bug fix

--- test/neubot/negotiate.cpp
+++ test/neubot/negotiate.cpp
@@ -3,9 +3,23 @@
 #include "src/ext/Catch/single_include/catch.hpp"
 #include <measurement_kit/neubot.hpp>
 #include "src/neubot/negotiate_impl.hpp"
+#include "src/net/emitter.hpp"
 
 using namespace mk;
 using namespace mk::neubot::negotiate;
+
+#ifdef ENABLE_INTEGRATION_TESTS
+TEST_CASE("Test works as expected") {
+    loop_with_initial_event([=]() {
+        run([=](Error error) {
+            REQUIRE(!error);
+            break_loop();
+        });
+
+    });
+}
+#endif
+
 
 static void fail(std::string, Callback<Error, mlabns::Reply> cb, Settings,
                  Var<Reactor>, Var<Logger>) {
@@ -14,12 +28,12 @@ static void fail(std::string, Callback<Error, mlabns::Reply> cb, Settings,
 
 TEST_CASE("run() deals with mlab-ns query error") {
     run_impl<fail>(
-        [](Error error) { REQUIRE(error); }, {},
+        [](Error error) { REQUIRE(error == MockedError()); }, {},
         Reactor::global(), Logger::global()
     );
 }
 
-/*static void receive_no_authentication_key(Var<net::Transport>, Settings, Headers,
+static void receive_no_authentication_key(Var<net::Transport>, Settings, Headers,
                                             std::string,
                                             Callback<Error, Var<http::Response>> cb,
                                             Var<Reactor> = Reactor::global(),
@@ -28,15 +42,16 @@ TEST_CASE("run() deals with mlab-ns query error") {
     response->status_code = 200;
     response->body = "{\"unchoked\": 0, "
                     "\"authorization\": \"antani\", "
-                    "\"queue_pos\": \"1\", "
+                    "\"queue_pos\": 1, "
                     "\"real_address\": \"0.0.0.0\"}";
     cb(NoError(), response);
-}*/
+}
 
-TEST_CASE("Too many negotiations") {
-    loop_negotiate( nullptr,
+TEST_CASE("Too many negotiations", "[xx]") {
+    Var<net::Transport> emitter(new net::Emitter);
+    loop_negotiate<receive_no_authentication_key>( emitter,
         [](Error error) { REQUIRE(error); }, {},
-        Reactor::global(), Logger::global(), 600
+        Reactor::global(), Logger::global()
     );
 }
 
@@ -52,19 +67,9 @@ static void receive_invalid_status_code(Var<net::Transport>, Settings, Headers,
 
 TEST_CASE("Make sure that an error is passed to callback if the response "
           "status is not 200") {
-    loop_negotiate<receive_invalid_status_code>( nullptr,
-        [](Error error) { REQUIRE(error); }, {},
+    Var<net::Transport> emitter(new net::Emitter);
+    loop_negotiate<receive_invalid_status_code>( emitter,
+        [](Error error) { REQUIRE(error == HttpRequestFailedError()); }, {},
         Reactor::global(), Logger::global()
     );
-}
-
-
-TEST_CASE("Test works as expected") {
-    loop_with_initial_event([=]() {
-        run([=](Error error) {
-            REQUIRE(!error);
-            break_loop();
-        });
-
-    });
 }
