72aac3ee7f9a58c02c87e13acb41918b47b4e459
Simone Basso <bassosimone@gmail.com>
2016-08-01 17:50:30 +0200

    Feature/library version (#749)
    * net_test: add callback to get entry as string
    
    While there, fix the name of the default OONI test to be `ooni_test`
    rather than being just `net_test`.
    
    * logger: add EOF handler
    
    This tells to a consumer of log messages that the current logger is about
    to die and resources associated with it could be safely freed.
    
    * Allow to access library version programmatically

---
+++ src/common/version.cpp
@@ -0,0 +1,13 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#include <measurement_kit/common/version.hpp>
+
+namespace mk {
+
+std::string library_version() {
+    return MEASUREMENT_KIT_VERSION;
+}
+
+} // namespace mk
