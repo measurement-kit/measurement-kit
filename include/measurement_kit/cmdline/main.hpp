3e470c0b0d11dd24c4670b4950cebcfbd8692c45
Simone Basso <bassosimone@gmail.com>
2016-08-06 11:00:47 +0200

    Add initial main() command

---
+++ include/measurement_kit/cmdline/main.hpp
@@ -0,0 +1,14 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+#ifndef MEASUREMENT_KIT_CMDLINE_MAIN_HPP
+#define MEASUREMENT_KIT_CMDLINE_MAIN_HPP
+
+namespace mk {
+namespace cmdline {
+
+int main(const char *progname, int argc, char **argv);
+
+} // namespace cmdline
+} // namespace mk
+#endif
---
+++ src/libmeasurement_kit/cmdline/main.cpp
@@ -0,0 +1,15 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#include <measurement_kit/cmdline.hpp>
+
+namespace mk {
+namespace cmdline {
+
+int main(const char *progname, int argc, char **argv) {
+    return 0;
+}
+
+} // namespace cmdline
+} // namespace mk
