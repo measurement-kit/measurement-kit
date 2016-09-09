aaf2d31743943079cb596c74ff8526d7061af85d
Simone Basso <bassosimone@gmail.com>
2016-09-09 21:24:10 +0200

    Simplify DNS to simplify porting to windows

--- include/measurement_kit/dns/dns.hpp
+++ include/measurement_kit/dns/dns.hpp
@@ -32,132 +32,10 @@ MK_DEFINE_ERR(MK_ERR_DNS(16), UnsupportedTypeError, "")
 
 Error dns_error(int code); ///< Map evdns code to proper error
 
-/// Available query classes id
-enum class QueryClassId { IN, CS, CH, HS };
-
-/// Available query types id (REVERSE_A and REVERSE_AAAA are non standard)
-enum class QueryTypeId {
-    A,
-    NS,
-    MD,
-    MF,
-    CNAME,
-    SOA,
-    MB,
-    MG,
-    MR,
-    NUL,
-    WKS,
-    PTR,
-    HINFO,
-    MINFO,
-    MX,
-    TXT,
-    AAAA,
-    REVERSE_A,
-    REVERSE_AAAA
-};
-
-/// Query class
-class QueryClass {
-  public:
-    /// Constructor with id
-    QueryClass(QueryClassId id = QueryClassId::IN) : id_(id) {}
-
-    /// Constructor with string
-    QueryClass(const char *x) {
-        std::string str(x);
-        if (str == "IN")
-            id_ = QueryClassId::IN;
-        else if (str == "CS")
-            id_ = QueryClassId::CS;
-        else if (str == "CH")
-            id_ = QueryClassId::CH;
-        else if (str == "HS")
-            id_ = QueryClassId::HS;
-        else
-            throw std::runtime_error("invalid argument");
-    }
-
-    /// Equality operator
-    bool operator==(QueryClassId id) const { return id_ == id; }
-
-    /// Unequality operator
-    bool operator!=(QueryClassId id) const { return id_ != id; }
-
-    /// Cast to query class id
-    operator QueryClassId() const { return id_; }
-
-  private:
-    QueryClassId id_;
-};
-
-/// Query class
-class QueryType {
-  public:
-    /// Constructor with id
-    QueryType(QueryTypeId id = QueryTypeId::A) : id_(id) {}
-
-    /// Constructor with string
-    QueryType(const char *x) {
-        std::string str(x);
-        if (str == "A")
-            id_ = QueryTypeId::A;
-        else if (str == "NS")
-            id_ = QueryTypeId::NS;
-        else if (str == "MD")
-            id_ = QueryTypeId::MD;
-        else if (str == "MF")
-            id_ = QueryTypeId::MF;
-        else if (str == "CNAME")
-            id_ = QueryTypeId::CNAME;
-        else if (str == "SOA")
-            id_ = QueryTypeId::SOA;
-        else if (str == "MB")
-            id_ = QueryTypeId::MB;
-        else if (str == "MG")
-            id_ = QueryTypeId::MG;
-        else if (str == "MR")
-            id_ = QueryTypeId::MR;
-        else if (str == "NUL")
-            id_ = QueryTypeId::NUL;
-        else if (str == "WKS")
-            id_ = QueryTypeId::WKS;
-        else if (str == "PTR")
-            id_ = QueryTypeId::PTR;
-        else if (str == "HINFO")
-            id_ = QueryTypeId::HINFO;
-        else if (str == "MINFO")
-            id_ = QueryTypeId::MINFO;
-        else if (str == "MX")
-            id_ = QueryTypeId::MX;
-        else if (str == "TXT")
-            id_ = QueryTypeId::TXT;
-        else if (str == "AAAA")
-            id_ = QueryTypeId::AAAA;
-        else if (str == "REVERSE_A")
-            id_ = QueryTypeId::REVERSE_A;
-        else if (str == "REVERSE_AAAA")
-            id_ = QueryTypeId::REVERSE_AAAA;
-        else
-            throw std::runtime_error("invalid argument");
-    }
-
-    /// Equality operator
-    bool operator==(QueryTypeId id) const { return id_ == id; }
-
-    /// Unequality operator
-    bool operator!=(QueryTypeId id) const { return id_ != id; }
-
-    /// Cast to query class id
-    operator QueryTypeId() const { return id_; }
-
-  private:
-    QueryTypeId id_;
-};
+using QueryType = std::string;
+using QueryClass = std::string;
 
 class Answer {
-
     public:
         QueryType type;
         QueryClass qclass;
@@ -179,7 +57,6 @@ class Answer {
         uint32_t retry_interval; ///< For SOA records
         uint32_t minimum_ttl; ///< For SOA records
         uint32_t expiration_limit; ///< For SOA records
-
 };
 
 class Query {
--- src/libmeasurement_kit/dns/query_impl.hpp
+++ src/libmeasurement_kit/dns/query_impl.hpp
@@ -113,7 +113,7 @@ static inline std::vector<Answer> build_answers_evdns(
         Answer answer;
         answer.code = code;
         answer.ttl = ttl;
-        answer.type = QueryTypeId::PTR;
+        answer.type = "PTR";
         // Note: cast magic copied from libevent regress tests
         answer.hostname = std::string(*(char **)addresses);
         logger->debug("dns: adding %s", answer.hostname.c_str());
@@ -152,10 +152,10 @@ static inline std::vector<Answer> build_answers_evdns(
                 answer.ttl = ttl;
                 if (family == PF_INET) {
                     answer.ipv4 = string;
-                    answer.type = QueryTypeId::A;
+                    answer.type = "A";
                 } else if (family == PF_INET6) {
                     answer.ipv6 = string;
-                    answer.type = QueryTypeId::AAAA;
+                    answer.type = "AAAA";
                 }
                 logger->debug("dns: adding '%s'", string);
                 answers.push_back(answer);
@@ -230,20 +230,20 @@ void query_impl(QueryClass dns_class, QueryType dns_type, std::string name,
         throw; // Let this propagate as we can do nothing
     }
 
-    if (dns_class != QueryClassId::IN) {
+    if (dns_class != "IN") {
         evdns_base_free(base, 1);
         cb(UnsupportedClassError(), nullptr);
         return;
     }
 
     // Allow PTR queries
-    if (dns_type == QueryTypeId::PTR) {
+    if (dns_type == "PTR") {
         std::string s;
         if ((s = mk::unreverse_ipv4(name)) != "") {
-            dns_type = QueryTypeId::REVERSE_A;
+            dns_type = "REVERSE_A";
             name = s;
         } else if ((s = mk::unreverse_ipv6(name)) != "") {
-            dns_type = QueryTypeId::REVERSE_AAAA;
+            dns_type = "REVERSE_AAAA";
             name = s;
         } else {
             evdns_base_free(base, 1);
@@ -273,7 +273,7 @@ void query_impl(QueryClass dns_class, QueryType dns_type, std::string name,
     // cancel pending evdns requests and uses the `cancelled`
     // variable to keep track of cancelled requests.
     //
-    if (dns_type == QueryTypeId::A) {
+    if (dns_type == "A") {
         QueryContext *context = new QueryContext(base, cb, message);
         if (evdns_base_resolve_ipv4(base, name.c_str(), DNS_QUERY_NO_SEARCH,
                     handle_resolve,
@@ -284,7 +284,7 @@ void query_impl(QueryClass dns_class, QueryType dns_type, std::string name,
         return;
     }
 
-    if (dns_type == QueryTypeId::AAAA) {
+    if (dns_type == "AAAA") {
         QueryContext *context = new QueryContext(base, cb, message);
         if (evdns_base_resolve_ipv6(base, name.c_str(), DNS_QUERY_NO_SEARCH,
                     handle_resolve,
@@ -295,7 +295,7 @@ void query_impl(QueryClass dns_class, QueryType dns_type, std::string name,
         return;
     }
 
-    if (dns_type == QueryTypeId::REVERSE_A) {
+    if (dns_type == "REVERSE_A") {
         in_addr netaddr;
         if (inet_pton(AF_INET, name.c_str(), &netaddr) != 1) {
             evdns_base_free(base, 1);
@@ -313,7 +313,7 @@ void query_impl(QueryClass dns_class, QueryType dns_type, std::string name,
         return;
     }
 
-    if (dns_type == QueryTypeId::REVERSE_AAAA) {
+    if (dns_type == "REVERSE_AAAA") {
         in6_addr netaddr;
         if (inet_pton(AF_INET6, name.c_str(), &netaddr) != 1) {
             evdns_base_free(base, 1);
