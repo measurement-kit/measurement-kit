99a09e48cc85d1783735e7d5b3ca4458c3d32b92
Simone Basso <bassosimone@gmail.com>
2016-09-10 11:52:32 +0200

    Simplify portable/err.h header

--- include/measurement_kit/portable/err.h
+++ include/measurement_kit/portable/err.h
@@ -1,87 +1,22 @@
 /*
  * Public domain
- * err.h compatibility shim
  */
-
-#ifdef HAVE_ERR_H
-
-#include_next <err.h>
-
-#else
-
-#ifndef LIBCRYPTOCOMPAT_ERR_H
-#define LIBCRYPTOCOMPAT_ERR_H
-
-#include <errno.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#ifndef MEASUREMENT_KIT_PORTABLE_ERR_H
+#define MEASUREMENT_KIT_PORTABLE_ERR_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-static inline void
-err(int eval, const char *fmt, ...)
-{
-	int sverrno = errno;
-	va_list ap;
+void err(int eval, const char *fmt, ...);
 
-	va_start(ap, fmt);
-	if (fmt != NULL) {
-		vfprintf(stderr, fmt, ap);
-		fprintf(stderr, ": ");
-	}
-	fprintf(stderr, "%s\n", strerror(sverrno));
-	exit(eval);
-	va_end(ap);
-}
+void errx(int eval, const char *fmt, ...);
 
-static inline void
-errx(int eval, const char *fmt, ...)
-{
-	va_list ap;
+void warn(const char *fmt, ...);
 
-	va_start(ap, fmt);
-	if (fmt != NULL)
-		vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
-	exit(eval);
-	va_end(ap);
-}
-
-static inline void
-warn(const char *fmt, ...)
-{
-	int sverrno = errno;
-	va_list ap;
-
-	va_start(ap, fmt);
-	if (fmt != NULL) {
-		vfprintf(stderr, fmt, ap);
-		fprintf(stderr, ": ");
-	}
-	fprintf(stderr, "%s\n", strerror(sverrno));
-	va_end(ap);
-}
-
-static inline void
-warnx(const char *fmt, ...)
-{
-	va_list ap;
-
-	va_start(ap, fmt);
-	if (fmt != NULL)
-		vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
-	va_end(ap);
-}
+void warnx(const char *fmt, ...);
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif
-
 #endif
