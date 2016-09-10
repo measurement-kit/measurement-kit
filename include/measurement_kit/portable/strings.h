93fed0f52e6e656e1904be7c0d8f748d709736b3
Simone Basso <bassosimone@gmail.com>
2016-09-10 11:47:12 +0200

    Simplify strings.h portable header

--- include/measurement_kit/portable/strings.h
+++ include/measurement_kit/portable/strings.h
@@ -4,7 +4,9 @@
 #ifndef MEASUREMENT_KIT_PORTABLE_STRINGS_H
 #define MEASUREMENT_KIT_PORTABLE_STRINGS_H
 
-#ifdef _WIN32
+#ifndef _WIN32
+#include <strings.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,11 +16,5 @@ int strcasecmp(const char *s1, const char *s2);
 
 #ifdef __cplusplus
 }
-#endif
-
-#else
-
-#include <strings.h>
-
 #endif
 #endif
