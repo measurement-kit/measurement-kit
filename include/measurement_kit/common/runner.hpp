7c782de9ca3066abcbdf2f5d2cecd3e0a8606412
Simone Basso <bassosimone@gmail.com>
2016-10-02 01:32:51 -0700

    Allow running OONI tests through runner class

--- include/measurement_kit/common/runner.hpp
+++ include/measurement_kit/common/runner.hpp
@@ -25,9 +25,11 @@ class Runner {
     ~Runner();
     static Var<Runner> global();
 
+    // Globally accessible attribute that other classes can use
+    Var<Reactor> reactor = Reactor::global();
+
   private:
     std::atomic<int> active{0};
-    Var<Reactor> reactor = Reactor::global();
     std::atomic<bool> running{false};
     std::thread thread;
 };
