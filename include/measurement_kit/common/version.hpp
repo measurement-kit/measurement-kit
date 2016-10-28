b13e24bd4f96e2e195945835f96f6b91659cad46
Antonio Langiu <antonio.langiu@studenti.polito.it>
2016-10-28 15:26:20 +0200

    update version

--- include/measurement_kit/common/version.hpp
+++ include/measurement_kit/common/version.hpp
@@ -7,7 +7,7 @@
 #include <string>
 
 // Note: we use semantic versioning (see: http://semver.org/)
-#define MEASUREMENT_KIT_VERSION "0.3.3"
+#define MEASUREMENT_KIT_VERSION "0.3.4"
 
 namespace mk {
 
