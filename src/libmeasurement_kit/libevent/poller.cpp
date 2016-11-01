9cc303bff51ae29759c266fccfdc975100f0a49e
Simone Basso <bassosimone@gmail.com>
2016-11-01 23:34:15 +0100

    Simplify poller implementation

--- src/libmeasurement_kit/libevent/poller.cpp
+++ src/libmeasurement_kit/libevent/poller.cpp
@@ -2,19 +2,19 @@
 // Measurement-kit is free software. See AUTHORS and LICENSE for more
 // information on the copying conditions.
 
-#include "../libevent/poller.hpp"
+#include "../libevent/poller_impl.hpp"
 
 // Using `extern "C"` for C callbacks is recommended by C++ FAQs.
 // See <https://isocpp.org/wiki/faq/pointers-to-members#memfnptr-vs-fnptr>.
 extern "C" {
 
-void mk_call_soon_cb(evutil_socket_t, short, void *p) {
-    auto cbp = static_cast<std::function<void()> *>(p);
+void mk_call_later_cb(evutil_socket_t, short, void *p) {
+    auto cbp = static_cast<mk::Callback<> *>(p);
     (*cbp)();
     delete cbp;
 }
 
-void mk_do_periodic_cb(evutil_socket_t, short, void *ptr) {
+void mk_loop_periodic_cb(evutil_socket_t, short, void *ptr) {
     mk::libevent::Poller *poller = static_cast<mk::libevent::Poller *>(ptr);
     poller->handle_periodic_();
 }
@@ -24,23 +24,28 @@ void mk_do_periodic_cb(evutil_socket_t, short, void *ptr) {
 namespace mk {
 namespace libevent {
 
-void Poller::call_later(double timeo, std::function<void()> cb) {
-    call_later_impl(timeo, cb);
+Poller::Poller() { base_ = poller_alloc_evbase(); }
+Poller::~Poller() {}
+event_base *Poller::get_event_base() { return base_.get(); }
+
+void Poller::call_later(double timeo, Callback<> cb) {
+    poller_call_later(base_, timeo, cb);
 }
 
-void Poller::loop() { loop_impl(); }
-void Poller::loop_once() { loop_once_impl(); }
-void Poller::break_loop() { break_loop_impl(); }
+void Poller::loop() { poller_loop(base_, this); }
+void Poller::loop_once() { poller_loop_once(base_); }
+void Poller::break_loop() { poller_break_loop(base_); }
 
 void Poller::handle_periodic_() {
     if (periodic_cb_) {
         // Protection against the callback calling on_periodic_()
+        // TODO: check whether this is needed (delegate should cope with that)
         auto fn = periodic_cb_;
         fn(this);
     }
 }
 
-void Poller::on_periodic_(std::function<void(Poller *)> cb) {
+void Poller::on_periodic_(Callback<Poller *> cb) {
     periodic_cb_ = cb;
 }
 
