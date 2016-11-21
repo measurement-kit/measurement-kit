8b63b5514c33cc302d1c2ab64efdec1432d25bdb
Antonio Langiu <antonio.langiu@studenti.polito.it>
2016-11-21 15:37:11 +0100

    fixes and add canonical name resolution

--- src/libmeasurement_kit/dns/system_resolver.hpp
+++ src/libmeasurement_kit/dns/system_resolver.hpp
@@ -7,6 +7,8 @@
 #include <cassert>
 #include <future>
 
+#include <iostream>
+
 namespace mk {
 namespace dns {
 
@@ -36,20 +38,22 @@ class ResolverContext {
         logger = l;
         memset(&hints, 0, sizeof(hints));
     }
-
 };
 
-template<MK_MOCK(getaddrinfo), MK_MOCK(inet_ntop)>
+template <MK_MOCK(getaddrinfo), MK_MOCK(inet_ntop)>
 void resolve_async(ResolverContext *context) {
     std::unique_ptr<ResolverContext> ctx(context);
     Callback<Error, Var<Message>> callback = ctx->cb;
     Var<Message> message = ctx->message;
-    addrinfo *servinfo_p;
+    addrinfo *servinfo_p = nullptr;
 
-    int error = getaddrinfo(ctx->name.c_str(), nullptr, &ctx->hints,
-                                               &servinfo_p);
-    std::unique_ptr<addrinfo, std::function<void(addrinfo *)>> servinfo
-                        (servinfo_p, [](addrinfo * servinfo) { freeaddrinfo(servinfo); });
+    int error =
+        getaddrinfo(ctx->name.c_str(), nullptr, &ctx->hints, &servinfo_p);
+    std::unique_ptr<addrinfo, std::function<void(addrinfo *)>> servinfo(
+        servinfo_p, [](addrinfo *servinfo) {
+            if (servinfo != nullptr)
+                freeaddrinfo(servinfo);
+        });
     if (error) {
         // check the error variable and return the correct error
         ctx->logger->warn("getaddrinfo failed: %s", gai_strerror(error));
@@ -57,7 +61,6 @@ void resolve_async(ResolverContext *context) {
             [callback]() { callback(ResolverError(), nullptr); });
         return;
     }
-
     assert(servinfo != nullptr);
 
     void *addr_ptr;
@@ -66,21 +69,22 @@ void resolve_async(ResolverContext *context) {
         Answer answer;
         answer.name = ctx->name;
         answer.qclass = ctx->dns_class;
+        assert(p->ai_family == AF_INET or p->ai_family == AF_INET6);
         if (p->ai_family == AF_INET) {
             answer.type = QueryTypeId::A;
             addr_ptr = &((sockaddr_in *)p->ai_addr)->sin_addr;
         } else if (p->ai_family == AF_INET6) {
             answer.type = QueryTypeId::AAAA;
             addr_ptr = &((sockaddr_in6 *)p->ai_addr)->sin6_addr;
-        } else {
-            ctx->reactor->call_soon(
-                [callback]() { callback(ResolverError(), nullptr); });
-            return;
+        }
+        if (p->ai_canonname != nullptr) {
+            answer.hostname = p->ai_canonname;
         }
         if (inet_ntop(p->ai_family, addr_ptr, address, sizeof(address)) ==
             nullptr) {
             ctx->logger->warn("dns: unexpected inet_ntop failure");
-            throw std::runtime_error("Unexpected inet_ntop failure");
+            ctx->reactor->call_soon(
+                [=]() { callback(ResolverError(), nullptr); });
         }
         if (p->ai_family == AF_INET) {
             answer.ipv4 = std::string(address);
@@ -92,21 +96,18 @@ void resolve_async(ResolverContext *context) {
     ctx->reactor->call_soon([=]() { callback(NoError(), message); });
 }
 
-template<MK_MOCK(getaddrinfo), MK_MOCK(inet_ntop)>
-void system_resolver(QueryClass dns_class, QueryType dns_type,
-                            std::string name, Callback<Error, Var<Message>> cb,
-                            Settings settings, Var<Reactor> reactor,
-                            Var<Logger> logger) {
-    std::unique_ptr<ResolverContext> ctx(new ResolverContext(dns_class, dns_type, name, cb,
-                                               settings, reactor, logger));
+template <MK_MOCK(getaddrinfo), MK_MOCK(inet_ntop)>
+void system_resolver(QueryClass dns_class, QueryType dns_type, std::string name,
+                     Callback<Error, Var<Message>> cb, Settings settings,
+                     Var<Reactor> reactor, Var<Logger> logger) {
+    std::unique_ptr<ResolverContext> ctx(new ResolverContext(
+        dns_class, dns_type, name, cb, settings, reactor, logger));
     Query query;
-    ctx->hints.ai_flags = AI_ALL;
+    ctx->hints.ai_flags = AI_ALL | AI_V4MAPPED | AI_CANONNAME;
     ctx->hints.ai_socktype = SOCK_STREAM;
 
     if (dns_class != QueryClassId::IN) {
-        reactor->call_soon([=]() {
-            cb(UnsupportedClassError(), nullptr);
-        });
+        reactor->call_soon([=]() { cb(UnsupportedClassError(), nullptr); });
         return;
     }
 
@@ -115,9 +116,7 @@ void system_resolver(QueryClass dns_class, QueryType dns_type,
     } else if (dns_type == QueryTypeId::AAAA) {
         ctx->hints.ai_family = AF_INET6;
     } else {
-        reactor->call_soon([=]() {
-            cb(UnsupportedClassError(), nullptr);
-        });
+        reactor->call_soon([=]() { cb(UnsupportedClassError(), nullptr); });
         return;
     }
 
@@ -127,7 +126,8 @@ void system_resolver(QueryClass dns_class, QueryType dns_type,
 
     ctx->message->queries.push_back(query);
 
-    std::async(std::launch::async, resolve_async<getaddrinfo, inet_ntop>, ctx.release());
+    std::async(std::launch::async, resolve_async<getaddrinfo, inet_ntop>,
+               ctx.release());
 }
 
 } // namespace dns
--- test/dns/system_resolver.cpp
+++ test/dns/system_resolver.cpp
@@ -14,81 +14,113 @@ static const char *null_inet_ntop(int, const void *, char *, socklen_t) {
     return nullptr;
 }
 
-static int null_getaddrinfo(const char *, const char *, const struct addrinfo *, struct addrinfo **) {
+static int null_getaddrinfo(const char *, const char *, const struct addrinfo *,
+                            struct addrinfo **) {
     return 10;
 }
 
 TEST_CASE("the system resolver is able to resolve an ipv4 address") {
     loop_with_initial_event([]() {
-            system_resolver("IN", "A", "www.neubot.org", [](Error e, Var<Message> message) {
-                    REQUIRE(!e);
-                    REQUIRE(message->answers.size() == 1);
-                    REQUIRE(message->answers[0].ipv4 == "130.192.16.172");
-                    break_loop();
-                    }, {}, Reactor::global(), Logger::global());
-            });
+        system_resolver("IN", "A", "www.neubot.org",
+                        [](Error e, Var<Message> message) {
+                            REQUIRE(!e);
+                            REQUIRE(message->answers.size() == 1);
+                            REQUIRE(message->answers[0].ipv4 ==
+                                    "130.192.16.172");
+                            break_loop();
+                        },
+                        {}, Reactor::global(), Logger::global());
+    });
 }
 
 TEST_CASE("the system resolver returns an error with an invalid_site") {
     loop_with_initial_event([]() {
-            system_resolver("IN", "A", "invalid_site", [](Error e, Var<Message>) {
-                    REQUIRE(e);
-                    break_loop();
-                    }, {}, Reactor::global(), Logger::global());
-            });
+        system_resolver("IN", "A", "invalid_site",
+                        [](Error e, Var<Message>) {
+                            REQUIRE(e);
+                            break_loop();
+                        },
+                        {}, Reactor::global(), Logger::global());
+    });
 }
 
 TEST_CASE("the system resolver can handle a getaddrinfo error") {
     loop_with_initial_event([]() {
-            system_resolver<null_getaddrinfo>("IN", "A", "www.neubot.org", [](Error e, Var<Message>) {
-                    REQUIRE(e);
-                    break_loop();
-                    }, {}, Reactor::global(), Logger::global());
-            });
+        system_resolver<null_getaddrinfo>("IN", "A", "www.neubot.org",
+                                          [](Error e, Var<Message>) {
+                                              REQUIRE(e);
+                                              break_loop();
+                                          },
+                                          {}, Reactor::global(),
+                                          Logger::global());
+    });
 }
 
 TEST_CASE("the system resolver can handle a inet_ntop error") {
     loop_with_initial_event([]() {
-            system_resolver<getaddrinfo, null_inet_ntop>("IN", "A", "invalid_site", [](Error e, Var<Message>) {
-                    REQUIRE(e);
-                    break_loop();
-                    }, {}, Reactor::global(), Logger::global());
-            });
+        system_resolver<getaddrinfo, null_inet_ntop>("IN", "A", "invalid_site",
+                                                     [](Error e, Var<Message>) {
+                                                         REQUIRE(e);
+                                                         break_loop();
+                                                     },
+                                                     {}, Reactor::global(),
+                                                     Logger::global());
+    });
 }
 
 TEST_CASE("the system resolver can handle an unsupported class") {
     loop_with_initial_event([]() {
-            system_resolver("CS", "A", "neubot.org", [](Error e, Var<Message>) {
-                    REQUIRE(e);
-                    break_loop();
-                    }, {}, Reactor::global(), Logger::global());
-            });
+        system_resolver("CS", "A", "neubot.org",
+                        [](Error e, Var<Message>) {
+                            REQUIRE(e);
+                            break_loop();
+                        },
+                        {}, Reactor::global(), Logger::global());
+    });
 }
 
 TEST_CASE("the system resolver can handle an unsupported query type") {
     loop_with_initial_event([]() {
-            system_resolver("IN", "NS", "neubot.org", [](Error e, Var<Message>) {
-                    REQUIRE(e);
-                    break_loop();
-                    }, {}, Reactor::global(), Logger::global());
-            });
+        system_resolver("IN", "NS", "neubot.org",
+                        [](Error e, Var<Message>) {
+                            REQUIRE(e);
+                            break_loop();
+                        },
+                        {}, Reactor::global(), Logger::global());
+    });
 }
 
 TEST_CASE("the system resolver is able to resolve an ipv6 address") {
     loop_with_initial_event([]() {
-            system_resolver("IN", "AAAA", "ooni.torproject.org", [](Error e, Var<Message> message) {
-                    REQUIRE(!e);
-                    REQUIRE(message->answers.size() > 0);
-                    auto found = false;
-                    for (auto answer : message->answers) {
-			std::cout << answer.ipv6 << std::endl;
-                        if (answer.ipv6 == "2001:858:2:2:aabb::563b:1e28" or
-                            answer.ipv6 == "2001:858:2:2:aabb:0:563b:1e28") {
-                                found = true;
-                        }
+        system_resolver(
+            "IN", "AAAA", "ooni.torproject.org",
+            [](Error e, Var<Message> message) {
+                REQUIRE(!e);
+                REQUIRE(message->answers.size() > 0);
+                auto found = false;
+                for (auto answer : message->answers) {
+                    if (answer.ipv6 == "2001:858:2:2:aabb::563b:1e28" or
+                        answer.ipv6 == "2001:858:2:2:aabb:0:563b:1e28") {
+                        found = true;
                     }
-                    REQUIRE(found);
-                    break_loop();
-                    }, {}, Reactor::global(), Logger::global());
-            });
+                }
+                REQUIRE(found);
+                break_loop();
+            },
+            {}, Reactor::global(), Logger::global());
+    });
+}
+
+TEST_CASE("the system resolver is able to resolve the canonical name") {
+    loop_with_initial_event([]() {
+        system_resolver("IN", "A", "ipv4.google.com",
+                        [](Error e, Var<Message> message) {
+                            REQUIRE(!e);
+                            REQUIRE(message->answers.size() > 0);
+                            REQUIRE(message->answers[0].hostname ==
+                                    "ipv4.l.google.com");
+                            break_loop();
+                        },
+                        {}, Reactor::global(), Logger::global());
+    });
 }
