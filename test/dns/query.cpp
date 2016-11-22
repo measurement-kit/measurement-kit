measurement-kit/measurement-kit

commit 1ee3803dfd21d662be18a5b0285c13b0227878f7

Fix query sent to link-local IPv6 resolvers like one in OnionSpace
Passes `works on my machine` test while being at OnionSpace:

```
./measurement_kit dns_query -vvN fe80::227:22ff:fe45:3a92%wlp1s0 geoip.ubuntu.com
```

src/libmeasurement_kit/libevent/dns_impl.hpp
@@ -11,6 +11,7 @@
 
 #include <cassert>
 #include <new>
+#include <memory>
 #include <limits.h>
 #include <type_traits>
 
@@ -53,37 +54,66 @@ class QueryContext : public NonMovable, public NonCopyable {
     }
 };
 
+struct evdns_base_deleter {
+    void operator()(evdns_base* p) {
+        constexpr int fail_requests = 1;
+        evdns_base_free(p, fail_requests);
+    }
+};
+
+struct evaddrinfo_deleter {
+    void operator()(evutil_addrinfo* p) {
+        evutil_freeaddrinfo(p);
+    }
+};
+
+using evdns_base_uptr = std::unique_ptr<evdns_base, evdns_base_deleter>;
+using evaddrinfo_uptr = std::unique_ptr<evutil_addrinfo, evaddrinfo_deleter>;
+
 template <MK_MOCK(evdns_base_new), MK_MOCK(evdns_base_nameserver_ip_add),
         MK_MOCK(evdns_base_free), MK_MOCK(evdns_base_set_option)>
 static inline evdns_base *create_evdns_base(
         Settings settings, Var<Reactor> reactor = Reactor::global()) {
 
-    evdns_base *base;
     event_base *evb = reactor->get_event_base();
-
-    if (settings.find("dns/nameserver") != settings.end()) {
-        if ((base = evdns_base_new(evb, 0)) == nullptr) {
-            throw std::bad_alloc();
-        }
-        if (evdns_base_nameserver_ip_add(
-                    base, settings["dns/nameserver"].c_str()) != 0) {
-            evdns_base_free(base, 1);
-            throw std::runtime_error("Cannot set server address");
-        }
-    } else if ((base = evdns_base_new(evb, 1)) == nullptr) {
+    const int initialize_nameservers = settings.count("dns/nameserver") ? 0 : 1;
+    evdns_base_uptr base(evdns_base_new(evb, initialize_nameservers));
+    if (!base) {
         throw std::bad_alloc();
+    }
+
+    if (settings.count("dns/nameserver")) {
+        // libevent can't handle link-local IPv6 nameserver in
+        // evdns_base_nameserver_ip_add, and there is no way to parse alike
+        // addresses in platform-independent way, so that's why getaddrinfo().
+        // fe80::227:22ff:fe45:3a92%wlan0 -- Android
+        // fe80::227:22ff:fe45:3a92%wlp1s0 -- Ubuntu Linux
+        evutil_addrinfo hints = { };
+        hints.ai_family = PF_UNSPEC;
+        hints.ai_flags = EVUTIL_AI_NUMERICSERV | EVUTIL_AI_NUMERICHOST;
+        hints.ai_socktype = SOCK_DGRAM;
+        evutil_addrinfo *res = nullptr;
+        const int eaierr = evutil_getaddrinfo(settings["dns/nameserver"].c_str(), "53", &hints, &res);
+        evaddrinfo_uptr ai(res);
+        if (eaierr) {
+            throw std::runtime_error(std::string("Cannot parse server address: ") + evutil_gai_strerror(eaierr));
+        }
+
+        constexpr unsigned unused = 0; // libevent API does not use it :-/
+        const int nserr = evdns_base_nameserver_sockaddr_add(base.get(), ai->ai_addr, ai->ai_addrlen, unused);
+        if (nserr != 0) {
+            throw std::runtime_error("Cannot set server address: " + std::to_string(nserr));
+        }
     }
 
     if (settings.find("dns/attempts") != settings.end() &&
             evdns_base_set_option(
-                    base, "attempts", settings["dns/attempts"].c_str()) != 0) {
-        evdns_base_free(base, 1);
+                    base.get(), "attempts", settings["dns/attempts"].c_str()) != 0) {
         throw std::runtime_error("Cannot set 'attempts' option");
     }
     if (settings.find("dns/timeout") != settings.end() &&
             evdns_base_set_option(
-                    base, "timeout", settings["dns/timeout"].c_str()) != 0) {
-        evdns_base_free(base, 1);
+                    base.get(), "timeout", settings["dns/timeout"].c_str()) != 0) {
         throw std::runtime_error("Cannot set 'timeout' option");
     }
 
@@ -93,12 +123,11 @@ static inline evdns_base *create_evdns_base(
     if (settings.find("dns/randomize_case") != settings.end()) {
         randomiz = settings["dns/randomize_case"];
     }
-    if (evdns_base_set_option(base, "randomize-case", randomiz.c_str()) != 0) {
-        evdns_base_free(base, 1);
+    if (evdns_base_set_option(base.get(), "randomize-case", randomiz.c_str()) != 0) {
         throw std::runtime_error("Cannot set 'randomize-case' option");
     }
 
-    return base;
+    return base.release();
 }
 
 template <MK_MOCK(inet_ntop)>

commit 786a910c79e4ce18d0d27dcf7bf8680bdca433ec

Support IPv6 link-local resolver (#915)
* dns_query: allow to pass dns nameserver with -N

Should help @darkk with understanding $bugs like this:

```
./measurement_kit dns_query -vvN fd00::3631:c4ff:fecd:f7ab geoip.ubuntu.com
```

Have fun! :-)

* Fix query sent to link-local IPv6 resolvers like one in OnionSpace

Passes `works on my machine` test while being at OnionSpace:

```
./measurement_kit dns_query -vvN fe80::227:22ff:fe45:3a92%wlp1s0 geoip.ubuntu.com
```

* Fix test/dns/query broken since 1ee3803d

* Breaking changes! Add separate dns/port option

The reason is that blind concatenation of `nameserver + ":53"` will lead
to hidden bug using IPv6 resolver.

* dns.md: also add port in the example

* Zap two empty lines

src/libmeasurement_kit/libevent/dns_impl.hpp
@@ -11,6 +11,7 @@
 
 #include <cassert>
 #include <new>
+#include <memory>
 #include <limits.h>
 #include <type_traits>
 
@@ -53,37 +54,70 @@ class QueryContext : public NonMovable, public NonCopyable {
     }
 };
 
-template <MK_MOCK(evdns_base_new), MK_MOCK(evdns_base_nameserver_ip_add),
-        MK_MOCK(evdns_base_free), MK_MOCK(evdns_base_set_option)>
+struct evdns_base_deleter {
+    void operator()(evdns_base* p) {
+        constexpr int fail_requests = 1;
+        evdns_base_free(p, fail_requests);
+    }
+};
+
+struct evaddrinfo_deleter {
+    void operator()(evutil_addrinfo* p) {
+        evutil_freeaddrinfo(p);
+    }
+};
+
+using evdns_base_uptr = std::unique_ptr<evdns_base, evdns_base_deleter>;
+using evaddrinfo_uptr = std::unique_ptr<evutil_addrinfo, evaddrinfo_deleter>;
+
+template <MK_MOCK(evdns_base_new), MK_MOCK(evdns_base_nameserver_sockaddr_add),
+        typename evdns_base_uptr = evdns_base_uptr, MK_MOCK(evdns_base_set_option)>
 static inline evdns_base *create_evdns_base(
         Settings settings, Var<Reactor> reactor = Reactor::global()) {
 
-    evdns_base *base;
     event_base *evb = reactor->get_event_base();
-
-    if (settings.find("dns/nameserver") != settings.end()) {
-        if ((base = evdns_base_new(evb, 0)) == nullptr) {
-            throw std::bad_alloc();
-        }
-        if (evdns_base_nameserver_ip_add(
-                    base, settings["dns/nameserver"].c_str()) != 0) {
-            evdns_base_free(base, 1);
-            throw std::runtime_error("Cannot set server address");
-        }
-    } else if ((base = evdns_base_new(evb, 1)) == nullptr) {
+    const int initialize_nameservers = settings.count("dns/nameserver") ? 0 : 1;
+    evdns_base_uptr base(evdns_base_new(evb, initialize_nameservers));
+    if (!base) {
         throw std::bad_alloc();
+    }
+
+    if (!initialize_nameservers) {
+        // libevent can't handle link-local IPv6 nameserver in
+        // evdns_base_nameserver_ip_add, and there is no way to parse alike
+        // addresses in platform-independent way, so that's why getaddrinfo().
+        // fe80::227:22ff:fe45:3a92%wlan0 -- Android
+        // fe80::227:22ff:fe45:3a92%wlp1s0 -- Ubuntu Linux
+        evutil_addrinfo hints = { };
+        hints.ai_family = PF_UNSPEC;
+        hints.ai_flags = EVUTIL_AI_NUMERICSERV | EVUTIL_AI_NUMERICHOST;
+        hints.ai_socktype = SOCK_DGRAM;
+        evutil_addrinfo *res = nullptr;
+        std::string port{"53"};
+        if (settings.count("dns/port")) {
+            port = settings["dns/port"];
+        }
+        const int eaierr = evutil_getaddrinfo(settings["dns/nameserver"].c_str(), port.c_str(), &hints, &res);
+        evaddrinfo_uptr ai(res);
+        if (eaierr) {
+            throw std::runtime_error(std::string("Cannot parse server address: ") + evutil_gai_strerror(eaierr));
+        }
+
+        constexpr unsigned unused = 0; // libevent API does not use it :-/
+        const int nserr = evdns_base_nameserver_sockaddr_add(base.get(), ai->ai_addr, ai->ai_addrlen, unused);
+        if (nserr != 0) {
+            throw std::runtime_error("Cannot set server address: " + std::to_string(nserr));
+        }
     }
 
     if (settings.find("dns/attempts") != settings.end() &&
             evdns_base_set_option(
-                    base, "attempts", settings["dns/attempts"].c_str()) != 0) {
-        evdns_base_free(base, 1);
+                    base.get(), "attempts", settings["dns/attempts"].c_str()) != 0) {
         throw std::runtime_error("Cannot set 'attempts' option");
     }
     if (settings.find("dns/timeout") != settings.end() &&
             evdns_base_set_option(
-                    base, "timeout", settings["dns/timeout"].c_str()) != 0) {
-        evdns_base_free(base, 1);
+                    base.get(), "timeout", settings["dns/timeout"].c_str()) != 0) {
         throw std::runtime_error("Cannot set 'timeout' option");
     }
 
@@ -93,20 +127,17 @@ static inline evdns_base *create_evdns_base(
     if (settings.find("dns/randomize_case") != settings.end()) {
         randomiz = settings["dns/randomize_case"];
     }
-    if (evdns_base_set_option(base, "randomize-case", randomiz.c_str()) != 0) {
-        evdns_base_free(base, 1);
+    if (evdns_base_set_option(base.get(), "randomize-case", randomiz.c_str()) != 0) {
         throw std::runtime_error("Cannot set 'randomize-case' option");
     }
 
-    return base;
+    return base.release();
 }
 
 template <MK_MOCK(inet_ntop)>
 static inline std::vector<Answer> build_answers_evdns(
         int code, char type, int count, int ttl, void *addresses,
         Var<Logger> logger = Logger::global()) {
-
-
 
     std::vector<Answer> answers;
 

src/libmeasurement_kit/ooni/templates.cpp
@@ -21,13 +21,15 @@ void dns_query(Var<Entry> entry, dns::QueryType query_type,
     std::string resolver_hostname, nameserver_part;
     std::stringstream nameserver_ss(nameserver);
 
+    // TODO: Fix IPv6 `nameserver`, use google's [2001:4860:4860::8888]:53 for testing.
     std::getline(nameserver_ss, nameserver_part, ':');
     resolver_hostname = nameserver_part;
 
     std::getline(nameserver_ss, nameserver_part, ':');
     resolver_port = std::stoi(nameserver_part, nullptr);
 
-    options["dns/nameserver"] = nameserver;
+    options["dns/nameserver"] = resolver_hostname;
+    options["dns/port"] = resolver_port;
     options["dns/attempts"] = 1;
 
     dns::query(query_class, query_type, query_name,

src/measurement_kit/cmdline/dns_query.cpp
@@ -12,17 +12,21 @@ namespace cmdline {
 namespace dns_query {
 
 static const char *kv_usage =
-    "usage: measurement_kit dns_query [-v] [-c class] [-t type] domain\n";
+    "usage: measurement_kit dns_query [-N nameserver] [-v] [-c class] [-t type] domain\n";
 
 int main(const char *, int argc, char **argv) {
 
+    std::string nameserver = "";
     std::string query_class = "IN";
     int ch;
     std::string query_type = "A";
-    while ((ch = mkp_getopt(argc, argv, "c:t:v")) != -1) {
+    while ((ch = mkp_getopt(argc, argv, "c:N:t:v")) != -1) {
         switch (ch) {
         case 'c':
             query_class = mkp_optarg;
+            break;
+        case 'N':
+            nameserver = mkp_optarg;
             break;
         case 't':
             query_type = mkp_optarg;
@@ -43,7 +47,12 @@ int main(const char *, int argc, char **argv) {
     }
     std::string domain = argv[0];
 
-    loop_with_initial_event([&query_class, &query_type, &domain]() {
+    Settings settings;
+    if (nameserver != "") {
+        settings["dns/nameserver"] = nameserver;
+    }
+
+    loop_with_initial_event([&query_class, &query_type, &domain, &settings]() {
         std::cout << query_class << " " << query_type << "\n";
         dns::query(query_class.data(), query_type.data(), domain,
             [&query_type](Error e, Var<dns::Message> m) {
@@ -62,7 +71,7 @@ int main(const char *, int argc, char **argv) {
                     }
                 }
                 break_loop();
-            });
+            }, settings);
     });
 
     return 0;

test/dns/query.cpp
@@ -36,7 +36,8 @@ static evdns_base *null_evdns_base_new(event_base *, int) {
     return (evdns_base *)nullptr;
 }
 
-static int null_evdns_base_nameserver_ip_add(evdns_base *, const char *) {
+static int null_evdns_base_nameserver_sockaddr_add(evdns_base *, const sockaddr *,
+                                                   ev_socklen_t, unsigned) {
     return -1;
 }
 
@@ -47,12 +48,15 @@ static int null_evdns_base_set_option_randomize(evdns_base *, const char *,
 
 #define BASE_FREE(name)                                                        \
     static bool base_free_##name##_flag = false;                               \
-    static void base_free_##name(struct evdns_base *base, int fail_requests) { \
-        ::evdns_base_free(base, fail_requests);                                \
-        base_free_##name##_flag = true;                                        \
-    }
+    struct evdns_base_##name##_deleter : private evdns_base_uptr::deleter_type { \
+        void operator()(evdns_base_uptr::pointer p) {                            \
+            evdns_base_uptr::deleter_type::operator()(p);                        \
+            base_free_##name##_flag = true;                                      \
+        }                                                                        \
+    };                                                                           \
+    using base_free_##name = std::unique_ptr<evdns_base_uptr::element_type, evdns_base_##name##_deleter>;
 
-BASE_FREE(evdns_base_nameserver_ip_add)
+BASE_FREE(evdns_base_nameserver_sockaddr_add)
 BASE_FREE(evdns_set_options_attempts)
 BASE_FREE(evdns_set_options_attempts_negative)
 BASE_FREE(evdns_set_options_timeout)
@@ -64,25 +68,33 @@ TEST_CASE("throw error while fails evdns_base_new") {
         std::bad_alloc);
 }
 
-TEST_CASE("throw error while fails evdns_base_nameserver_ip_add") {
+TEST_CASE("throw error on literal port") {
+    // NB: dns/port=128000 just overflows uint16
     REQUIRE_THROWS_AS(
-        (create_evdns_base<::evdns_base_new, null_evdns_base_nameserver_ip_add,
-                           base_free_evdns_base_nameserver_ip_add>(
-            {{"dns/nameserver", "nexa"}}, Reactor::global())),
+        (create_evdns_base(
+            {{"dns/nameserver", "8.8.8.8"}, {"dns/port", "domain"}}, Reactor::global())),
         std::runtime_error);
-    REQUIRE(base_free_evdns_base_nameserver_ip_add_flag);
 }
 
-TEST_CASE("throw error while fails evdns_base_nameserver_ip_add and base_new") {
+TEST_CASE("throw error while fails evdns_base_nameserver_sockaddr_add") {
+    REQUIRE_THROWS_AS(
+        (create_evdns_base<::evdns_base_new, null_evdns_base_nameserver_sockaddr_add,
+                           base_free_evdns_base_nameserver_sockaddr_add>(
+            {{"dns/nameserver", "nexa"}}, Reactor::global())),
+        std::runtime_error);
+    REQUIRE(base_free_evdns_base_nameserver_sockaddr_add_flag);
+}
+
+TEST_CASE("throw error while fails evdns_base_nameserver_sockaddr_add and base_new") {
     REQUIRE_THROWS_AS((create_evdns_base<null_evdns_base_new,
-                                         null_evdns_base_nameserver_ip_add>(
+                                         null_evdns_base_nameserver_sockaddr_add>(
                           {{"dns/nameserver", "nexa"}}, Reactor::global())),
                       std::bad_alloc);
 }
 
 TEST_CASE("throw error while fails evdns_set_options for attempts") {
     REQUIRE_THROWS_AS(
-        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_ip_add,
+        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_sockaddr_add,
                            base_free_evdns_set_options_attempts>(
             {{"dns/attempts", "nexa"}}, Reactor::global())),
         std::runtime_error);
@@ -91,7 +103,7 @@ TEST_CASE("throw error while fails evdns_set_options for attempts") {
 
 TEST_CASE("throw error while fails evdns_set_options for timeout") {
     REQUIRE_THROWS_AS(
-        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_ip_add,
+        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_sockaddr_add,
                            base_free_evdns_set_options_timeout>(
             {{"dns/attempts", "nexa"}}, Reactor::global())),
         std::runtime_error);
@@ -100,7 +112,7 @@ TEST_CASE("throw error while fails evdns_set_options for timeout") {
 
 TEST_CASE("throw error while fails evdns_set_options for negative attempts") {
     REQUIRE_THROWS_AS(
-        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_ip_add,
+        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_sockaddr_add,
                            base_free_evdns_set_options_attempts_negative>(
             {{"dns/attempts", -1}}, Reactor::global())),
         std::runtime_error);
@@ -109,7 +121,7 @@ TEST_CASE("throw error while fails evdns_set_options for negative attempts") {
 
 TEST_CASE("throw error while fails evdns_set_options for randomize-case") {
     REQUIRE_THROWS_AS(
-        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_ip_add,
+        (create_evdns_base<::evdns_base_new, ::evdns_base_nameserver_sockaddr_add,
                            base_free_evdns_set_options_randomize,
                            null_evdns_base_set_option_randomize>(
             {{"dns/randomize_case", ""}}, Reactor::global())),