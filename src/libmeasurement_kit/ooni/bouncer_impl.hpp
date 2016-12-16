1cf7e2378157c382eaebee1d2a827d5ff5bbf90f
Simone Basso <bassosimone@gmail.com>
2016-12-16 13:40:52 +0100

    clang-format -i

--- src/libmeasurement_kit/ooni/bouncer_impl.hpp
+++ src/libmeasurement_kit/ooni/bouncer_impl.hpp
@@ -12,9 +12,11 @@ namespace bouncer {
 
 template <MK_MOCK_NAMESPACE(http, request)>
 void post_net_tests_impl(std::string base_bouncer_url, std::string test_name,
-                    std::string test_version, std::list<std::string> helpers,
-                    Callback<Error, Var<BouncerReply>> cb, Settings settings,
-                    Var<Reactor> reactor, Var<Logger> logger) {
+                         std::string test_version,
+                         std::list<std::string> helpers,
+                         Callback<Error, Var<BouncerReply>> cb,
+                         Settings settings, Var<Reactor> reactor,
+                         Var<Logger> logger) {
 
     nlohmann::json request;
     nlohmann::json net_tests;
@@ -26,22 +28,23 @@ void post_net_tests_impl(std::string base_bouncer_url, std::string test_name,
 
     settings["http/url"] = base_bouncer_url;
     settings["http/method"] = "POST";
-    http_request(
-        settings, {{"Content-Type", "application/json"}}, request.dump(),
-        [=](Error error, Var<http::Response> resp) {
-            if (error) {
-                cb(error, nullptr);
-                return;
-            }
+    http_request(settings, {{"Content-Type", "application/json"}},
+                 request.dump(),
+                 [=](Error error, Var<http::Response> resp) {
+                     if (error) {
+                         cb(error, nullptr);
+                         return;
+                     }
 
-            ErrorOr<Var<BouncerReply>> reply(BouncerReply::create(resp->body, logger));
-            if (!reply) {
-                cb(reply.as_error(), nullptr);
-                return;
-            }
-            cb(NoError(), *reply);
-        },
-        reactor, logger, nullptr, 0);
+                     ErrorOr<Var<BouncerReply>> reply(
+                         BouncerReply::create(resp->body, logger));
+                     if (!reply) {
+                         cb(reply.as_error(), nullptr);
+                         return;
+                     }
+                     cb(NoError(), *reply);
+                 },
+                 reactor, logger, nullptr, 0);
 }
 
 } // namespace mk
