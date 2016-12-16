a46b1086442f485e3d7ccbc77071ba54ace31ba2
Antonio Langiu <antonio.langiu@studenti.polito.it>
2016-12-16 12:37:56 +0100

    remove general noexception function

--- include/measurement_kit/common/noexceptions.hpp
+++ include/measurement_kit/common/noexceptions.hpp
@@ -4,42 +4,27 @@
 #ifndef MEASUREMENT_KIT_COMMON_NOEXCEPTIONS_HPP
 #define MEASUREMENT_KIT_COMMON_NOEXCEPTIONS_HPP
 
-#include <measurement_kit/common/error_or.hpp>
 #include <cassert>
+#include <measurement_kit/common/error_or.hpp>
 
 namespace mk {
 
-
 // this is a specific implementation for json
-template <typename T>
-ErrorOr<T> json_noexcept (std::function<T()> fn) {
-	try {
-		return fn();
+template <typename T> ErrorOr<T> json_noexcept(std::function<T()> fn) {
+    try {
+        return fn();
     } catch (std::invalid_argument &) {
         return JsonParseError();
     } catch (std::out_of_range &) {
         return JsonKeyError();
     } catch (std::domain_error &) {
         return JsonDomainError();
-    } catch (Error& err) {
+    } catch (Error &err) {
         return err;
-    } catch (std::exception& ex) {
-        return GenericError(); // define a specific one
+    } catch (std::exception &ex) {
+        return UncaughtExceptionError(); // define a specific one
     }
     // you should never be here!
-    assert(false);
-}
-
-template <typename T>
-ErrorOr<T> noexceptions (std::function<T()> fn) {
-	try {
-		return fn();
-    } catch (Error& err) {
-        return err;
-	} catch (std::exception& ex) {
-        return GenericError(); // define a specific one
-	}
-    // you should never be here
     assert(false);
 }
 
