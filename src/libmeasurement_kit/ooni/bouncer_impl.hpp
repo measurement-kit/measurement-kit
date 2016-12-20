3edc2be891e5a8b69a8a9277cc622f90ddda53c7
Simone Basso <bassosimone@gmail.com>
2016-12-20 15:00:48 +0200

    Refine and finish bouncer implementation (#1014)
    * The _impl suffix should only be used by implementation headers
    
    * Stop using using
    
    * More cosmetic changes
    
    * More cosmetics
    
    * Add include guards
    
    * Add some public bits
    
    * More robust JSON parsing
    
    The amount of code enclosed by try..catch is so small that catching all
    kind of exceptions seems more robust than offensive.
    
    > When in doubt, use brute force
    
    (Seriously, our time is better spent doing other stuff such as debugging
    the crash iOS app rather than being precise with exceptions here.)
    
    * Set content-type
    
    * clang-format -i
    
    * clang-format -i
    
    * Let base-url be a real base URL
    
    * Never use the global reactor
    
    * Mock case where HTTP fails due to network error
    
    * Cleanup up namespaces a bit
    
    * bouncer: Attempt to arrive at coverage 100%
    
    * Gimme a bit more coverage
    
    * Try to cover one more line
    
    * Fix typo
    
    * More typo fixing
    
    * Unicorns and typo fixes

--- src/libmeasurement_kit/ooni/bouncer_impl.hpp
+++ src/libmeasurement_kit/ooni/bouncer_impl.hpp
@@ -1,67 +1,91 @@
 // Part of measurement-kit <https://measurement-kit.github.io/>.
 // Measurement-kit is free software. See AUTHORS and LICENSE for more
 // information on the copying conditions.
+#ifndef SRC_LIBMEASUREMENT_KIT_OONI_BOUNCER_IMPL_HPP
+#define SRC_LIBMEASUREMENT_KIT_OONI_BOUNCER_IMPL_HPP
 
-#include <measurement_kit/ext.hpp>
-#include <measurement_kit/http.hpp>
 #include <measurement_kit/ooni.hpp>
-
-using json = nlohmann::json;
 
 namespace mk {
 namespace ooni {
 namespace bouncer {
 
-class BouncerReply {
- public:
-    json response;
+static inline nlohmann::json json_parse(std::string s) {
+    return nlohmann::json::parse(s);
+}
 
-    static ErrorOr<Var<BouncerReply>> create(std::string, Var<Logger>);
-
-    ErrorOr<std::string> get_collector();
-    ErrorOr<std::string> get_collector_alternate(std::string type);
-    ErrorOr<std::string> get_name();
-    ErrorOr<std::string> get_test_helper(std::string name);
-    ErrorOr<std::string> get_test_helper_alternate(std::string name, std::string type);
-    ErrorOr<std::string> get_version();
- private:
-    json get_base();
-};
+template <MK_MOCK(json_parse)>
+ErrorOr<Var<BouncerReply>> create_impl(std::string data, Var<Logger> logger) {
+    Var<BouncerReply> reply{new BouncerReply};
+    try {
+        reply->response = json_parse(data);
+        if (reply->response.find("error") != reply->response.end()) {
+            if (reply->response["error"] == "collector-not-found") {
+                logger->warn("collector not found for the requested test");
+                return BouncerCollectorNotFoundError();
+            }
+            if (reply->response["error"] == "invalid-request") {
+                logger->warn("invalid request sent to the bouncer");
+                return BouncerInvalidRequestError();
+            }
+            // I assume that if we receive a json with the key "error"
+            // then the json has an unknown schema and we should not
+            // continue to process it
+            logger->warn("bouncer generic error");
+            return BouncerGenericError();
+        }
+        if (reply->response["net-tests"].empty()) {
+            logger->warn("generic bouncer error");
+            return BouncerTestHelperNotFoundError();
+        }
+    } catch (std::invalid_argument &) {
+        return JsonParseError();
+    } catch (std::out_of_range &) {
+        return JsonKeyError();
+    } catch (std::domain_error &) {
+        return JsonDomainError();
+    }
+    return reply;
+}
 
 template <MK_MOCK_NAMESPACE(http, request)>
 void post_net_tests_impl(std::string base_bouncer_url, std::string test_name,
-                    std::string test_version, std::list<std::string> helpers,
-                    Callback<Error, Var<BouncerReply>> cb, Settings settings,
-                    Var<Reactor> reactor, Var<Logger> logger) {
+                         std::string test_version,
+                         std::list<std::string> helpers,
+                         Callback<Error, Var<BouncerReply>> cb,
+                         Settings settings, Var<Reactor> reactor,
+                         Var<Logger> logger) {
 
-    json request;
-    json net_tests;
+    nlohmann::json request;
+    nlohmann::json net_tests;
     net_tests["input-hashes"] = {};
     net_tests["name"] = test_name;
     net_tests["test-helpers"] = helpers;
     net_tests["version"] = test_version;
     request["net-tests"][0] = net_tests;
 
-    settings["http/url"] = base_bouncer_url;
+    settings["http/url"] = base_bouncer_url + "/net-tests";
     settings["http/method"] = "POST";
-    http_request(
-        settings, {}, request.dump(),
-        [=](Error e, Var<http::Response> resp) {
-            if (e) {
-                cb(e, nullptr);
-                return;
-            }
+    http_request(settings, {{"Content-Type", "application/json"}},
+                 request.dump(),
+                 [=](Error error, Var<http::Response> resp) {
+                     if (error) {
+                         cb(error, nullptr);
+                         return;
+                     }
 
-            ErrorOr<Var<BouncerReply>> reply(BouncerReply::create(resp->body, logger));
-            if (!!reply) {
-                cb(NoError(), *reply);
-                return;
-            }
-            cb(reply.as_error(), nullptr);
-        },
-        reactor, logger, nullptr, 0);
+                     ErrorOr<Var<BouncerReply>> reply(
+                         BouncerReply::create(resp->body, logger));
+                     if (!reply) {
+                         cb(reply.as_error(), nullptr);
+                         return;
+                     }
+                     cb(NoError(), *reply);
+                 },
+                 reactor, logger, nullptr, 0);
 }
 
-} // namespace mk
-} // namespace ooni
 } // namespace bouncer
+} // namespace ooni
+} // namespace mk
+#endif
