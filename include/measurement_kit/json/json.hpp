c94d44099f2cfc25de14c2920f2a13ab9197f9c7
Antonio Langiu <antonio.langiu@studenti.polito.it>
2016-12-28 12:21:47 +0100

    create wrapper for json

---
+++ include/measurement_kit/json/json.hpp
@@ -0,0 +1,15 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+#ifndef MEASUREMENT_KIT_JSON_JSON_HPP
+#define MEASUREMENT_KIT_JSON_JSON_HPP
+
+namespace mk {
+namespace json {
+
+#include <measurement_kit/json/nhlohmann/json.hpp>
+
+} // namespace json
+} // namespace mk
+
+#endif
