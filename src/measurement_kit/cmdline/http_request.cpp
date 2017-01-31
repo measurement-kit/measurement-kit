c29c8bab0dba7f3c59f210221992d8d6cb05c1bc
Simone Basso <bassosimone@gmail.com>
2017-01-31 15:46:44 +0100

    Port test case from TheTorProject/ooni-probe#729

--- src/measurement_kit/cmdline/http_request.cpp
+++ src/measurement_kit/cmdline/http_request.cpp
@@ -12,18 +12,41 @@ namespace cmdline {
 namespace http_request {
 
 static const char *kv_usage =
-    "usage: measurement_kit http_request [-v] [-b body] [-m method]\n"
-    "                                    [-R max-redirect] url\n";
+    "usage: measurement_kit http_request [-v] [-b body] [-H 'key: value']\n"
+    "                                    [-m method] [-R max-redirect] url\n";
+
+static bool set_header(http::Headers &headers, const std::string option) {
+    auto kv = mk::split<std::vector<std::string>>(option, ":");
+    if (kv.size() != 2) {
+        std::cout << "invalid header: " << mkp_optarg << "\n";
+        return false;
+    }
+    auto key = kv[0];
+    auto value = kv[1];
+    // TODO: it would be nice to have {r,l}trim() in common.hpp
+    while (value.size() > 0 && value[0] == ' ') {
+        value = value.substr(1);
+    }
+    headers[key] = value;
+    return true;
+}
 
 int main(const char *, int argc, char **argv) {
 
     Settings settings;
     std::string body;
+    http::Headers headers;
     int ch;
-    while ((ch = mkp_getopt(argc, argv, "b:m:R:v")) != -1) {
+    while ((ch = mkp_getopt(argc, argv, "b:H:m:R:v")) != -1) {
         switch (ch) {
         case 'b':
             body = mkp_optarg;
+            break;
+        case 'H':
+            if (!set_header(headers, mkp_optarg)) {
+                exit(1);
+                // NOTREACHED
+            }
             break;
         case 'm':
             settings["http/method"] = mkp_optarg;
@@ -39,15 +62,13 @@ int main(const char *, int argc, char **argv) {
             exit(1);
         }
     }
-    argc -= mkp_optind;
-    argv += mkp_optind;
+    argc -= mkp_optind, argv += mkp_optind;
     if (argc != 1) {
         std::cout << kv_usage;
         exit(1);
     }
     settings["http/url"] = argv[0];
 
-    http::Headers headers;
     loop_with_initial_event([&]() {
         http::request(
             settings,
