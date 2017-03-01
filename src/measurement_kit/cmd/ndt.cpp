7dfa50c33b246157e0e99ef0619e9e5d17d162ee
Simone Basso <bassosimone@gmail.com>
2017-03-01 21:55:26 +0100

    Refactor cmdline to have OONI-like interface
    This should help the integration of MK as a OONI third party test.

---
+++ src/measurement_kit/cmd/ndt.cpp
@@ -0,0 +1,78 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+
+#include "../cmdline.hpp"
+#include <measurement_kit/ext/json.hpp>
+#include <measurement_kit/ndt.hpp>
+
+namespace ndt {
+
+#define USAGE                                                                  \
+    "usage: measurement_kit [options] ndt [-m metro] [-N tool] [-p port]\n"    \
+    "                       [-T phase] [host]\n"                               \
+    "\n"                                                                       \
+    "Available tool names for mlab-ns: ndt, neubot (default: ndt)\n"           \
+    "Available phases: download, download-ext, none, upload\n"                 \
+    "  (default: -T download -T upload)\n"
+
+int main(std::list<Callback<BaseTest &>> &initializers, int argc, char **argv) {
+    NdtTest test;
+    int test_suite = 0;
+    int use_default_test_suite = true;
+
+    for (int ch; (ch = getopt(argc, argv, "m:N:p:T:")) != -1;) {
+        switch (ch) {
+        case 'm':
+            test.set_options("mlabns/policy", "metro");
+            test.set_options("mlabns/metro", optarg);
+            break;
+        case 'N':
+            test.set_options("mlabns_tool_name", optarg);
+            break;
+        case 'p':
+            test.set_options("port", optarg);
+            break;
+        case 'T':
+            use_default_test_suite = false;
+            if (strcmp(optarg, "download") == 0) {
+                test_suite |= MK_NDT_DOWNLOAD;
+            } else if (strcmp(optarg, "download-ext") == 0) {
+                test_suite |= MK_NDT_DOWNLOAD_EXT;
+            } else if (strcmp(optarg, "none") == 0) {
+                test_suite = 0;
+            } else if (strcmp(optarg, "upload") == 0) {
+                test_suite |= MK_NDT_UPLOAD;
+            } else {
+                fprintf(stderr, "invalid parameter for -T option: %s", optarg);
+                exit(1);
+            }
+            break;
+        default:
+            fprintf(stderr, "%s\n", USAGE);
+            exit(1);
+            /* NOTREACHED */
+        }
+    }
+    argc -= optind, argv += optind;
+    if (argc > 1) {
+        fprintf(stderr, "%s\n", USAGE);
+        exit(1);
+        /* NOTREACHED */
+    }
+
+    // If the user expressed preference force test suite, otherwise the
+    // code would use the default test suite (download|upload).
+    if (!use_default_test_suite) {
+        test.set_options("test_suite", test_suite);
+    }
+
+    if (argc == 1) {
+        test.set_options("address", argv[0]);
+    }
+
+    ndt_init(initializers, test).run();
+    return 0;
+}
+
+} // namespace ndt
