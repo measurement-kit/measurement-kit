cdb93812ec3aae584e56d4ac9c3c64725632f753
Joe Landers <joe@joelanders.net>
2017-03-03 16:16:35 +0100

    add test for meek_fronted_requests

--- test/nettests/meek_fronted_requests.cpp
+++ test/nettests/meek_fronted_requests.cpp
@@ -6,36 +6,23 @@
 #define CATCH_CONFIG_MAIN
 #include "../src/libmeasurement_kit/ext/catch.hpp"
 
-#include <measurement_kit/nettests.hpp>
+#include "../nettests/utils.hpp"
 
-#include <chrono>
-#include <iostream>
-#include <thread>
-
+using namespace mk::nettests;
 using namespace mk;
 
 TEST_CASE("Synchronous meek-fronted-requests test") {
-    nettests::MeekFrontedRequestsTest{}
-//        .set_options("backend", "https://a.collector.test.ooni.io:4444")
-//        .set_options("geoip_country_path", "GeoIP.dat")
-//        .set_options("geoip_asn_path", "GeoIPASNum.dat")
-//        .set_options("nameserver", "8.8.8.8")
-//        .set_input_filepath("test/fixtures/urls.txt")
+    test::nettests::make_test<MeekFrontedRequestsTest>()
+        .set_options("expected_body", "I\u2019m just a happy little web server.\n")
+        .set_options("outer_host", "a0.awsstatic.com")
+        .set_options("inner_host", "d2zfqthxsdq309.cloudfront.net")
         .run();
 }
 
 TEST_CASE("Asynchronous meek-fronted-requests test") {
-    bool done = false;
-    nettests::MeekFrontedRequestsTest{}
-//        .set_options("backend", "https://a.collector.test.ooni.io:4444")
-//        .set_options("geoip_country_path", "GeoIP.dat")
-//        .set_options("geoip_asn_path", "GeoIPASNum.dat")
-//        .set_options("nameserver", "8.8.8.8")
-//        .set_input_filepath("test/fixtures/urls.txt")
-        .start([&done]() { done = true; });
-    do {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    } while (!done);
+    auto t = test::nettests::make_test<MeekFrontedRequestsTest>("meek_fronted_requests.txt");
+    t.runnable->needs_input = true;
+    test::nettests::run_async(t);
 }
 
 #else
