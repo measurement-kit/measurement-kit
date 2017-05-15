4d7215c588cfa0a54b898fdaa8edf3061aabe196
Simone Basso <bassosimone@gmail.com>
2017-05-15 15:05:21 +0200

    n/connect.cpp: compile with libevent v2.0.x
    This should repair the currently broken regress tests.
    
    See <https://travis-ci.org/measurement-kit/measurement-kit/jobs/232206094>.

--- src/libmeasurement_kit/net/connect.cpp
+++ src/libmeasurement_kit/net/connect.cpp
@@ -353,9 +353,25 @@ void connect(std::string address, int port,
                                     return;
                                 }
                                 if (*allow_dirty_shutdown == true) {
+                                    /*
+                                     * This useful libevent function is only
+                                     * available since libevent 2.1.0:
+                                     */
+#ifdef HAVE_BUFFEREVENT_OPENSSL_SET_ALLOW_DIRTY_SHUTDOWN
                                     bufferevent_openssl_set_allow_dirty_shutdown(
                                         bev, 1);
                                     logger->info("Allowing dirty SSL shutdown");
+#else
+                                    logger->warn("Cannot tell libevent to "
+                                                 "allow SSL dirty shutdowns "
+                                                 "as requested by the "
+                                                 "programmer: as a result"
+                                                 "some SSL connections may "
+                                                 "interrupt abruptly with an "
+                                                 "error. This happens because "
+                                                 "you are not using version "
+                                                 "2.1.x of libevent.");
+#endif
                                 }
                                 Var<Transport> txp =
                                     libevent::Connection::make(
