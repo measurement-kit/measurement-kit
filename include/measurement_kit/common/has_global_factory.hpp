98aff68e824d14b283ae69434d9d4dc1a7b0c18a
Simone Basso <bassosimone@gmail.com>
2017-05-17 17:41:09 +0200

    Add HasGlobalFactory implementation
    Helps to write less when you define a class where you have
    a factory that returns a singleton as a `Var`.

---
+++ include/measurement_kit/common/has_global_factory.hpp
@@ -0,0 +1,23 @@
+// Part of measurement-kit <https://measurement-kit.github.io/>.
+// Measurement-kit is free software. See AUTHORS and LICENSE for more
+// information on the copying conditions.
+#ifndef MEASUREMENT_KIT_COMMON_HAS_GLOBAL_FACTORY_HPP
+#define MEASUREMENT_KIT_COMMON_HAS_GLOBAL_FACTORY_HPP
+
+#include <measurement_kit/common/var.hpp>
+
+namespace mk {
+
+template <typename T> class HasGlobalFactory {
+  public:
+    template <typename... A> static Var<T> global(A &&... a) {
+        static Var<T> singleton;
+        if (!singleton) {
+            singleton = Var<T>::make(std::forward<A>(a)...);
+        }
+        return singleton;
+    }
+};
+
+} // namespace mk
+#endif
