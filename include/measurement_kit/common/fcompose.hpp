dc21df74a1918304721b44a749f690a1aa4c1892
Simone Basso <bassosimone@gmail.com>
2017-05-20 15:10:08 +0300

    Improve implementation of fcompose()
    Basically this program works:
    
    ```C++
    
    int main() {
        mk::fcompose(
            [](double d, int off) { return d * 2 + off; },
            [](double d) { return d + 1; },
            [](double d) { return std::make_tuple(3, std::to_string(d)); },
            [](int x, std::string s) {
                std::cout << x << " " << s << "\n";
            }
        )(3.17, 7);
    }
    ```
    
    It is now possible to chain an arbitrary number of functors; to pass
    any argument to them; to return tuples that are automatically unpacked
    to downstream functors as their arguments.

--- include/measurement_kit/common/fcompose.hpp
+++ include/measurement_kit/common/fcompose.hpp
@@ -4,23 +4,47 @@
 #ifndef MEASUREMENT_KIT_COMMON_FCOMPOSE_HPP
 #define MEASUREMENT_KIT_COMMON_FCOMPOSE_HPP
 
-#include <functional>
+#include <measurement_kit/common/fapply.hpp>
+#include <measurement_kit/common/fhead.hpp>
+#include <measurement_kit/common/ftail.hpp>
 
 namespace mk {
 
-/*
- * Note: with template magic it should be possible:
- *
- * 1. to make this a chain of callables so to avoid std::function
- *
- * 2. to extend this to an arbitrary number of callables
- *
- * Since this is a proof of concept, and I don't know yet how to
- * do 1. and 2., I've kept this simple for now.
- */
-template <typename R, typename... A, typename G>
-auto fcompose(std::function<R(A...)> f, G g) {
-    return [=](A &&... a) { return g(f(std::forward<A>(a)...)); };
+// applies to `g` the return value of `f(args...)`
+template <typename F, typename G> auto fcompose_impl_(F f, G g) {
+    return [f = std::move(f), g = std::move(g)](auto... args) {
+        /*
+         * We do this in two steps such that, if `f` returns a tuple,
+         * then the tuple is unpacked as the arguments to `g`.
+         */
+        return fapply(g, fapply(f, args...));
+    };
+}
+
+// pathologic corner case
+template <typename F, typename... G>
+auto fcompose(F f, std::tuple<G...> g, std::index_sequence<0>) {
+    return f;
+}
+
+// base case
+template <typename F, typename... G>
+auto fcompose(F f, std::tuple<G...> &&g, std::index_sequence<1>) {
+    return fcompose_impl_(f, fhead(g));
+}
+
+// generic case
+template <typename F, typename... G, std::size_t I,
+          typename = typename std::enable_if<(I >= 2)>::type>
+auto fcompose(F f, std::tuple<G...> &&g, std::index_sequence<I>) {
+    return fcompose(fcompose_impl_(f, fhead(g)), ftail(g),
+                    std::index_sequence<I - 1>{});
+}
+
+// convert G... into a tuple and start compile-time recursion
+template <typename F, typename... G> auto fcompose(F f, G &&... g) {
+    return fcompose(f, std::make_tuple<G...>(std::forward<G>(g)...),
+                    std::index_sequence<sizeof...(G)>{});
 }
 
 } // namespace mk
