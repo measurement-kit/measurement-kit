0bc481f73da75c0333a5f8dc09790a86f91d8ee2
Simone Basso <bassosimone@gmail.com>
2017-05-23 13:06:34 +0200

    We don't actually need ReturnType (#1239)

--- include/measurement_kit/common/locked.hpp
+++ include/measurement_kit/common/locked.hpp
@@ -4,23 +4,11 @@
 #ifndef MEASUREMENT_KIT_COMMON_LOCKED_HPP
 #define MEASUREMENT_KIT_COMMON_LOCKED_HPP
 
-#include <measurement_kit/common/callback.hpp>
-#include <measurement_kit/common/return_type.hpp>
-
 #include <mutex>
 
 namespace mk {
 
-template <typename Func, typename = typename std::enable_if<std::is_void<
-                             typename ReturnType<Func>::type>::value>::type>
-void locked(std::mutex &mutex, Func func) {
-    std::lock_guard<std::mutex> guard{mutex};
-    func();
-}
-
-template <typename Func, typename = typename std::enable_if<!std::is_void<
-                             typename ReturnType<Func>::type>::value>::type>
-typename ReturnType<Func>::type locked(std::mutex &mutex, Func func) {
+template <typename Func> auto locked(std::mutex &mutex, Func func) {
     std::lock_guard<std::mutex> guard{mutex};
     return func();
 }
