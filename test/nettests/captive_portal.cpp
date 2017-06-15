657a5dc50b473364827488677bf31294cab900d2
Joe Landers <joe@joelanders.net>
2017-06-15 08:52:27 -0400

    captive_portal test fills report

--- test/nettests/captive_portal.cpp
+++ test/nettests/captive_portal.cpp
@@ -13,29 +13,15 @@ using namespace mk::nettests;
 using namespace mk;
 using namespace mk::ooni;
 
-//TEST_CASE("Synchronous captive portal test") {
-//    test::nettests::make_test<CaptivePortalTest>()
-//        .run();
-//}
-//
-//TEST_CASE("Asynchronous captive portal test") {
-//    test::nettests::run_async(
-//        test::nettests::make_test<CaptivePortalTest>()
-//    );
-//}
+TEST_CASE("Synchronous captive portal test") {
+    test::nettests::make_test<CaptivePortalTest>()
+        .run();
+}
 
-TEST_CASE("gen_no_cp_f works") {
-    input i;
-    i["name"] = "Android Lollipop HTTP";
-    i["url"] = "http://connectivitycheck.android.com/generate_204";
-    i["status"] = "204";
-
-    Var<http::Response> r{new http::Response};
-
-    Var<no_cp_f_t> no_cp_f(new no_cp_f_t);
-    gen_no_cp_f(i, no_cp_f);
-
-    (*no_cp_f)(r);
+TEST_CASE("Asynchronous captive portal test") {
+    test::nettests::run_async(
+        test::nettests::make_test<CaptivePortalTest>()
+    );
 }
 
 #else
