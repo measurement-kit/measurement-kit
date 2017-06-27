af8bc01949c2cfa5fdb6c164da418c1029c7b3d2
Simone Basso <bassosimone@gmail.com>
2017-06-27 16:21:54 +0200

    [ci skip] I guess this was caused by a merge

--- include/private/mlabns/mlabns_impl.hpp
+++ include/private/mlabns/mlabns_impl.hpp
@@ -7,9 +7,6 @@
 #include "private/common/json.hpp"
 #include "private/common/mock.hpp"
 
-#include "private/common/json.hpp"
-#include "private/common/mock.hpp"
-
 #include <measurement_kit/ext.hpp>
 #include <measurement_kit/http.hpp>
 #include <measurement_kit/mlabns.hpp>
