554098c0615cd53f1c5415c581d2976ad9a27ac2
Simone Basso <bassosimone@gmail.com>
2017-07-06 17:23:12 +0200

    [ci skip] Make sure regress tests compile locally

--- test/nettests/captive_portal.cpp
+++ test/nettests/captive_portal.cpp
@@ -5,13 +5,11 @@
 #ifdef ENABLE_INTEGRATION_TESTS
 #define CATCH_CONFIG_MAIN
 #include "private/ext/catch.hpp"
-#include "private/ooni/captive_portal.hpp"
 
 #include "../nettests/utils.hpp"
 
 using namespace mk::nettests;
 using namespace mk;
-using namespace mk::ooni;
 
 TEST_CASE("Synchronous captive portal test") {
     test::nettests::make_test<CaptivePortalTest>()
