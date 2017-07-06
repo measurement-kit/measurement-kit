e8d2595cb851d3b82a352aa8f6a0bbcf40562b22
Simone Basso <bassosimone@gmail.com>
2017-07-06 17:21:18 +0200

    [ci skip] remove unneeded header
    While there, also run `clang-format -i`.

--- src/libmeasurement_kit/ooni/captive_portal.cpp
+++ src/libmeasurement_kit/ooni/captive_portal.cpp
@@ -4,7 +4,6 @@
 
 #include "private/common/parallel.hpp"
 #include "private/common/utils.hpp"
-#include "private/ooni/captive_portal.hpp"
 #include "private/ooni/constants.hpp"
 #include "private/ooni/utils.hpp"
 
@@ -13,10 +12,15 @@ namespace ooni {
 
 using namespace mk::report;
 
+//
+// Implementation note:
 // all but one of the current vendor tests are HTTP tests.
 // these check either a status code or the body content.
-typedef std::map<std::string,std::string> input_t;
-void gen_http_inputs(Var<std::vector<input_t>> is, Var<Logger> logger) {
+//
+
+typedef std::map<std::string, std::string> input_t; // Syntactic sugar
+
+static void gen_http_inputs(Var<std::vector<input_t>> is, Var<Logger> logger) {
     logger->info("starting to gen");
     input_t i;
     i["name"] = "Microsoft HTTP";
@@ -26,19 +30,21 @@ void gen_http_inputs(Var<std::vector<input_t>> is, Var<Logger> logger) {
     i.clear();
     i["name"] = "Apple HTTP 1";
     i["url"] = "http://captive.apple.com";
-    i["body"] = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>\n";
+    i["body"] = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</"
+                "BODY></HTML>\n";
     i["ua"] = "CaptiveNetworkSupport/1.0 wispr";
     is->push_back(i);
     i.clear();
     i["name"] = "Apple HTTP 2";
     i["url"] = "http://captive.apple.com/hotspot_detect.html";
-    i["body"] = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>\n";
+    i["body"] = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</"
+                "BODY></HTML>\n";
     i["ua"] = "CaptiveNetworkSupport/1.0 wispr";
     is->push_back(i);
     i.clear();
     i["name"] = "Android KitKat HTTP";
     i["url"] = "http://clients3.google.com/generate_204";
-    i["status"] = "204"; //XXX: easier to just convert to an int later...
+    i["status"] = "204"; // XXX: easier to just convert to an int later...
     is->push_back(i);
     i.clear();
     i["name"] = "Android Lollipop HTTP";
@@ -54,40 +60,37 @@ void gen_http_inputs(Var<std::vector<input_t>> is, Var<Logger> logger) {
     i["name"] = "Android Nougat HTTP 1";
     i["url"] = "https://www.google.com/generate_204";
     i["status"] = "204";
-    i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36";
+    i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "
+              "Gecko) Chrome/52.0.2743.82 Safari/537.36";
     is->push_back(i);
     i.clear();
     i["name"] = "Android Nougat HTTP 2";
     i["url"] = "http://www.google.com/gen_204";
     i["status"] = "204";
-    i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36";
+    i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "
+              "Gecko) Chrome/52.0.2743.82 Safari/537.36";
     is->push_back(i);
     i.clear();
     i["name"] = "Android Nougat HTTP 3";
     i["url"] = "http://play.googleapis.com/generate_204";
     i["status"] = "204";
-    i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36";
+    i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "
+              "Gecko) Chrome/52.0.2743.82 Safari/537.36";
     is->push_back(i);
     i.clear();
 }
 
-void http_many(Var<Entry> entry,
-               Callback<Error> all_done_cb,
-               Settings options,
-               Var<Reactor> reactor,
-               Var<Logger> logger) {
+void http_many(Var<Entry> entry, Callback<Error> all_done_cb, Settings options,
+               Var<Reactor> reactor, Var<Logger> logger) {
 
-    auto http_cb = [=](const input_t& input, Callback<Error> done_cb) {
+    auto http_cb = [=](const input_t &input, Callback<Error> done_cb) {
         return [=](Error err, Var<http::Response> response) {
             // result: true means unfiltered
-            Entry result = { {"name", input.at("name")},
-                             {"result", nullptr},
-                             {"url", input.at("url")},
-                             {"expected_body", nullptr},
-                             {"actual_body", nullptr},
-                             {"expected_status", nullptr},
-                             {"actual_status", nullptr},
-                             {"failure", nullptr} };
+            Entry result = {
+                  {"name", input.at("name")}, {"result", nullptr},
+                  {"url", input.at("url")},   {"expected_body", nullptr},
+                  {"actual_body", nullptr},   {"expected_status", nullptr},
+                  {"actual_status", nullptr}, {"failure", nullptr}};
 
             if (input.count("body")) {
                 result["expected_body"] = input.at("body");
@@ -111,7 +114,8 @@ void http_many(Var<Entry> entry,
                 if (input.count("body")) {
                     unfiltered = !!(input.at("body") == response->body);
                 } else if (input.count("status")) {
-                    unfiltered = !!(std::stoul(input.at("status")) == response->status_code);
+                    unfiltered = !!(std::stoul(input.at("status")) ==
+                                    response->status_code);
                 } else {
                     exit(1);
                 }
@@ -128,13 +132,13 @@ void http_many(Var<Entry> entry,
     gen_http_inputs(inputs, logger);
 
     std::vector<Continuation<Error>> continuations;
-    for (const auto& input : *inputs) {
+    for (const auto &input : *inputs) {
         logger->info("setting up %s", input.at("name").c_str());
         options["http/url"] = input.at("url");
         std::string body;
         http::Headers headers;
         if (input.count("ua")) {
-            headers = { { "User-Agent", input.at("ua") } };
+            headers = {{"User-Agent", input.at("ua")}};
         } else {
             headers = constants::COMMON_CLIENT_HEADERS;
         }
@@ -142,55 +146,52 @@ void http_many(Var<Entry> entry,
         // Note: the following callback uses `[=]`, which means that the
         // options are copied in each run. Thus, each test is getting its
         // right URL to connect to and check.
-        continuations.push_back(
-            [=](Callback<Error> done_cb) {
-                templates::http_request(entry, options, headers, body,
-                    http_cb(input, done_cb), reactor, logger);
-            }
-        );
+        continuations.push_back([=](Callback<Error> done_cb) {
+            templates::http_request(entry, options, headers, body,
+                                    http_cb(input, done_cb), reactor, logger);
+        });
     }
     mk::parallel(continuations, all_done_cb, 3);
 }
 
 // this is the only test that doesn't follow the pattern of those above.
 // this hostname should always resolve to this IP.
-void dns_msft_ncsi(Var<Entry> entry,
-                   Callback<Error> done_cb,
-                   Settings options,
-                   Var<Reactor> reactor,
-                   Var<Logger> logger) {
+void dns_msft_ncsi(Var<Entry> entry, Callback<Error> done_cb, Settings options,
+                   Var<Reactor> reactor, Var<Logger> logger) {
     std::string hostname = "dns.msftncsi.com";
     std::string nameserver = "";
-    templates::dns_query(
-        entry, "A", "IN", hostname, nameserver,
-        [=](Error err, Var<dns::Message> message) {
-            Entry result = { {"name", "Microsoft DNS"},
-                             {"result", nullptr},
-                             {"hostname", "dns.msftncsi.com"},
-                             {"expected_ip", "131.107.255.255"},
-                             {"failure", nullptr} };
-            if (!!err) {
-                logger->info("dns_query err: %s", err.as_ooni_error().c_str());
-                result["failure"] = err.as_ooni_error();
-                done_cb(err);
-            } else {
-                for (const auto& a : message->answers) {
-                    result["actual_ips"].push_back(a.ipv4);
-                }
-                if (message->answers.size() != 1) {
-                    logger->info("maybe captive portal");
-                    result["result"] = false;
-                } else if (message->answers[0].ipv4 != "131.107.255.255") {
-                    logger->info("probably captive portal");
-                    result["result"] = false;
-                } else {
-                    logger->info("no captive portal");
-                    result["result"] = true;
-                }
-                (*entry)["vendor_dns_tests"].push_back(result);
-                done_cb(NoError());
-            }
-        }, options, reactor, logger);
+    templates::dns_query(entry, "A", "IN", hostname, nameserver,
+                         [=](Error err, Var<dns::Message> message) {
+                             Entry result = {{"name", "Microsoft DNS"},
+                                             {"result", nullptr},
+                                             {"hostname", "dns.msftncsi.com"},
+                                             {"expected_ip", "131.107.255.255"},
+                                             {"failure", nullptr}};
+                             if (!!err) {
+                                 logger->info("dns_query err: %s",
+                                              err.as_ooni_error().c_str());
+                                 result["failure"] = err.as_ooni_error();
+                                 done_cb(err);
+                             } else {
+                                 for (const auto &a : message->answers) {
+                                     result["actual_ips"].push_back(a.ipv4);
+                                 }
+                                 if (message->answers.size() != 1) {
+                                     logger->info("maybe captive portal");
+                                     result["result"] = false;
+                                 } else if (message->answers[0].ipv4 !=
+                                            "131.107.255.255") {
+                                     logger->info("probably captive portal");
+                                     result["result"] = false;
+                                 } else {
+                                     logger->info("no captive portal");
+                                     result["result"] = true;
+                                 }
+                                 (*entry)["vendor_dns_tests"].push_back(result);
+                                 done_cb(NoError());
+                             }
+                         },
+                         options, reactor, logger);
 }
 
 void captive_portal(std::string /*input*/, Settings options,
@@ -199,20 +200,22 @@ void captive_portal(std::string /*input*/, Settings options,
     Var<Entry> entry(new Entry);
 
     logger->info("starting http_many");
-    http_many(entry, [=](Error err){
-        if (!!err) {
-            logger->info("http_many error");
-        }
-        logger->info("starting dns_msft_ncsi");
-        dns_msft_ncsi(entry, [=](Error err){
-            if (!!err) {
-                logger->info("dns_msft_ncsi error");
-            }
-            callback(entry);
-        }, options, reactor, logger);
-    }, options, reactor, logger);
-
-
+    http_many(entry,
+              [=](Error err) {
+                  if (!!err) {
+                      logger->info("http_many error");
+                  }
+                  logger->info("starting dns_msft_ncsi");
+                  dns_msft_ncsi(entry,
+                                [=](Error err) {
+                                    if (!!err) {
+                                        logger->info("dns_msft_ncsi error");
+                                    }
+                                    callback(entry);
+                                },
+                                options, reactor, logger);
+              },
+              options, reactor, logger);
 }
 
 } // namespace ooni
