87a2a8e6da5f8fcc544cdbe9db3864d219f4eb9f
Joe Landers <joe@joelanders.net>
2017-07-25 10:53:14 -0400

    keep old report format

--- src/libmeasurement_kit/ooni/captive_portal.cpp
+++ src/libmeasurement_kit/ooni/captive_portal.cpp
@@ -23,55 +23,55 @@ typedef std::map<std::string, std::string> input_t; // Syntactic sugar
 static std::vector<input_t> gen_http_inputs() {
     std::vector<input_t> is;
     input_t i;
-    i["name"] = "Microsoft HTTP";
+    i["name"] = "MS HTTP Captive Portal";
     i["url"] = "http://www.msftncsi.com/ncsi.txt";
     i["body"] = "Microsoft NCSI";
     is.push_back(i);
     i.clear();
-    i["name"] = "Apple HTTP 1";
+    i["name"] = "Apple HTTP Captive Portal";
     i["url"] = "http://captive.apple.com";
     i["body"] = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</"
                 "BODY></HTML>\n";
     i["ua"] = "CaptiveNetworkSupport/1.0 wispr";
     is.push_back(i);
     i.clear();
-    i["name"] = "Apple HTTP 2";
+    i["name"] = "Apple HTTP Captive Portal 2";
     i["url"] = "http://captive.apple.com/hotspot_detect.html";
     i["body"] = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</"
                 "BODY></HTML>\n";
     i["ua"] = "CaptiveNetworkSupport/1.0 wispr";
     is.push_back(i);
     i.clear();
-    i["name"] = "Android KitKat HTTP";
+    i["name"] = "Android KitKat HTTP Captive Portal";
     i["url"] = "http://clients3.google.com/generate_204";
     i["status"] = "204"; // XXX: easier to just convert to an int later...
     is.push_back(i);
     i.clear();
-    i["name"] = "Android Lollipop HTTP";
+    i["name"] = "Android Lollipop HTTP Captive Portal";
     i["url"] = "http://connectivitycheck.android.com/generate_204";
     i["status"] = "204";
     is.push_back(i);
     i.clear();
-    i["name"] = "Android Marshmallow HTTP";
+    i["name"] = "Android Marshmallow HTTP Captive Portal";
     i["url"] = "http://connectivitycheck.gstatic.com/generate_204";
     i["status"] = "204";
     is.push_back(i);
     i.clear();
-    i["name"] = "Android Nougat HTTP 1";
+    i["name"] = "Android Nougat HTTP Captive Portal 1";
     i["url"] = "https://www.google.com/generate_204";
     i["status"] = "204";
     i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "
               "Gecko) Chrome/52.0.2743.82 Safari/537.36";
     is.push_back(i);
     i.clear();
-    i["name"] = "Android Nougat HTTP 2";
+    i["name"] = "Android Nougat HTTP Captive Portal 2";
     i["url"] = "http://www.google.com/gen_204";
     i["status"] = "204";
     i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "
               "Gecko) Chrome/52.0.2743.82 Safari/537.36";
     is.push_back(i);
     i.clear();
-    i["name"] = "Android Nougat HTTP 3";
+    i["name"] = "Android Nougat HTTP Captive Portal 3";
     i["url"] = "http://play.googleapis.com/generate_204";
     i["status"] = "204";
     i["ua"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "
@@ -88,16 +88,19 @@ void http_many(Var<Entry> entry, Callback<Error> all_done_cb, Settings options,
         return [=](Error err, Var<http::Response> response) {
             // result: true means unfiltered
             Entry result = {
-                  {"name", input.at("name")}, {"result", nullptr},
-                  {"url", input.at("url")},   {"expected_body", nullptr},
-                  {"actual_body", nullptr},   {"expected_status", nullptr},
-                  {"actual_status", nullptr}, {"failure", nullptr}};
+                  {"URL", input.at("url")},
+                  {"http_status_number", nullptr},  // expected status code, if there is one
+                  {"http_status_summary", nullptr}, // leaving key present (for bw compat.)
+                  {"result", nullptr},              // true means no captive portal
+                  {"User_Agent", nullptr},          // if specified by the vendor
+                  {"failure", nullptr}
+            };
 
-            if (input.count("body")) {
-                result["expected_body"] = input.at("body");
-            }
             if (input.count("status")) {
-                result["expected_status"] = input.at("status");
+                result["http_status_number"] = input.at("status");
+            }
+            if (input.count("ua")) {
+                result["User_Agent"] = input.at("ua");
             }
 
             if (!!err) {
@@ -108,10 +111,6 @@ void http_many(Var<Entry> entry, Callback<Error> all_done_cb, Settings options,
                 logger->warn("null response");
                 result["failure"] = "unknown_error";
             } else {
-                result["actual_body"] = response->body;
-                result["actual_status"] = response->status_code;
-                // all tests check status or body but never both or neither
-                result["actual_body"] = response->body;
                 bool unfiltered = false;
                 if (input.count("body")) {
                     unfiltered = !!(input.at("body") == response->body);
@@ -129,7 +128,7 @@ void http_many(Var<Entry> entry, Callback<Error> all_done_cb, Settings options,
                              unfiltered ? "yes" : "no");
                 result["result"] = unfiltered;
             }
-            (*entry)["vendor_http_tests"].push_back(result);
+            (*entry)["vendor_tests"][input.at("name")] = result;
             done_cb(err);
         };
     };
@@ -170,34 +169,16 @@ void dns_msft_ncsi(Var<Entry> entry, Callback<Error> done_cb, Settings options,
     std::string nameserver = "";
     templates::dns_query(entry, "A", "IN", hostname, nameserver,
                          [=](Error err, Var<dns::Message> message) {
-                             Entry result = {{"name", "Microsoft DNS"},
-                                             {"result", nullptr},
-                                             {"hostname", "dns.msftncsi.com"},
-                                             {"expected_ip", "131.107.255.255"},
-                                             {"failure", nullptr}};
                              if (!!err) {
                                  logger->info("dns_query err: %s",
                                               err.as_ooni_error().c_str());
-                                 result["failure"] = err.as_ooni_error();
                                  done_cb(err);
                              } else {
                                  for (const auto &a : message->answers) {
                                      // XXX: This should actually be adapted
                                      // to also deal with IPv6 replies
-                                     result["actual_ips"].push_back(a.ipv4);
+                                     (*entry)["vendor_dns_tests"]["ms_dns_cp"].push_back(a.ipv4);
                                  }
-                                 if (message->answers.size() != 1) {
-                                     logger->info("maybe captive portal");
-                                     result["result"] = false;
-                                 } else if (message->answers[0].ipv4 !=
-                                            "131.107.255.255") {
-                                     logger->info("probably captive portal");
-                                     result["result"] = false;
-                                 } else {
-                                     logger->info("no captive portal");
-                                     result["result"] = true;
-                                 }
-                                 (*entry)["vendor_dns_tests"].push_back(result);
                                  done_cb(NoError());
                              }
                          },
