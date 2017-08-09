7b3fb922f3f6cbb79cde79cd6cd351ed27213acc
Simone Basso <bassosimone@gmail.com>
2017-08-09 23:22:12 +0200

    cleanup(worker): rewrite with less abstraction (#1287)
    The rewrite is quite straightforward but I am using less MK specific
    abstraction and more standard library stuff directly.

--- include/private/common/worker.hpp
+++ include/private/common/worker.hpp
@@ -4,39 +4,91 @@
 #ifndef PRIVATE_COMMON_WORKER_HPP
 #define PRIVATE_COMMON_WORKER_HPP
 
+#include <measurement_kit/common/callback.hpp>
 #include <measurement_kit/common/has_global_factory.hpp>
+#include <measurement_kit/common/logger.hpp>
 #include <measurement_kit/common/non_copyable.hpp>
 #include <measurement_kit/common/non_movable.hpp>
-#include <measurement_kit/common/reactor.hpp>
 
+#include <cassert>
+#include <functional>
 #include <list>
+#include <memory>
 #include <mutex>
+#include <thread>
+#include <utility>
 
 namespace mk {
 
-class Worker : public HasGlobalFactory<Worker>,
-               public NonCopyable,
-               public NonMovable {
+class Worker : public HasGlobalFactory<Worker> {
   public:
-    void run_in_background_thread(Callback<> &&func);
-    size_t parallelism();
-    void set_parallelism(size_t);
-    short concurrency();
+    class State : public NonCopyable, public NonMovable {
+      public:
+        unsigned short active = 0;
+        std::mutex mutex;
+        unsigned short parallelism = 3;
+        std::list<Callback<>> queue;
+    };
+
+    void run_in_background_thread(Callback<> &&func) {
+        std::unique_lock<std::mutex> _{state->mutex};
+
+        // Move function such that the running-in-background thread
+        // has unique ownership and controls its lifecycle.
+        state->queue.push_back(std::move(func));
+
+        if (state->active >= state->parallelism) {
+            return;
+        }
+
+        // Note: pass only the internal state, so that the thread can possibly
+        // continue to work even when the external object is gone.
+        auto task = [S = state]() {
+            for (;;) {
+                Callback<> func = [&]() {
+                    std::unique_lock<std::mutex> _{S->mutex};
+                    // Initialize inside the lock such that there is only
+                    // one critical section in which we could be
+                    if (S->queue.size() <= 0) {
+                        --S->active;
+                        return Callback<>{};
+                    }
+                    auto front = S->queue.front();
+                    S->queue.pop_front();
+                    return front;
+                }();
+                if (!func) {
+                    break;
+                }
+                try {
+                    func();
+                } catch (...) {
+                    mk::warn("worker thread: unhandled exception");
+                }
+            }
+        };
+
+        std::thread{task}.detach();
+        ++state->active;
+    }
+
+    unsigned short parallelism() const {
+        std::unique_lock<std::mutex> _{state->mutex};
+        return state->parallelism;
+    }
+
+    void set_parallelism(unsigned short newval) const {
+        std::unique_lock<std::mutex> _{state->mutex};
+        state->parallelism = newval;
+    }
+
+    unsigned short concurrency() const {
+        std::unique_lock<std::mutex> _{state->mutex};
+        return state->active;
+    }
 
   private:
-    /*
-     * Variables allocated indirectly using `Var<T>` because they need to
-     * survive this class lifetime when background threads use them.
-     */
-    Var<std::list<Callback<>>> queue_ = Var<std::list<Callback<>>>::make();
-    Var<std::mutex> mutex_ = Var<std::mutex>::make();
-    Var<short> active_ = Var<short>::make();
-
-    /*
-     * Not allocated using `Var<T>`, because it is only accessed by
-     * this object and background threads do not see it.
-     */
-    size_t parallelism_ = 3;
+    std::shared_ptr<State> state{std::make_shared<State>()};
 };
 
 } // namespace mk
