64c10120aea64020862b48f02e3ac81a03bb6dd6
Simone Basso <bassosimone@gmail.com>
2017-09-12 20:51:30 +0200

    cleanup: simplify lexical_cast and settings (#1352)

--- include/measurement_kit/common/scalar.hpp
+++ include/measurement_kit/common/scalar.hpp
@@ -1,34 +1,27 @@
 // Part of measurement-kit <https://measurement-kit.github.io/>.
 // Measurement-kit is free software under the BSD license. See AUTHORS
 // and LICENSE for more information on the copying conditions.
-#ifndef MEASUREMENT_KIT_COMMON_SETTINGS_ENTRY_HPP
-#define MEASUREMENT_KIT_COMMON_SETTINGS_ENTRY_HPP
+#ifndef MEASUREMENT_KIT_COMMON_SCALAR_HPP
+#define MEASUREMENT_KIT_COMMON_SCALAR_HPP
 
 #include <measurement_kit/common/error_or.hpp>
-
 #include <sstream>
 
 namespace mk {
 
-/// This class is a specialization of the ordinary string that can also
-/// store integral and boolean types. Once you have stored a value inside
-/// of this class, you can access its string representation easily since
-/// this class can be assigned to a string. If that fails, use the `str()`
-/// method that explicitly converts to a string. To convert to integral
-/// types, instead, use the `as<T>()` template method.
-class SettingsEntry : public std::string {
+class Scalar : public std::string {
   public:
-    SettingsEntry() {}
+    Scalar() {}
 
-    template <typename Type> SettingsEntry(Type value) {
+    template <typename Type> Scalar(Type value) {
         std::stringstream ss;
         ss << value;
         assign(ss.str());
     }
 
     template <typename Type> Type as() const {
-        std::stringstream ss(c_str());
-        Type value;
+        std::stringstream ss{c_str()};
+        Type value{};
         ss >> value;
         if (!ss.eof()) {
             throw ValueError(); // Not all input was converted
@@ -39,15 +32,13 @@ class SettingsEntry : public std::string {
         return value;
     }
 
-    template <typename Type> ErrorOr<Type> as_noexcept() const {
+    template <typename Type> ErrorOr<Type> as_noexcept() const noexcept {
         try {
             return as<Type>();
-        } catch (Error &e) {
+        } catch (const Error &e) {
             return e;
         }
     }
-
-    std::string str() const { return as<std::string>(); }
 
   protected:
   private:
