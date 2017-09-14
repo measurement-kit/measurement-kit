e9254c0ff50923368ecbd6bff53b4301a61884e5
Simone Basso <bassosimone@gmail.com>
2017-09-14 13:59:24 +0200

    cleanup(encoding): reorganize implementation

--- include/measurement_kit/common/detail/citrus_ctype.h
+++ include/measurement_kit/common/detail/citrus_ctype.h
@@ -1,7 +1,11 @@
-/*      $OpenBSD: citrus_ctype.h,v 1.5 2016/09/05 09:47:02 schwarze Exp $ */
-
 /*-
- * Copyright (c)2002 Citrus Project,
+ * Part of measurement-kit <https://measurement-kit.github.io/>.
+ * Measurement-kit is free software under the BSD license. See AUTHORS
+ * and LICENSE for more information on the copying conditions.
+ * =============================================================
+ * Based on $OpenBSD: citrus_ctype.h,v 1.5 2016/09/05 09:47:02 schwarze Exp $
+ *
+ * Portions Copyright (c)2002 Citrus Project,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,35 +28,38 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
  */
 
 #ifndef MEASUREMENT_KIT_COMMON_DETAIL_CITRUS_CTYPE_H
 #define MEASUREMENT_KIT_COMMON_DETAIL_CITRUS_CTYPE_H
 
+#include <wchar.h>
+
+#ifndef __restrict
+# define __restrict /* nothing */
+#endif
+
 #define _CITRUS_UTF8_MB_CUR_MAX 4
+
+struct _utf8_state {
+	wchar_t	ch;
+	int	want;
+	wchar_t	lbound;
+};
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-size_t	_citrus_none_ctype_mbrtowc(wchar_t * __restrict,
-		const char * __restrict, size_t);
-size_t  _citrus_none_ctype_mbsnrtowcs(wchar_t * __restrict,
-		const char ** __restrict, size_t, size_t);
-size_t  _citrus_none_ctype_wcrtomb(char * __restrict, wchar_t);
-size_t  _citrus_none_ctype_wcsnrtombs(char * __restrict,
-		const wchar_t ** __restrict, size_t, size_t);
-
-size_t	_citrus_utf8_ctype_mbrtowc(wchar_t * __restrict,
+size_t	mk_utf8_mbrtowc(wchar_t * __restrict,
 		const char * __restrict, size_t, mbstate_t * __restrict);
-int     _citrus_utf8_ctype_mbsinit(const mbstate_t * __restrict);
-size_t  _citrus_utf8_ctype_mbsnrtowcs(wchar_t * __restrict,
+int     mk_utf8_mbsinit(const mbstate_t * __restrict);
+size_t  mk_utf8_mbsnrtowcs(wchar_t * __restrict,
 		const char ** __restrict, size_t, size_t,
 		mbstate_t * __restrict);
-size_t  _citrus_utf8_ctype_wcrtomb(char * __restrict, wchar_t,
+size_t  mk_utf8_wcrtomb(char * __restrict, wchar_t,
 		mbstate_t * __restrict);
-size_t  _citrus_utf8_ctype_wcsnrtombs(char * __restrict,
+size_t  mk_utf8_wcsnrtombs(char * __restrict,
 		const wchar_t ** __restrict, size_t, size_t,
 		mbstate_t * __restrict);
 
