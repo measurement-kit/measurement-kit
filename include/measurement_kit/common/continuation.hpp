8c162c00aa9c649a6d5f2bdbef12ddbad15c2253
Simone Basso <bassosimone@gmail.com>
2017-09-15 17:31:26 +0200

    [ci skip] improvements

--- include/measurement_kit/common/continuation.hpp
+++ include/measurement_kit/common/continuation.hpp
@@ -15,31 +15,6 @@ namespace mk {
 /// input a callback to be called later when done. The `Continuation` alias
 /// allows to express the returned function more compactly.
 ///
-/// For example:
-///
-/// ```C++
-///
-/// static Continuation<Error> measure_foo(std::string address) {
-///     return [address = std::move(address)](Callback<Error> &&cb) {
-///         foo_start(std::move(address), std::move(cb));
-///     };
-/// }
-///
-/// static Continuation<Error> measure_foobar(std::string address) {
-///     return [address = std::move(address)](Callback<Error> &&cb) {
-///         foobar_start(std::move(address), std::move(cb));
-///     };
-/// }
-///
-/// ParallelExecutor{}
-///     .add(measure_foo(address))
-///     .add(measure_foobar(address))
-///     .start([](Error err) {
-///         // This will be called when both are done
-///     });
-///
-/// ```
-///
 /// \since v0.2.0.
 template <typename... T>
 using Continuation = std::function<void(Callback<T...>)>;
