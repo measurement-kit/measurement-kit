d9ed657a96e5cf861af41264a7d2b159d323244f
Simone Basso <bassosimone@gmail.com>
2017-10-30 15:28:09 +0100

    Repair build

--- src/libmeasurement_kit/ooni/whatsapp.cpp
+++ src/libmeasurement_kit/ooni/whatsapp.cpp
@@ -10,7 +10,7 @@
 #include <cassert>
 #include <measurement_kit/net/utils.hpp>
 #include <measurement_kit/ooni.hpp>
-#include <measurement_kit/portable/sys/socket.h>
+#include <measurement_kit/common/portable/sys/socket.h>
 
 namespace mk {
 namespace ooni {
