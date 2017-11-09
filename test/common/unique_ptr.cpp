d34010262e9f1da128b76719432ae256cf60efbf
Simone Basso <bassosimone@gmail.com>
2017-11-09 08:07:00 -0500

    Rename raw_ptr as unique_ptr (#1471)
    After reading excerpts of book on C++ recommended by @hellais, I did
    convince myself that it's better not to implicitly cast a smart pointer
    to its underlying pointer type, but it's better to have an explicit
    method, as `get()` for doing that.
    
    After this change, RawPtr now really looks like a wrapper for a unique
    pointer, hence just rename it for clarity and propagate changes.

--- test/common/unique_ptr.cpp
+++ test/common/unique_ptr.cpp
@@ -15,23 +15,18 @@ struct Foobar {
 };
 
 TEST_CASE("The get() method throws if the underlying pointer is null") {
-    RawPtr<Foobar> ptr;
+    UniquePtr<Foobar> ptr;
     REQUIRE_THROWS(ptr.get());
 }
 
-TEST_CASE("The cast operator throws if the underlying pointer is null") {
-    RawPtr<Foobar> ptr;
-    REQUIRE_THROWS((Foobar *)ptr);
-}
-
 TEST_CASE("The release method does not call the destructor") {
-    RawPtr<Foobar> ptr{new Foobar};
+    UniquePtr<Foobar> ptr{new Foobar};
     delete ptr.release();
     // Valgrind will tell us if we double free
 }
 
 TEST_CASE("The reset method does call the destructor") {
-    RawPtr<Foobar> ptr{new Foobar};
+    UniquePtr<Foobar> ptr{new Foobar};
     ptr.reset(new Foobar);
     // Valgrind will tell us if we leak
 }
