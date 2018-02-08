ac1562a6230791c85754066111119d4c7e04bf5e
Simone Basso <bassosimone@gmail.com>
2018-02-08 17:03:26 +0100

    cope with integration tests being enabled

--- test/engine/ndt_example.cpp
+++ test/engine/ndt_example.cpp
@@ -1,2 +1,6 @@
 // We want to run the unmodified example through Valgrind
+#ifdef ENABLE_INTEGRATION_TESTS
 #include "example/engine/ndt.cpp"
+#else
+int main(){}
+#endif
--- test/swig/ndt_example.cpp
+++ test/swig/ndt_example.cpp
@@ -1,2 +1,6 @@
 // We want to run the unmodified example through Valgrind
+#ifdef ENABLE_INTEGRATION_TESTS
 #include "example/swig/ndt.cpp"
+#else
+int main(){}
+#endif
