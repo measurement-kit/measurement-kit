e95116d4a979c9658c013577b534986ff4c776aa
Simone Basso <bassosimone@gmail.com>
2018-03-02 15:12:58 +0100

    feature(engine): add WebConnectivity examples

---
+++ example/engine/web_connectivity.cpp
@@ -0,0 +1,25 @@
+// Public domain 2017, Simone Basso <bassosimone@gmail.com.
+
+#define MK_ENGINE_INTERNALS
+#include <measurement_kit/engine.h>
+
+#include <iostream>
+
+#include <measurement_kit/common/nlohmann/json.hpp>
+
+int main() {
+    nlohmann::json settings{
+        {"inputs", {
+            "https://www.google.com/",
+            "https://ooni.torproject.org/",
+        }},
+        {"name", "WebConnectivity"},
+        {"verbosity", "INFO"},
+    };
+    std::clog << settings.dump() << "\n";
+    mk::engine::Task task{std::move(settings)};
+    while (!task.is_done()) {
+        auto event = task.wait_for_next_event();
+        std::clog << event << "\n";
+    }
+}
---
+++ example/swig/web_connectivity.cpp
@@ -0,0 +1,28 @@
+// Public domain 2017, Simone Basso <bassosimone@gmail.com.
+
+#include <measurement_kit/swig.hpp>
+
+#include <stdlib.h>
+
+#include <iostream>
+
+int main() {
+    std::string settings = R"({
+        "inputs": [
+            "https://slashdot.org/",
+            "http://www.microsoft.com"
+        ],
+        "name": "WebConnectivity",
+        "verbosity": "INFO"
+    })";
+    mk::swig::Task task;
+    auto rv = task.initialize_ex(settings);
+    if (!rv.result) {
+        std::clog << "ERROR: " << rv.reason << std::endl;
+        exit(1);
+    }
+    while (!task.is_done()) {
+        std::string event = task.wait_for_next_event();
+        std::clog << event << "\n";
+    }
+}
