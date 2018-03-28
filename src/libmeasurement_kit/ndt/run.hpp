d6e8dbbb1acd8bb4910780b3fec0ca7da1fec036
Simone Basso <bassosimone@gmail.com>
2018-03-28 17:47:36 +0200

    ndt: make most headers private (#1589)
    Only keep public the part containing the defines.

--- src/libmeasurement_kit/ndt/run.hpp
+++ src/libmeasurement_kit/ndt/run.hpp
@@ -1,24 +1,13 @@
 // Part of Measurement Kit <https://measurement-kit.github.io/>.
 // Measurement Kit is free software under the BSD license. See AUTHORS
 // and LICENSE for more information on the copying conditions.
-#ifndef MEASUREMENT_KIT_NDT_RUN_HPP
-#define MEASUREMENT_KIT_NDT_RUN_HPP
+#ifndef SRC_LIBMEASUREMENT_KIT_NDT_RUN_HPP
+#define SRC_LIBMEASUREMENT_KIT_NDT_RUN_HPP
 
 #include <measurement_kit/report.hpp>
 
 namespace mk {
 namespace ndt {
-
-// By default we pass MK_NDT_UPLOAD|MK_NDT_DOWNLOAD as settings["test_suite"]
-// but you can tweak that by only requesting a single phase.
-#define MK_NDT_MIDDLEBOX 1
-#define MK_NDT_UPLOAD 2
-#define MK_NDT_DOWNLOAD 4
-#define MK_NDT_SIMPLE_FIREWALL 8
-#define MK_NDT_STATUS 16
-#define MK_NDT_META 32
-#define MK_NDT_UPLOAD_EXT 64 // not implemented
-#define MK_NDT_DOWNLOAD_EXT 128
 
 void run_with_specific_server(SharedPtr<report::Entry> entry, std::string address, int port,
                               Callback<Error> callback, Settings settings = {},
