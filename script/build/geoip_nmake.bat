689ad85055e9f498738b91a0c536a481f4799cf9
Simone Basso <bassosimone@gmail.com>
2018-03-29 14:01:11 +0200

    build: fix GeoIP to produce `/MT` static lib

--- script/build/geoip_nmake.bat
+++ script/build/geoip_nmake.bat
@@ -6,7 +6,6 @@ if [%1] NEQ [] (
     cd ..\..\MK_BUILD\windows\%1\geoip\geoip-api-c\libGeoIP
     nmake -f Makefile.vc
     mkdir ..\..\..\..\..\..\MK_DIST\windows\%1\geoip\lib
-    copy GeoIP.dll ..\..\..\..\..\..\MK_DIST\windows\%1\geoip\lib
     copy GeoIP.lib ..\..\..\..\..\..\MK_DIST\windows\%1\geoip\lib
     mkdir ..\..\..\..\..\..\MK_DIST\windows\%1\geoip\include
     copy GeoIP.h ..\..\..\..\..\..\MK_DIST\windows\%1\geoip\include
