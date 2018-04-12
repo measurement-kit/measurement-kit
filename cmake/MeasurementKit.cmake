f7233240bda9a11ba8ec0a6771a6f6a6f1bfaa93
Simone Basso <bassosimone@gmail.com>
2018-04-12 13:10:50 +0200

    Feature/cmake improvements (#1602)

--- cmake/MeasurementKit.cmake
+++ cmake/MeasurementKit.cmake
@@ -1,28 +1,10 @@
 # measurement_kit
 
 if (${MK_BUILD_BINARIES})
+    file(STRINGS "cmake/BinSources.txt" MK_BIN_SOURCES)
     add_executable(
         measurement_kit
-        ${MK_ROOT}/src/measurement_kit/portable/_getopt.h
-        ${MK_ROOT}/src/measurement_kit/portable/err.h
-        ${MK_ROOT}/src/measurement_kit/portable/getopt.h
-        ${MK_ROOT}/src/measurement_kit/portable/unistd.h
-        ${MK_ROOT}/src/measurement_kit/cmdline.hpp
-        ${MK_ROOT}/src/measurement_kit/cmd/captive_portal.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/dash.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/dns_injection.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/facebook_messenger.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/http_header_field_manipulation.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/http_invalid_request_line.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/meek_fronted_requests.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/multi_ndt.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/ndt.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/tcp_connect.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/telegram.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/web_connectivity.cpp
-        ${MK_ROOT}/src/measurement_kit/cmd/whatsapp.cpp
-        ${MK_ROOT}/src/measurement_kit/main.cpp
-        ${MK_ROOT}/src/measurement_kit/utils.cpp
+        ${MK_BIN_SOURCES}
     )
 
     target_link_libraries(
