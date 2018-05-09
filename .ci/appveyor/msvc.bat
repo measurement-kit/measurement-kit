c6762d708b31159cf10540c783c485c31d0a5dee
Simone Basso <bassosimone@gmail.com>
2018-05-09 12:50:29 +0200

    More fixes to appveyor build

--- .ci/appveyor/msvc.bat
+++ .ci/appveyor/msvc.bat
@@ -2,6 +2,6 @@ PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
 cd %APPVEYOR_BUILD_FOLDER%
 set CHERE_INVOKING=yes
 bash -lc "./autogen.sh --cmake" || exit /b
-cmake.exe "-G$%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release . || exit /b
+cmake.exe "-G"%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release . || exit /b
 cmake.exe --build . || exit /b
 ctest.exe -a -j8 || exit /b
