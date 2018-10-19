d1c2eac5c0a7f2924aeaddefeda63d0d17825392
Simone Basso <bassosimone@gmail.com>
2018-10-19 13:23:13 +0200

    Embed lib{curl,mmdb}x in MK

---
+++ src/libmeasurement_kit/vendor/libcurlx.cpp
@@ -0,0 +1,2 @@
+#define MK_CURLX_INLINE_IMPL
+#include <measurement_kit/vendor/libcurlx/v0.3.2/libcurlx.h>
---
+++ src/libmeasurement_kit/vendor/libmmdbx.cpp
@@ -0,0 +1,2 @@
+#define MK_MMDBX_INLINE_IMPL
+#include <measurement_kit/vendor/libmmdbx/v0.2.0/libmmdbx.h>
