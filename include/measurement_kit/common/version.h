2caa400f13eb372e68cb7c380afd21ba298a24ff
Simone Basso <bassosimone@gmail.com>
2019-02-14 15:11:00 +0000

    release 0.10.0-beta: step 2: update headers

--- include/measurement_kit/common/version.h
+++ include/measurement_kit/common/version.h
@@ -14,7 +14,7 @@
 // MK_VERSION is measurement-kit version according to manual tagging. This is
 // updated when we bless a new release. It does not accurately describe a
 // development build, for which MK_VERSION_FULL may be better.
-#define MK_VERSION "0.9.0"
+#define MK_VERSION "0.10.0-beta"
 
 // MEASUREMENT_KIT_VERSION is a backward compatibility alias for MK_VERSION.
 #define MEASUREMENT_KIT_VERSION MK_VERSION
@@ -23,19 +23,19 @@
 // as part of the `./autogen.sh` script that prepares a source tarball. If we
 // cannot run `git` because we're not in a git repository, then we set this
 // variable equal to MK_VERSION.
-#define MK_VERSION_FULL "v0.9.0"
+#define MK_VERSION_FULL "v0.10.0-beta"
 
 // MK_VERSION_MAJOR is the major version number extracted from MK_VERSION_FULL.
 #define MK_VERSION_MAJOR 0
 
 // MK_VERSION_MINOR is the minor version number extracted from MK_VERSION_FULL.
-#define MK_VERSION_MINOR 9
+#define MK_VERSION_MINOR 10
 
 // MK_VERSION_PATCH is the patch version number extracted from MK_VERSION_FULL.
 #define MK_VERSION_PATCH 0
 
 // MK_VERSION_STABLE is 1 if this is a stable release, 0 otherwise.
-#define MK_VERSION_STABLE 1
+#define MK_VERSION_STABLE 0
 
 // MK_VERSION_NUMERIC is the numberic representation of MK_VERSION_FULL,
 // suitable to test the version of MK inside of headers.
@@ -47,7 +47,7 @@
 //       ^^^     ^^^     ^^^     ^^^
 //      major   minor   patch   stable
 // ```
-#define MK_VERSION_NUMERIC 0x00000000009000001LL
+#define MK_VERSION_NUMERIC 0x00000000010000000LL
 
 #ifdef __cplusplus
 extern "C" {
