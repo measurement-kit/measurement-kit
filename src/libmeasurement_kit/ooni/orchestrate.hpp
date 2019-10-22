0716b6b98c6db9e6786aa24e252d04b0dabd38c0
Simone Basso <bassosimone@gmail.com>
2019-10-22 17:13:58 +0000

    orchestra: use probe-services

--- src/libmeasurement_kit/ooni/orchestrate.hpp
+++ src/libmeasurement_kit/ooni/orchestrate.hpp
@@ -23,9 +23,9 @@ namespace orchestrate {
 #define MK_OONI_PRODUCTION_PROTEUS_REGISTRY_URL                                \
     "https://a.registry.proteus.ooni.io"
 #define MK_OONI_TESTING_PROTEUS_REGISTRY_URL                                   \
-    "https://registry.proteus.test.ooni.io"
+    "https://ps-test.ooni.io"
 #define MK_OONI_PRODUCTION_PROTEUS_EVENTS_URL "https://a.events.proteus.ooni.io"
-#define MK_OONI_TESTING_PROTEUS_EVENTS_URL "https://events.proteus.test.ooni.io"
+#define MK_OONI_TESTING_PROTEUS_EVENTS_URL "https://ps-test.ooni.io"
 
 std::string production_registry_url();
 std::string testing_registry_url();
